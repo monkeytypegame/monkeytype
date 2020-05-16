900acfe27f9ab47838a8f7ba93c35f0fd798c84b
Jack <bartnikjack@gmail.com>
2020-05-16 17:57:42 +0100

    made sure new words dont include spaces

--- public/js/script.js
+++ public/js/script.js
@@ -163,6 +163,9 @@ function buildSentences() {
 function addWord() {
   let language = words[config.language]
   let randomWord = language[Math.floor(Math.random() * language.length)];
+  while (randomWord.indexOf(' ') > -1) {
+    randomWord = language[Math.floor(Math.random() * language.length)];
+  }
   wordsList.push(randomWord);
   let w = "<div class='word'>";
   for (let c = 0; c < randomWord.length; c++) {
