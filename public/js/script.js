1e40307478d9c78acc2f7435080f9ec0b0b0fff3
Jack <bartnikjack@gmail.com>
2020-05-28 13:25:45 +0100

    fixed contextmenu appearing in the input

--- public/js/script.js
+++ public/js/script.js
@@ -1108,6 +1108,7 @@ $(document).keypress(function(event) {
   if (event["keyCode"] == 13) return;
   if (event["keyCode"] == 32) return;
   if (event["keyCode"] == 27) return;
+  if (event["keyCode"] == 93) return;
   //start the test
   if (currentInput == "" && inputHistory.length == 0) {
     try{
