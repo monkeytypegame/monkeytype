1cfb8bedac1109db183a08428c7d40804d56c2e9
Jack <bartnikjack@gmail.com>
2020-07-26 15:20:49 +0100

    changed the max number of characters to 14

--- functions/index.js
+++ functions/index.js
@@ -62,7 +62,7 @@ function isUsernameValid(name) {
   if (name === null || name === undefined || name === "") return false;
   if (/miodec/.test(name.toLowerCase())) return false;
   if (/bitly/.test(name.toLowerCase())) return false;
-  if (name.length > 12) return false;
+  if (name.length > 14) return false;
   return /^[0-9a-zA-Z_.-]+$/.test(name);
 }
 
