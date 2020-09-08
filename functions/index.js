6dc51171cb18c75f5f471b5e75f1d9ba07ec90a3
Jack <bartnikjack@gmail.com>
2020-09-08 22:38:59 +0100

    moving to a single patreon document

--- functions/index.js
+++ functions/index.js
@@ -450,17 +450,36 @@ exports.getPatreons = functions.https.onRequest(async (request, response) => {
   response.set("Access-Control-Allow-Origin", "*");
   response.set("Access-Control-Allow-Headers", "*");
   response.set("Access-Control-Allow-Credentials", "true");
+  if (request.method === "OPTIONS") {
+    // Send response to OPTIONS requests
+    response.set("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    response.set("Access-Control-Allow-Headers", "Authorization,Content-Type");
+    response.set("Access-Control-Max-Age", "3600");
+    response.status(204).send("");
+    return;
+  }
+  request = request.body.data;
+  try {
 
-  let patreon = await db.collection("patreon").orderBy("value", "desc").get();
-  let ret = [];
-  patreon.docs.forEach((pdoc) => {
-    ret.push(pdoc.data().name);
-  });
+    let patreon = await db.collection("patreon").doc('patreons').get();
+    let data = patreon.data().list;
 
-  // console.log(ret);
+    data = data.sort((a, b) => { return b.value - a.value });
+    
+    let ret = [];
+    data.forEach((pdoc) => {
+      ret.push(pdoc.name);
+    });
 
-  response.status(200).send({ data: ret });
+    response.status(200).send({ data: ret });
+    return;
+  } catch (e) {
+    response.status(200).send({e});
+    return;
+  }
 });
+
+
 
 async function incrementTestCounter(uid) {
   try{
