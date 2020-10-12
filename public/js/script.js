2cbc1a0a1dcaa86e6d038c0b36f538ff1ef53212
Jack <bartnikjack@gmail.com>
2020-10-12 01:04:36 +0100

    wrong var name

--- public/js/script.js
+++ public/js/script.js
@@ -2218,7 +2218,7 @@ function showResult(difficultyFailed = false) {
                       stats.wpm,
                       stats.acc,
                       stats.wpmRaw,
-                      stats.consistency
+                      consistency
                     );
                   } else if (e.data.resultCode === 1) {
                     if (localPb) {
