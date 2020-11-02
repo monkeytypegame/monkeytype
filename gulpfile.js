4bf148b944b218f0e0229ce0e58868173b80e05f
typer <teiqeo@gmail.com>
2020-11-01 23:06:30 -0800

    fixed a bug with sass watching and cleaned up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,31 +5,32 @@ const vinylPaths = require("vinyl-paths");
 var sass = require("gulp-sass");
 sass.compiler = require("dart-sass");
 
+//the order of files is important
 const gulpSrc = [
-  "src/js/misc.js",
-  "src/js/words.js",
-  "src/js/layouts.js",
-  "src/js/db.js",
-  "src/js/userconfig.js",
-  "src/js/commandline.js",
-  "src/js/leaderboards.js",
-  "src/js/settings.js",
-  "src/js/account.js",
-  "src/js/script.js",
+  "./src/js/misc.js",
+  "./src/js/words.js",
+  "./src/js/layouts.js",
+  "./src/js/db.js",
+  "./src/js/userconfig.js",
+  "./src/js/commandline.js",
+  "./src/js/leaderboards.js",
+  "./src/js/settings.js",
+  "./src/js/account.js",
+  "./src/js/script.js",
 ];
 
 task("cat", function () {
-  return src(gulpSrc).pipe(concat("monkeytype.js")).pipe(dest("public/js"));
+  return src(gulpSrc).pipe(concat("monkeytype.js")).pipe(dest("./public/js"));
 });
 
 task("sass", function () {
-  return src("src/sass/*.scss")
+  return src("./src/sass/*.scss")
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(dest("public/css"));
 });
 
 task("dist", function () {
-  return src("./static/**/*").pipe(dest("public/"));
+  return src("./static/**/*").pipe(dest("./public/"));
 });
 
 task("clean", function () {
@@ -39,7 +40,7 @@ task("clean", function () {
 task("build", series("dist", "sass", "cat"));
 
 task("watch", function () {
-  watch(["./static/**/*", ...gulpSrc], series("build"));
+  watch(["./static/**/*", "./src/**/*"], series("build"));
 });
 
 task("rebuild", series("clean", "build"));
