8ade02f17064fb7cc5483dfe712318289a401955
Jack <bartnikjack@gmail.com>
2020-11-11 15:26:09 +0000

    added qwertz

--- src/js/layouts.js
+++ src/js/layouts.js
@@ -82,6 +82,17 @@ const layouts = {
             " "
         ],
     },
+    qwertz: {
+        keymapShowTopRow: false,
+        iso: true,
+        keys: [
+            "^°", "1!", "2\"", "3§", "4$", "5%", "6&", "7/", "8(", "9)", "0=", "ß?", "´`",
+            "qQ", "wW", "eE", "rR", "tT", "zZ", "uU", "iI", "oO", "pP", "üÜ", "+*", "'#",
+            "aA", "sS", "dD", "fF", "gG", "hH", "jJ", "kK", "lL", "öÖ", "äÄ",
+            "<>", "yY", "xX", "cC", "vV", "bB", "nN", "mM", ",;", ".:", "-_",
+            " "
+        ]
+    },
     workman: {
         keymapShowTopRow: false,
         keys: [
