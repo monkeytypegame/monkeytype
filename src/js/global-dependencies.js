dd8b01ff5227e44dce9fe04b7595177d7e1cee1f
Marco Pagani <Marco-Pagani@users.noreply.github.com>
2020-11-18 00:27:36 -0500

    replaced jquery and html2canvas with external deps
    fixed import statement for jquery

--- src/js/global-dependencies.js
+++ src/js/global-dependencies.js
@@ -1,13 +1,18 @@
 // this file should be concatenated at the top of the legacy js files
 
+import $ from "jquery";
+import "jquery-color";
+import "jquery-easing";
+import "jquery.cookie";
+
 import Chart from "chart.js";
 import chartTrendline from "chartjs-plugin-trendline";
 import chartAnnotation from "chartjs-plugin-annotation";
-
-import moment from "moment";
-
 Chart.plugins.register(chartTrendline);
 Chart.plugins.register(chartAnnotation);
+
+import moment from "moment";
+import html2canvas from "html2canvas";
 
 import {
   db_getSnapshot,
