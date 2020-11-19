24143cfe67e557036d8a4cc4bdab3011860a57b0
decaf <decaf>
2020-11-19 08:35:32 -0600

    Fix "Bail Out" subcommands not hiding in single command mode

--- src/js/misc.js
+++ src/js/misc.js
@@ -511,6 +511,20 @@ function toggleFullscreen(elem) {
   }
 }
 
+function canBailOut() {
+  return (
+    (config.mode === "custom" &&
+      customTextIsRandom &&
+      customTextWordCount >= 5000) ||
+    (config.mode === "custom" &&
+      !customTextIsRandom &&
+      customText.length >= 5000) ||
+    (config.mode === "words" && config.words >= 5000) ||
+    config.words === 0 ||
+    (config.mode === "time" && (config.time >= 3600 || config.time === 0))
+  );
+}
+
 let simplePopups = {};
 
 class SimplePopup {
