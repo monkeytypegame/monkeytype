d47686a1046726489a537619cfd75a0eb076ba2a
Jack <bartnikjack@gmail.com>
2020-12-28 23:36:58 +0000

    fixed error while trying to get insertedat from ull

--- functions/index.js
+++ functions/index.js
@@ -1236,8 +1236,11 @@ exports.testCompleted = functions.https.onRequest(async (request, response) => {
                 );
 
                 if (
-                  obj.mode === "time" &&
-                  (obj.mode2 == "15" || obj.mode2 == "60")
+                  // obj.mode === "time" &&
+                  // (obj.mode2 == "15" || obj.mode2 == "60") &&
+                  // obj.language === "english"
+                  globallb != null &&
+                  dailylb != null
                 ) {
                   let updatedLbMemory = await getUpdatedLbMemory(
                     userdata,
