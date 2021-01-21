512b9ba8b8f0be9f32a0ee95b3911ee63b0a1223
avi123nash <avinash.kini@gmail.com>
2021-01-21 11:44:02 +0530

    Reverted duration change added to error notification

--- src/js/leaderboards.js
+++ src/js/leaderboards.js
@@ -218,7 +218,7 @@ function updateLeaderboards() {
     })
     .catch((e) => {
       hideBackgroundLoader();
-      Notifications.add("Something went wrong: " + e.message, -1, 3);
+      Notifications.add("Something went wrong: " + e.message, -1);
     });
 }
 
