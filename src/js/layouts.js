c8d688d8da4a66581bb07cd1e5f0fcb407a452a6
ramnathk <karthikeyan.ramnath@gmail.com>
2021-01-22 17:16:27 -0800

    renamed the layout

--- src/js/layouts.js
+++ src/js/layouts.js
@@ -235,7 +235,7 @@ const layouts = {
             " "
         ]
     },
-    handsdownalt: {
+    handsdown_alt: {
         keymapShowTopRow: false,
         keys: [
             "`~", "1!", "2@", "3#", "4$", "5%", "6^", "7&", "8*", "9(", "0)", "-_", "=+",
