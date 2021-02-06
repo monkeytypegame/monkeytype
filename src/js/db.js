08a45e54258c79bc1757ea13fefb165738e41012
Miodec <bartnikjack@gmail.com>
2021-02-06 05:27:14 +0000

    disabled pbs for quote mode

--- src/js/db.js
+++ src/js/db.js
@@ -263,6 +263,7 @@ export async function db_saveLocalPB(
   raw,
   consistency
 ) {
+  if(mode == "quote") return;
   function cont() {
     try {
       let found = false;
@@ -367,6 +368,7 @@ export async function db_saveLocalTagPB(
   raw,
   consistency
 ) {
+  if(mode == "quote") return;
   function cont() {
     let filteredtag = dbSnapshot.tags.filter((t) => t.id === tagId)[0];
     try {
