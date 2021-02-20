2a5cab9f6b509229683b02b2dfb8d9e592d0419b
Miodec <bartnikjack@gmail.com>
2021-02-20 20:29:44 +0000

    delaying graphs to avoid duplicating

--- src/js/tribe.js
+++ src/js/tribe.js
@@ -1233,7 +1233,7 @@ MP.socket.on("mp_room_user_finished", (data) => {
           let userGraph = MP.room.userGraphs[sid];
           userGraph.graph = drawMinigraph(sid, userGraph.data);
           next();
-        }, 1);
+        }, 50);
       });
     } else if (MP.room.userFinished) {
       MP.room.userGraphs[data.sid].graph = drawMinigraph(data.sid, data.result);
