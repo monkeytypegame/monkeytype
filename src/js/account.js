84a4beb2ed4030d6cf2064fddb432a40fcb3b435
Miodec <bartnikjack@gmail.com>
2021-03-12 17:42:07 +0000

    reverted one change

--- src/js/account.js
+++ src/js/account.js
@@ -244,18 +244,6 @@ function signOut() {
     });
 }
 
-function accountIconLoading(truefalse) {
-  if (truefalse) {
-    $("#top #menu .account .icon").html(
-      '<i class="fas fa-fw fa-spin fa-circle-notch"></i>'
-    );
-    $("#top #menu .account").css("opacity", 1).css("pointer-events", "none");
-  } else {
-    $("#top #menu .account .icon").html('<i class="fas fa-fw fa-user"></i>');
-    $("#top #menu .account").css("opacity", 1).css("pointer-events", "auto");
-  }
-}
-
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     // User is signed in.
