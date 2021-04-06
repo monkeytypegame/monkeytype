65ca667ca07c164b44922189728c751c15b1b8a8
Jack <bartnikjack@gmail.com>
2021-04-07 00:02:15 +0100

    clearing result when queueing again

--- src/js/tribe/tribe.js
+++ src/js/tribe/tribe.js
@@ -2006,6 +2006,7 @@ $(".pageTest #result #queueAgainButton").click((e) => {
     $(".pageTribe .prelobby .matchmaking .leaveMatchmakingButton").removeClass(
       "hidden"
     );
+    resetResult();
   }, 1000);
 });
 
