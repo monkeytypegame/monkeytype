6cde0b470b303ab2e88bb278ac03047c4447f47d
Jack <bartnikjack@gmail.com>
2021-04-11 19:54:32 +0100

    storing how many players are in queue

--- src/js/tribe/tribe-online-stats.js
+++ src/js/tribe/tribe-online-stats.js
@@ -1,11 +1,41 @@
 import * as Tribe from "./tribe";
 
+export let inQueueNumbers = [0, 0, 0, 0];
+
 export function showLoading() {
   $(".pageTribe .prelobby .welcome .onlineStatsLoader").removeClass("hidden");
 }
 
 export function hideLoading() {
   $(".pageTribe .prelobby .welcome .onlineStatsLoader").addClass("hidden");
+}
+
+export function incrementQueues(queues) {
+  queues.forEach((queue) => {
+    inQueueNumbers[queue]++;
+  });
+  updateQueueButtons();
+}
+
+export function decrementQueues(queues) {
+  queues.forEach((queue) => {
+    inQueueNumbers[queue]--;
+  });
+  updateQueueButtons();
+}
+
+export function setInQueue(newNum) {
+  inQueueNumbers = newNum;
+  updateQueueButtons();
+}
+
+export function updateQueueButtons() {
+  let buttons = $(".pageTribe .prelobby .matchmaking .buttons .button");
+  inQueueNumbers.forEach((num, index) => {
+    $(buttons[index])
+      .find(".subtext")
+      .text("In queue: " + num);
+  });
 }
 
 export function refresh() {
