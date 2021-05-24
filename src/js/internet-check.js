ac8bea3607dbb551956ebfb936bffa1948b23065
UnrealApex <dojo1011234@gmail.com>
2021-05-24 15:30:27 +0000

    Added notfication logic

--- src/js/internet-check.js
+++ src/js/internet-check.js
@@ -1,9 +1,15 @@
+import * as Notifications from "src/js/elements/notifications.js";
+
 // checks if we have an internet connection
-window.addEventListener('offline', function(e) {alert("No Internet Connection Detected" + "\n" + "Any Changes Made Now May Not Be Saved");});
+window.addEventListener("offline", function (e) {
+  Notifications.add(
+    "No Internet Connection Detected\nAny Changes Made Now May Not Be Saved",
+    1
+  );
+});
 
 // if we want to manually check for a connection, we can call this function
 function isOnline() {
-	var online = navigator.onLine;
-    alert("Is online: " + online);
+  var online = navigator.onLine;
+  Notifications.add(`Is online: ${online}`, 1);
 }
-
