3f1d111dcecb106065a904d22bed66a0de7353c2
lukew3 <lukew25073@gmail.com>
2021-05-26 19:18:17 -0400

    fixed non-standard schemas

--- backend/models/leaderboard.js
+++ backend/models/leaderboard.js
@@ -2,25 +2,25 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const leaderboardEntrySchema = new Schema({
-  name: String,
-  wpm: Number,
-  raw: Number,
-  acc: Number,
-  consistency: Number, //can be null
-  mode: String, //not sure why mode and mode2 are needed
-  mode2: Number,
-  timestamp: Date, //Is this the right type?
-  hidden: Boolean,
+  name: { type: String },
+  wpm: { type: Number },
+  raw: { type: Number },
+  acc: { type: Number },
+  consistency: { type: Number }, //can be null
+  mode: { type: String }, //not sure why mode and mode2 are needed
+  mode2: { type: Number },
+  timestamp: { type: Date },
+  hidden: { type: Boolean },
 });
 
 const leaderboardSchema = new Schema(
   {
-    resetTime: Date, //or Number, only on daily lb
-    size: Number,
+    resetTime: { type: Date }, //or Number, only on daily lb
+    size: { type: Number },
     board: [{ type: leaderboardEntrySchema }], //contents of leaderbaord
-    mode: String, //only time for now
-    mode2: Number, //only 15 and 60 for now
-    type: String, //global or local
+    mode: { type: String }, //only equal to 'time' for now
+    mode2: { type: Number }, //only equal to 15 and 60 for now
+    type: { type: String }, //global or local
   },
   {
     timestamps: true,
