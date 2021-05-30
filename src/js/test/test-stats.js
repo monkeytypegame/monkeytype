56e8d3a065e6f2a37f069bfdf663100d025f8ed4
Otard95 <stian.myklebostad@getmail.no>
2021-05-30 20:27:01 +0200

    Create function to calculate acc with grace

--- src/js/test/test-stats.js
+++ src/js/test/test-stats.js
@@ -167,6 +167,12 @@ export function setLastSecondNotRound() {
 export function calculateAccuracy() {
   return (accuracy.correct / (accuracy.correct + accuracy.incorrect)) * 100;
 }
+export function calculateAccuracyWithGrace() {
+  const period = Math.min(accuracy.correct + accuracy.incorrect, 15) / 15;
+  const grace = Math.pow(period, 2.5);
+  const incorrect = accuracy.incorrect * grace;
+  return (accuracy.correct / (accuracy.correct + incorrect)) * 100;
+}
 
 export function incrementAccuracy(correctincorrect) {
   if (correctincorrect) {
