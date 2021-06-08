70e66f7ef2bd80514db841512908c52ae85e1771
Jack <bartnikjack@gmail.com>
2021-06-08 23:54:30 +0100

    updated bot dao, removed bot file that is not needed anymore

--- backend/dao/botDAO.js
+++ backend/dao/botDAO.js
@@ -1,11 +1,38 @@
 const MonkeyError = require("../handlers/error");
 const { mongoDB } = require("../init/mongodb");
+
+async function addCommand(command, arguments) {
+  return await mongoDB()
+    .collection("bot-commands")
+    .insertOne({
+      command,
+      arguments,
+      executed: false,
+      requestTimestamp: Date.now(),
+    });
+}
+
 class BotDAO {
-  static async addCommand(command, arguments) {
-    return await mongoDB()
-      .collection("bot-commands")
-      .insertOne({ command, arguments, executed: false, requestTimestamp: Date.now() });
+  static async updateDiscordRole(discordId, wpm) {
+    return await addCommand("updateRole", [discordId, wpm]);
   }
+
+  static async announceLbUpdate(discordId, pos, lb, wpm, raw, acc, con) {
+    return await addCommand("sayLbUpdate", [
+      discordId,
+      pos,
+      lb,
+      wpm,
+      raw,
+      acc,
+      con,
+    ]);
+  }
+
+  // this probably will be rewritten but keeping the old code just in case
+  // static async announceDailyLbResult(lbdata) {
+  //   return await addCommand("announceDailyLbResult", [lbdata]);
+  // }
 }
 
 module.exports = BotDAO;
