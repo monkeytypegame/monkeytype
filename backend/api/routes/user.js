c84f444e15c74f59cec99e31d51b3474381aefff
Miodec <bartnikjack@gmail.com>
2021-06-16 00:46:07 +0100

    using require instead of imports, added check name endpoint

--- backend/api/routes/user.js
+++ backend/api/routes/user.js
@@ -1,11 +1,13 @@
-import { authenticateRequest } from "../../middlewares/auth";
+const { authenticateRequest } = require("../../middlewares/auth");
 
 const { Router } = require("express");
-import UserController from "../controllers/user";
+const UserController = require("../controllers/user");
 
 const router = Router();
 
 router.post("/user/signup", UserController.createNewUser);
+
+router.post("/user/checkName", authenticateRequest, UserController.checkName);
 
 router.post("/user/updateName", authenticateRequest, UserController.updateName);
 
