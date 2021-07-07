c3b058c8368d6059e774e4c1a39ed6930c3269dd
Miodec <bartnikjack@gmail.com>
2021-07-07 14:05:04 +0100

    added comment

--- backend/dao/public-stats.js
+++ backend/dao/public-stats.js
@@ -1,32 +1,22 @@
-const MonkeyError = require("../handlers/error");
+// const MonkeyError = require("../handlers/error");
 const { mongoDB } = require("../init/mongodb");
 const { roundTo2 } = require("../handlers/misc");
 
 class PublicStatsDAO {
-
-  static async increment(started, completed, time){
+  //needs to be rewritten, this is public stats not user stats
+  static async increment(started, completed, time) {
     time = roundTo2(time);
     await mongoDB()
-    .collection("users")
-    .updateOne(
-      { name: 'startedTests' },
-      { $inc: { value: started } }
-    );
+      .collection("users")
+      .updateOne({ name: "startedTests" }, { $inc: { value: started } });
     await mongoDB()
-    .collection("users")
-    .updateOne(
-      { name: 'completedTests' },
-      { $inc: { value: completed } }
-    );
+      .collection("users")
+      .updateOne({ name: "completedTests" }, { $inc: { value: completed } });
     await mongoDB()
-    .collection("users")
-    .updateOne(
-      { name: 'timeTyping' },
-      { $inc: { value: time } }
-    );
+      .collection("users")
+      .updateOne({ name: "timeTyping" }, { $inc: { value: time } });
     return true;
   }
-
 }
 
 module.exports = PublicStatsDAO;
