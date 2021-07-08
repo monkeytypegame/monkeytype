2c5dde2ddafb233c817dfb8c87c4fba5a31d9a30
Miodec <bartnikjack@gmail.com>
2021-07-08 15:59:28 +0100

    missing this

--- backend/handlers/error.js
+++ backend/handlers/error.js
@@ -3,12 +3,12 @@ const uuid = require("uuid");
 class MonkeyError {
   constructor(status, message, stack = null) {
     this.status = status ?? 500;
-    let errorID = uuid.v4();
+    this.errorID = uuid.v4();
     if (this.status === 500) {
       this.message =
         process.env.MODE === "dev"
           ? String(stack)
-          : "Internal Server Error " + errorID;
+          : "Internal Server Error " + this.errorID;
     } else {
       this.message =
         process.env.MODE === "dev"
