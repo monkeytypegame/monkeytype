8bbe531d56a990769fa910a11e0262817e3c7a3b
UnrealApex <dojo1011234@gmail.com>
2021-07-19 21:39:19 +0000

    Notification instead of alert on successful clipboard write

---
+++ src/js/copy-user-name.js
@@ -0,0 +1,11 @@
+import * as Notifications from "./notifications";
+
+export function copyUserName() {
+    if (true) {
+        navigator.clipboard.writeText("Miodec#1512");
+        Notifications.add("Copied To Clipboard!", 0);
+    } else {
+        Notifications.add("Unable to copy user name", 0);
+    }
+}
+
