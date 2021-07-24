3f83344c2e01c8122697c87a73f13342b5af43d8
Jack <bartnikjack@gmail.com>
2021-07-24 15:51:13 +0100

    typo

--- src/js/test/test-logic.js
+++ src/js/test/test-logic.js
@@ -667,7 +667,7 @@ export async function init() {
     rq.text = rq.text.replace(/\\t/gm, "\t");
     rq.text = rq.text.replace(/\\n/gm, "\n");
     rq.text = rq.text.replace(/( *(\r\n|\r|\n) *)/g, "\n ");
-    rq.text = rq.trim();
+    rq.text = rq.text.trim();
 
     setRandomQuote(rq);
 
