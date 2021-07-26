88ef4ba920bdb032117638a195e649b30b6db718
Dharmaraj <63334359+DharmarajX24@users.noreply.github.com>
2021-07-26 19:43:49 +0530

    added ternary operator check status

--- backend/handlers/error.js
+++ backend/handlers/error.js
@@ -5,21 +5,16 @@ class MonkeyError {
   constructor(status, message, stack = null, uid) {
     this.status = status ?? 500;
     this.errorID = uuid.v4();
-    if (this.status === 500) {
-      this.message =
-        process.env.MODE === "dev"
-          ? stack
-            ? String(stack)
-            : String(message)
-          : "Internal Server Error " + this.errorID;
-    } else {
-      this.message =
-        process.env.MODE === "dev"
-          ? stack
-            ? String(stack)
+
+    this.message =
+      process.env.MODE === "dev"
+        ? stack
+          ? String(stack)
+          : this.status === 500
+            ? String(message) 
             : message
-          : message;
-    }
+        : "Internal Server Error " + this.errorID;
+  
     console.log("Error", message, stack);
     if (process.env.MODE !== "dev" && this.status === 500) {
       mongoDB()
