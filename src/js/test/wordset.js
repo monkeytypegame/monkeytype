a43908927ac7d4112144724e2819410e77340402
David McLeish <dave@mcleish.id.au>
2021-08-14 16:16:20 +1000

    add Gibberish class

--- src/js/test/wordset.js
+++ src/js/test/wordset.js
@@ -1,13 +1,6 @@
 let currentWordset = null;
 
-export function withWords(words) {
-  if (currentWordset == null || words !== currentWordset.words) {
-    currentWordset = new Wordset(words);
-  }
-  return currentWordset;
-}
-
-export class Wordset {
+class Wordset {
   constructor(words) {
     this.words = words;
     this.length = this.words.length;
@@ -16,4 +9,54 @@ export class Wordset {
   random() {
     return this.words[Math.floor(Math.random() * this.length)];
   }
+}
+
+const prefixSize = 3;
+
+class Gibberish extends Wordset {
+  constructor(words) {
+    super(words);
+    this.length = Infinity;
+
+    this.prefixes = {};
+    for (let word of words) {
+      word += ' ';
+      let prefix = '';
+      for (const c of word) {
+        if (!(prefix in this.prefixes)) {
+          this.prefixes[prefix] = [];
+        }
+        this.prefixes[prefix].push(c);
+        prefix = prefix.substr(-prefixSize);
+      }
+    }
+  }
+
+  random() {
+    let word = '';
+    for (; ;) {
+      let nextChoices = this.prefixes[word.substr(-prefixSize)];
+      if (!nextChoices) {
+        word = '';
+        continue;
+      }
+      const nextChar = nextChoices[Math.floor(Math * nextChoices.length)];
+      if (nextChar == ' ') {
+        break;
+      }
+      word += nextChar;
+    }
+    return word;
+  }
+}
+
+export function withWords(words, gibberish) {
+  if (
+    currentWordset == null ||
+    words !== currentWordset.words ||
+    gibberish != (currentWordset instanceof Gibberish)
+  ) {
+    currentWordset = new Wordset(words);
+  }
+  return currentWordset;
 }
