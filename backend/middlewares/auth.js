367b1046795c451ae3bc2df4ba73f033f27ce1e9
Miodec <bartnikjack@gmail.com>
2021-08-18 01:41:29 +0100

    better errors

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,12 +3,20 @@ const { verifyIdToken } = require("../handlers/auth");
 
 module.exports = {
   async authenticateRequest(req, res, next) {
+    console.log();
     try {
       const { authorization } = req.headers;
-      if (!authorization) throw new MonkeyError(401, "Unauthorized");
+      if (!authorization)
+        throw new MonkeyError(
+          401,
+          "Unauthorized",
+          `endpoint: ${req.baseUrl} no authrizaion header found`
+        );
       const token = authorization.split(" ");
       if (token[0].trim() !== "Bearer")
-        return next(new MonkeyError(400, "Invalid Token"));
+        return next(
+          new MonkeyError(400, "Invalid Token", "Incorrect token type")
+        );
       req.decodedToken = await verifyIdToken(token[1]);
       return next();
     } catch (e) {
