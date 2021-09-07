45181f67dc9e95e3eaa5c1dae0a47598d3a20615
Jack <bartnikjack@gmail.com>
2021-09-07 16:35:43 +0100

    keeping uid on the user that requested it if user is logged in, checking if user is not banned and doesnt need to update email when checking leaderboards updating lbmemory when checking sending back the rank if the user was still found

--- backend/api/controllers/leaderboards.js
+++ backend/api/controllers/leaderboards.js
@@ -1,10 +1,24 @@
 const LeaderboardsDAO = require("../../dao/leaderboards");
 const ResultDAO = require("../../dao/result");
+const UserDAO = require("../../dao/user");
+const admin = require("firebase-admin");
+const { verifyIdToken } = require("../../handlers/auth");
 
 class LeaderboardsController {
   static async get(req, res, next) {
     try {
       const { language, mode, mode2, skip, limit } = req.query;
+
+      let uid;
+
+      const { authorization } = req.headers;
+      if (authorization) {
+        const token = authorization.split(" ");
+        if (token[0].trim() == "Bearer")
+          req.decodedToken = await verifyIdToken(token[1]);
+        uid = req.decodedToken.uid;
+      }
+
       if (!language || !mode || !mode2 || !skip) {
         return res.status(400).json({
           message: "Missing parameters",
@@ -18,7 +32,11 @@ class LeaderboardsController {
         limit
       );
       retval.forEach((item) => {
-        delete item.uid;
+        if (uid && item.uid == uid) {
+          //
+        } else {
+          delete item.uid;
+        }
       });
       return res.status(200).json(retval);
     } catch (e) {
@@ -51,6 +69,27 @@ class LeaderboardsController {
           message: "Missing parameters",
         });
       }
+      //verify user first
+      let user = await UserDAO.getUser(uid);
+      if (!user) {
+        return res.status(400).json({
+          message: "User not found",
+        });
+      }
+      if (user.banned === true) {
+        return res.status(200).json({
+          message: "User banned",
+          banned: true,
+        });
+      }
+      let userauth = await admin.auth().getUser(uid);
+      if (!userauth.emailVerified) {
+        return res.status(200).json({
+          message: "User needs to verify email address",
+          needsToVerifyEmail: true,
+        });
+      }
+
       let result = await ResultDAO.getResult(uid, rid);
       if (!result.language) result.language = "english";
       if (
@@ -59,14 +98,49 @@ class LeaderboardsController {
         (result.mode2 == 15 || result.mode2 == 60) &&
         ["english"].includes(result.language)
       ) {
-        //run update
-        let retval = await LeaderboardsDAO.update(
-          result.mode,
-          result.mode2,
-          result.language,
-          uid
-        );
-        return res.status(200).json(retval);
+        //check if its better than their current lb pb
+        let lbpb =
+          user?.lbPersonalBests?.[result.mode]?.[result.mode2]?.[
+            result.language
+          ]?.wpm;
+        if (lbpb && result.wpm > lbpb) {
+          //run update
+          let retval = await LeaderboardsDAO.update(
+            result.mode,
+            result.mode2,
+            result.language,
+            uid
+          );
+          if (retval.rank) {
+            await UserDAO.updateLbMemory(
+              uid,
+              result.mode,
+              result.mode2,
+              result.language,
+              retval.rank
+            );
+          }
+          return res.status(200).json(retval);
+        } else {
+          let rank = await LeaderboardsDAO.getRank(
+            result.mode,
+            result.mode2,
+            result.language,
+            uid
+          );
+          rank = rank.rank;
+          await UserDAO.updateLbMemory(
+            uid,
+            result.mode,
+            result.mode2,
+            result.language,
+            rank
+          );
+          return res.status(200).json({
+            message: "Not a new leaderboard personal best",
+            rank,
+          });
+        }
       } else {
         return res.status(400).json({
           message: "This result is not eligible for any leaderboard",
