541d77836f0cf7bf7fd8344b496d6ff49802a5ab
Jack <bartnikjack@gmail.com>
2021-09-08 20:18:33 +0100

    updated layout of account page pb tables

--- src/js/account/pb-tables.js
+++ src/js/account/pb-tables.js
@@ -71,80 +71,96 @@ export function update() {
     pbData = pb.time[15].sort((a, b) => b.wpm - a.wpm)[0];
     text += `<tr>
       <td>15</td>
-      <td>${pbData.wpm}</td>
-      <td>${pbData.raw === undefined ? "-" : pbData.raw}</td>
-      <td>${pbData.acc === undefined ? "-" : pbData.acc + "%"}</td>
-      <td>
-      ${pbData.consistency === undefined ? "-" : pbData.consistency + "%"}
-      </td>
+      <td>${pbData.wpm}<br><span class="sub">${
+      pbData.acc === undefined ? "-" : pbData.acc + "%"
+    }</span></td>
+      <td>${pbData.raw}<br><span class="sub">${
+      pbData.consistency === undefined ? "-" : pbData.consistency + "%"
+    }</span></td>
+      <td>${moment(pbData.timestamp).format(
+        "DD MMM YYYY"
+      )}<br><div class='sub'>${moment(pbData.timestamp).format(
+      "HH:mm"
+    )}</div></td>
     </tr>`;
   } catch (e) {
     text += `<tr>
       <td>15</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
     </tr>`;
   }
   try {
     pbData = pb.time[30].sort((a, b) => b.wpm - a.wpm)[0];
     text += `<tr>
-      <td>30</td>
-      <td>${pbData.wpm}</td>
-      <td>${pbData.raw === undefined ? "-" : pbData.raw}</td>
-      <td>${pbData.acc === undefined ? "-" : pbData.acc + "%"}</td>
-      <td>
-      ${pbData.consistency === undefined ? "-" : pbData.consistency + "%"}
-      </td>
+    <td>30</td>
+      <td>${pbData.wpm}<br><span class="sub">${
+      pbData.acc === undefined ? "-" : pbData.acc + "%"
+    }</span></td>
+      <td>${pbData.raw}<br><span class="sub">${
+      pbData.consistency === undefined ? "-" : pbData.consistency + "%"
+    }</span></td>
+      <td>${moment(pbData.timestamp).format(
+        "DD MMM YYYY"
+      )}<br><div class='sub'>${moment(pbData.timestamp).format(
+      "HH:mm"
+    )}</div></td>
     </tr>`;
   } catch (e) {
     text += `<tr>
       <td>30</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
     </tr>`;
   }
   try {
     pbData = pb.time[60].sort((a, b) => b.wpm - a.wpm)[0];
     text += `<tr>
       <td>60</td>
-      <td>${pbData.wpm}</td>
-      <td>${pbData.raw === undefined ? "-" : pbData.raw}</td>
-      <td>${pbData.acc === undefined ? "-" : pbData.acc + "%"}</td>
-      <td>
-      ${pbData.consistency === undefined ? "-" : pbData.consistency + "%"}
-      </td>
+      <td>${pbData.wpm}<br><span class="sub">${
+      pbData.acc === undefined ? "-" : pbData.acc + "%"
+    }</span></td>
+      <td>${pbData.raw}<br><span class="sub">${
+      pbData.consistency === undefined ? "-" : pbData.consistency + "%"
+    }</span></td>
+      <td>${moment(pbData.timestamp).format(
+        "DD MMM YYYY"
+      )}<br><div class='sub'>${moment(pbData.timestamp).format(
+      "HH:mm"
+    )}</div></td>
     </tr>`;
   } catch (e) {
     text += `<tr>
       <td>60</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
     </tr>`;
   }
   try {
     pbData = pb.time[120].sort((a, b) => b.wpm - a.wpm)[0];
     text += `<tr>
       <td>120</td>
-      <td>${pbData.wpm}</td>
-      <td>${pbData.raw === undefined ? "-" : pbData.raw}</td>
-      <td>${pbData.acc === undefined ? "-" : pbData.acc + "%"}</td>
-      <td>
-      ${pbData.consistency === undefined ? "-" : pbData.consistency + "%"}
-      </td>
+      <td>${pbData.wpm}<br><span class="sub">${
+      pbData.acc === undefined ? "-" : pbData.acc + "%"
+    }</span></td>
+      <td>${pbData.raw}<br><span class="sub">${
+      pbData.consistency === undefined ? "-" : pbData.consistency + "%"
+    }</span></td>
+      <td>${moment(pbData.timestamp).format(
+        "DD MMM YYYY"
+      )}<br><div class='sub'>${moment(pbData.timestamp).format(
+      "HH:mm"
+    )}</div></td>
     </tr>`;
   } catch (e) {
     text += `<tr>
       <td>120</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
     </tr>`;
   }
   $(".pageAccount .timePbTable tbody").html(text);
@@ -154,80 +170,96 @@ export function update() {
     pbData = pb.words[10].sort((a, b) => b.wpm - a.wpm)[0];
     text += `<tr>
       <td>10</td>
-      <td>${pbData.wpm}</td>
-      <td>${pbData.raw === undefined ? "-" : pbData.raw}</td>
-      <td>${pbData.acc === undefined ? "-" : pbData.acc + "%"}</td>
-      <td>
-      ${pbData.consistency === undefined ? "-" : pbData.consistency + "%"}
-      </td>
+      <td>${pbData.wpm}<br><span class="sub">${
+      pbData.acc === undefined ? "-" : pbData.acc + "%"
+    }</span></td>
+      <td>${pbData.raw}<br><span class="sub">${
+      pbData.consistency === undefined ? "-" : pbData.consistency + "%"
+    }</span></td>
+      <td>${moment(pbData.timestamp).format(
+        "DD MMM YYYY"
+      )}<br><div class='sub'>${moment(pbData.timestamp).format(
+      "HH:mm"
+    )}</div></td>
     </tr>`;
   } catch (e) {
     text += `<tr>
       <td>10</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
     </tr>`;
   }
   try {
     pbData = pb.words[25].sort((a, b) => b.wpm - a.wpm)[0];
     text += `<tr>
       <td>25</td>
-      <td>${pbData.wpm}</td>
-      <td>${pbData.raw === undefined ? "-" : pbData.raw}</td>
-      <td>${pbData.acc === undefined ? "-" : pbData.acc + "%"}</td>
-      <td>
-      ${pbData.consistency === undefined ? "-" : pbData.consistency + "%"}
-      </td>
+      <td>${pbData.wpm}<br><span class="sub">${
+      pbData.acc === undefined ? "-" : pbData.acc + "%"
+    }</span></td>
+      <td>${pbData.raw}<br><span class="sub">${
+      pbData.consistency === undefined ? "-" : pbData.consistency + "%"
+    }</span></td>
+      <td>${moment(pbData.timestamp).format(
+        "DD MMM YYYY"
+      )}<br><div class='sub'>${moment(pbData.timestamp).format(
+      "HH:mm"
+    )}</div></td>
     </tr>`;
   } catch (e) {
     text += `<tr>
       <td>25</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
     </tr>`;
   }
   try {
     pbData = pb.words[50].sort((a, b) => b.wpm - a.wpm)[0];
     text += `<tr>
       <td>50</td>
-      <td>${pbData.wpm}</td>
-      <td>${pbData.raw === undefined ? "-" : pbData.raw}</td>
-      <td>${pbData.acc === undefined ? "-" : pbData.acc + "%"}</td>
-      <td>
-      ${pbData.consistency === undefined ? "-" : pbData.consistency + "%"}
-      </td>
+      <td>${pbData.wpm}<br><span class="sub">${
+      pbData.acc === undefined ? "-" : pbData.acc + "%"
+    }</span></td>
+      <td>${pbData.raw}<br><span class="sub">${
+      pbData.consistency === undefined ? "-" : pbData.consistency + "%"
+    }</span></td>
+      <td>${moment(pbData.timestamp).format(
+        "DD MMM YYYY"
+      )}<br><div class='sub'>${moment(pbData.timestamp).format(
+      "HH:mm"
+    )}</div></td>
     </tr>`;
   } catch (e) {
     text += `<tr>
       <td>50</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
     </tr>`;
   }
   try {
     pbData = pb.words[100].sort((a, b) => b.wpm - a.wpm)[0];
     text += `<tr>
       <td>100</td>
-      <td>${pbData.wpm}</td>
-      <td>${pbData.raw === undefined ? "-" : pbData.raw}</td>
-      <td>${pbData.acc === undefined ? "-" : pbData.acc + "%"}</td>
-      <td>
-      ${pbData.consistency === undefined ? "-" : pbData.consistency + "%"}
-      </td>
+      <td>${pbData.wpm}<br><span class="sub">${
+      pbData.acc === undefined ? "-" : pbData.acc + "%"
+    }</span></td>
+      <td>${pbData.raw}<br><span class="sub">${
+      pbData.consistency === undefined ? "-" : pbData.consistency + "%"
+    }</span></td>
+      <td>${moment(pbData.timestamp).format(
+        "DD MMM YYYY"
+      )}<br><div class='sub'>${moment(pbData.timestamp).format(
+      "HH:mm"
+    )}</div></td>
     </tr>`;
   } catch (e) {
     text += `<tr>
       <td>100</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
+      <td>-<br><span class="sub">-</span></td>
     </tr>`;
   }
   $(".pageAccount .wordsPbTable tbody").html(text);
