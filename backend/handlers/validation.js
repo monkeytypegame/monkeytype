8c377cca58ff55d3d7821e5680ebd8b24c472939
Jack <bartnikjack@gmail.com>
2021-09-14 15:40:44 +0100

    username validation

--- backend/handlers/validation.js
+++ backend/handlers/validation.js
@@ -7,7 +7,7 @@ function isUsernameValid(name) {
   if (/miodec/.test(name.toLowerCase())) return false;
   //sorry for the bad words
   if (
-    /bitly|fuck|bitch|shit|pussy|nigga|niqqa|niqqer|nigger|ni99a|ni99er|niga|niger|cunt|faggot|retard/.test(
+    /bitly|fuck|bitch|shit|pussy|nigga|niqqa|niqqer|nigger|ni99a|ni99er|niggas|niga|niger|cunt|faggot|retard/.test(
       name.toLowerCase()
     )
   )
