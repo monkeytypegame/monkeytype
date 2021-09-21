59bde1811cb8e90260c1bb2dbd87ed6eb28f0eb5
Jack <bartnikjack@gmail.com>
2021-09-21 22:46:59 +0100

    different log

--- src/js/ui.js
+++ src/js/ui.js
@@ -108,10 +108,11 @@ export function swapElements(
 }
 
 export function changePage(page, norestart = false) {
-  console.log("change");
   if (pageTransition) {
+    console.log(`change page ${page} stopped`);
     return;
   }
+  console.log(`change page ${page}`);
   let activePage = $(".page.active");
   $(".page").removeClass("active");
   $("#wordsInput").focusout();
@@ -255,7 +256,7 @@ $(".merchBanner .fas").click((event) => {
 });
 
 $(".scrollToTopButton").click((event) => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
 $(document).on("click", "#bottom .leftright .right .current-theme", (e) => {
