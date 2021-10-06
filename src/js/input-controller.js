77e36726827fa30360988e556c345539147dd0df
SeerLite <seerlite@tuta.io>
2021-10-06 20:33:00 -0300

    Always reset #wordsInput with layout emulator
    From now on always call setWordsInput right after handleChar().
    
    This should finally fix all layout emulator issues.

--- src/js/input-controller.js
+++ src/js/input-controller.js
@@ -399,7 +399,6 @@ function handleChar(char, charIndex) {
 
   if (!thisCharCorrect && Misc.trailingComposeChars.test(resultingWord)) {
     TestLogic.input.current = resultingWord;
-    setWordsInput(" " + TestLogic.input.current);
     TestUI.updateWordElement();
     Caret.updatePosition();
     return;
@@ -472,7 +471,6 @@ function handleChar(char, charIndex) {
       charIndex < TestLogic.words.getCurrent().length + 20)
   ) {
     TestLogic.input.current = resultingWord;
-    setWordsInput(" " + TestLogic.input.current);
   }
 
   if (!thisCharCorrect && Config.difficulty == "master") {
@@ -545,7 +543,6 @@ function handleChar(char, charIndex) {
     Caret.updatePosition();
   }
 
-  setWordsInput(" " + TestLogic.input.current);
 }
 
 function handleTab(event) {
@@ -608,6 +605,7 @@ function handleTab(event) {
         } else {
           event.preventDefault();
           handleChar("\t", TestLogic.input.current.length);
+          setWordsInput(" " + TestLogic.input.current);
         }
       } else if (!TestUI.resultVisible) {
         if (
@@ -620,6 +618,7 @@ function handleTab(event) {
         } else {
           event.preventDefault();
           handleChar("\t", TestLogic.input.current.length);
+          setWordsInput(" " + TestLogic.input.current);
         }
       }
     } else if (Config.quickTab) {
@@ -708,6 +707,7 @@ $(document).keydown((event) => {
       TestLogic.finish();
     } else {
       handleChar("\n", TestLogic.input.current.length);
+      setWordsInput(" " + TestLogic.input.current);
     }
   }
 
@@ -735,6 +735,7 @@ $(document).keydown((event) => {
       handleChar(char, TestLogic.input.current.length);
       updateUI();
     }
+    setWordsInput(" " + TestLogic.input.current);
   }
 });
 
