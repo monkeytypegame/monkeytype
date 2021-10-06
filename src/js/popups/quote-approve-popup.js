70855acee11c4987087858f137d60696060c47a3
Jack <bartnikjack@gmail.com>
2021-10-06 14:59:26 +0100

    stylng changes, added refresh list button

--- src/js/popups/quote-approve-popup.js
+++ src/js/popups/quote-approve-popup.js
@@ -8,6 +8,47 @@ import * as Loader from "./loader";
 
 let quotes = [];
 
+function updateList() {
+  $("#quoteApprovePopupWrapper .quotes").empty();
+  quotes.forEach((quote) => {
+    let quoteEl = $(`
+      <div class="quote">
+        <textarea class="text">${quote.text}</textarea>
+        <input type="text" class="source" placeholder="Source" value="${quote.source}">
+        <div class="buttons">
+          <div class="icon-button" aria-label="Undo changes" data-balloon-pos="left"><i class="fas fa-fw fa-undo-alt"></i></div>
+          <div class="icon-button" aria-label="Refuse quote" data-balloon-pos="left"><i class="fas fa-fw fa-times"></i></div>
+          <div class="icon-button" aria-label="Approve quote" data-balloon-pos="left"><i class="fas fa-fw fa-check"></i></div>
+        </div>
+      </div>
+    `);
+    quoteEl.click(() => {
+      // approveQuote(quote);
+    });
+    $("#quoteApprovePopupWrapper .quotes").append(quoteEl);
+  });
+}
+
+async function getQuotes() {
+  Loader.show();
+  let response;
+  try {
+    response = await axiosInstance.get("/new-quotes/get");
+  } catch (e) {
+    Loader.hide();
+    let msg = e?.response?.data?.message ?? e.message;
+    Notifications.add("Failed to get new quotes: " + msg, -1);
+    return;
+  }
+  Loader.hide();
+  if (response.status !== 200) {
+    Notifications.add(response.data.message);
+  } else {
+    quotes = response.data;
+    updateList();
+  }
+}
+
 export async function show(noAnim = false) {
   if ($("#quoteApprovePopupWrapper").hasClass("hidden")) {
     quotes = [];
@@ -38,44 +79,12 @@ export function hide() {
   }
 }
 
-function updateList() {
-  $("#quoteApprovePopupWrapper .quotes").empty();
-  quotes.forEach((quote) => {
-    let quoteEl = $(`
-      <div class="quote">
-        <div class="quote-text">${quote.text}</div>
-        <div class="quote-author">${quote.source}</div>
-      </div>
-    `);
-    quoteEl.click(() => {
-      // approveQuote(quote);
-    });
-    $("#quoteApprovePopupWrapper .quotes").append(quoteEl);
-  });
-}
-
-async function getQuotes() {
-  Loader.show();
-  let response;
-  try {
-    response = await axiosInstance.get("/new-quotes/get");
-  } catch (e) {
-    Loader.hide();
-    let msg = e?.response?.data?.message ?? e.message;
-    Notifications.add("Failed to get new quotes: " + msg, -1);
-    return;
-  }
-  Loader.hide();
-  if (response.status !== 200) {
-    Notifications.add(response.data.message);
-  } else {
-    quotes = response.data;
-    updateList();
-  }
-}
-
 $("#quoteApprovePopupWrapper").on("mousedown", (e) => {
   if ($(e.target).attr("id") === "quoteApprovePopupWrapper") {
     hide();
   }
+});
+
+$("#quoteApprovePopupWrapper .button.refreshList").on("click", (e) => {
+  getQuotes();
 });
