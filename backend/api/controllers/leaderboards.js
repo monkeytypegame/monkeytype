3df56ff5bef1cda2c14858a0d2d6cee6b46367ca
Jack <bartnikjack@gmail.com>
2021-10-24 21:31:50 +0100

    removed discord id from being sent with the leaderboard data

--- backend/api/controllers/leaderboards.js
+++ backend/api/controllers/leaderboards.js
@@ -35,6 +35,7 @@ class LeaderboardsController {
         if (uid && item.uid == uid) {
           //
         } else {
+          delete item.discordId;
           delete item.uid;
         }
       });
