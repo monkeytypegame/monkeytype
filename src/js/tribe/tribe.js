b8dccbcd9f53388c707d680f471834904f98f06c
Jack <bartnikjack@gmail.com>
2021-11-01 13:42:45 +0000

    reconnection, version check

--- src/js/tribe/tribe.js
+++ src/js/tribe/tribe.js
@@ -3,6 +3,7 @@ import * as Notifications from "./notifications";
 import * as UpdateConfig from "./config";
 import * as DB from "./db";
 import * as TribePages from "./tribe-pages";
+import * as AccountController from "./account-controller";
 
 const socket = io(
   window.location.hostname === "localhost"
@@ -13,11 +14,10 @@ const socket = io(
     autoConnect: false,
     secure: true,
     reconnectionAttempts: 0,
+    reconnection: false,
   }
 );
 export let state = -1;
-export let activePage = "preloader";
-export let pageTransition = false;
 export let expectedVersion = "0.9.12";
 
 let autoJoin = undefined;
@@ -26,16 +26,48 @@ export function setAutoJoin(code) {
   autoJoin = code;
 }
 
-export function init() {
-  // $(".pageTribe .preloader .icon").html(
-  //   `<i class="fas fa-fw fa-spin fa-circle-notch"></i>`
-  // );
-  // $(".pageTribe .preloader .text").text("Connecting to Tribe");
-  Notifications.add("Connecting to Tribe", 0, undefined, "Tribe");
-  socket.connect();
+export async function init() {
+  $(".pageTribe .tribePage.preloader .icon").html(
+    `<i class="fas fa-fw fa-spin fa-circle-notch"></i>`
+  );
+  $(".pageTribe .tribePage.preloader .text").text("Waiting for login");
+
+  await AccountController.authPromise;
+
+  $(".pageTribe .tribePage.preloader .text").text("Connecting to Tribe");
+
+  setTimeout(() => {
+    socket.connect();
+  }, 500);
 }
 
-socket.on("connect", (e) => {
+$(".pageTribe .tribePage.preloader .reconnectButton").click((e) => {
+  $(".pageTribe .tribePage.preloader .reconnectButton").addClass(`hidden`);
+  init();
+});
+
+socket.on("connect", async (e) => {
+  let versionCheck = await new Promise((resolve, reject) => {
+    socket.emit(
+      "system_version_check",
+      { version: expectedVersion },
+      (response) => {
+        if (response.status !== "ok") {
+          socket.disconnect();
+          $(".pageTribe .tribePage.preloader .icon").html(
+            `<i class="fas fa-exclamation-triangle"></i>`
+          );
+          $(".pageTribe .tribePage.preloader .text").html(
+            `Version mismatch.<br>Try refreshing or clearing cache.<br><br>Client version: ${expectedVersion}<br>Server version: ${response.version}`
+          );
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      }
+    );
+  });
+  if (!versionCheck) return;
   UpdateConfig.setTimerStyle("mini", true);
   state = 1;
   Notifications.add("Connected", 1, undefined, "Tribe");
@@ -48,4 +80,36 @@ socket.on("connect", (e) => {
   TribePages.change("prelobby");
   // setName(name);
   // changeActiveSubpage("prelobby");
+});
+
+socket.on("disconnect", (e) => {
+  state = -1;
+  TribePages.change("preloader");
+  $(".pageTribe .tribePage.preloader .icon").html(
+    `<i class="fas fa-fw fa-times"></i>`
+  );
+  $(".pageTribe .tribePage.preloader .text").text(`Disconnected`);
+  $(".pageTribe .tribePage.preloader .reconnectButton").removeClass(`hidden`);
+});
+
+socket.on("connect_failed", (e) => {
+  state = -1;
+  console.error(e);
+  TribePages.change("preloader");
+  $(".pageTribe .tribePage.preloader .icon").html(
+    `<i class="fas fa-fw fa-times"></i>`
+  );
+  $(".pageTribe .tribePage.preloader .text").text(`Connection failed`);
+  $(".pageTribe .tribePage.preloader .reconnectButton").removeClass(`hidden`);
+});
+
+socket.on("connect_error", (e) => {
+  state = -1;
+  console.error(e);
+  TribePages.change("preloader");
+  $(".pageTribe .tribePage.preloader .icon").html(
+    `<i class="fas fa-fw fa-times"></i>`
+  );
+  $(".pageTribe .tribePage.preloader .text").text(`Connection error`);
+  $(".pageTribe .tribePage.preloader .reconnectButton").removeClass(`hidden`);
 });
