83b2504d16140f2d3d374bef775010baaa915ef1
Jack <bartnikjack@gmail.com>
2021-11-02 11:47:02 +0000

    added tribe sounds

---
+++ src/js/tribe/tribe-sound.js
@@ -0,0 +1,20 @@
+import { Howl } from "howler";
+
+let sounds = {
+  join: new Howl("../sound/tribe_ui/join.wav"),
+  leave: new Howl("../sound/tribe_ui/leave.wav"),
+  start: new Howl("../sound/tribe_ui/start.wav"),
+  chat1: new Howl("../sound/tribe_ui/chat1.wav"),
+  chat2: new Howl("../sound/tribe_ui/chat2.wav"),
+  chat_mention: new Howl("../sound/tribe_ui/chat_mention.wav"),
+  finish: new Howl("../sound/tribe_ui/finish.wav"),
+  finish_win: new Howl("../sound/tribe_ui/finish_win.wav"),
+  glow: new Howl("../sound/tribe_ui/glow.wav"),
+  cd: new Howl("../sound/tribe_ui/cd2.wav"),
+  cd_go: new Howl("../sound/tribe_ui/cd_go2.wav"),
+};
+
+export function play(name) {
+  sounds[name].seek(0);
+  sounds[name].play();
+}
