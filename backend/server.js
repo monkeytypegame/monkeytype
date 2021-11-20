e7e5f08bb33c9747a21982da0b2be778d3178476
Jack <bartnikjack@gmail.com>
2021-11-20 19:45:13 +0000

    only sending bot command if result is recent

--- backend/server.js
+++ backend/server.js
@@ -104,14 +104,19 @@ app.listen(PORT, async () => {
         .toArray();
 
       let changed;
+      let recent = false;
       for (let index in before15) {
         if (before15[index].uid !== after15[index].uid) {
           //something changed at this index
+          if (after15[index].timestamp > Date.now() - 1000 * 60 * 10) {
+            //checking if test is within 10 minutes
+            recent = true;
+          }
           changed = after15[index];
           break;
         }
       }
-      if (changed) {
+      if (changed && recent) {
         let name = changed.discordId ?? changed.name;
         BotDAO.announceLbUpdate(
           name,
