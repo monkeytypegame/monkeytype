9af0dc79b1a9cba3a86b1e4612caab9e6144b35a
Ferotiq <64989416+Ferotiq@users.noreply.github.com>
2022-01-01 09:06:41 -0600

    Fix retry button duplication issue. (#2231)

--- static/sw.js
+++ static/sw.js
@@ -9,7 +9,7 @@ self.addEventListener("install", function (event) {
 self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+      return response;
     })
   );
 });
