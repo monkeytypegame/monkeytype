d1bdeed00ee231e007f313b8fb45fb912ff272e1
Oxey <44652133+O-X-E-Y@users.noreply.github.com>
2022-01-06 20:11:31 +0100

    Update layouts.js

--- src/js/layouts.js
+++ src/js/layouts.js
@@ -899,7 +899,7 @@ const layouts = {
             " "
         ]
     },
-    octa8: {
+    dvarf: {
         keymapShowTopRow: false,
         keys: [
             "`~", "1!", "2@", "3#", "4$", "5%", "6^", "7&", "8*", "9(", "0)", "[{", "]}",
