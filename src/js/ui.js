44b809d3c18b4f31244831437f6d2aa14383c647
Miodec <bartnikjack@gmail.com>
2022-01-20 00:31:40 +0100

    change page function will change to a page based on pathname if no page name provided

--- src/js/ui.js
+++ src/js/ui.js
@@ -121,6 +121,23 @@ export function changePage(page, norestart = false) {
     console.log(`change page ${page} stopped`);
     return;
   }
+
+  if (page == undefined) {
+    //use window loacation
+    let pages = {
+      "/": "test",
+      "/login": "login",
+      "/settings": "settings",
+      "/about": "about",
+      "/account": "account",
+    };
+    let path = pages[window.location.pathname];
+    if (!path) {
+      path = "test";
+    }
+    page = path;
+  }
+
   console.log(`change page ${page}`);
   let activePageElement = $(".page.active");
   activePage = undefined;
