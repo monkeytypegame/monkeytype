6f54e0cc83bae88221d275942c5e243a60682648
Achess <jealtzo26@gmail.com>
2022-01-24 16:32:02 -0600

    Adding latam layout

--- src/js/layouts.js
+++ src/js/layouts.js
@@ -21,6 +21,17 @@ const layouts = {
             " "
         ]
     },
+    latam_qwerty: {
+        keymapShowTopRow: false,
+		iso: true,
+        keys: [
+            "|°", "1!", "2\"", "3#", "4$", "5%", "6&", "7/", "8(", "9)", "0=", "'?", "¿¡",
+            "qQ", "wW", "eE", "rR", "tT", "yY", "uU", "iI", "oO", "pP", "´¨", "+*", " ",
+            "aA", "sS", "dD", "fF", "gG", "hH", "jJ", "kK", "lL", "ñÑ", "{[",
+            "<>", "zZ", "xX", "cC", "vV", "bB", "nN", "mM", ",;", ".:", "-_",
+            " "
+        ]
+    },
     dvorak: {
         keymapShowTopRow: false,
         keys: [
