7ce9146c7dc7f85927a28f83035ac163dc7c9832
Bruce Berrios <58147810+Bruception@users.noreply.github.com>
2022-01-29 16:03:02 -0500

    Add initial report quote endpoint (#2367) by Bruception
    * Add initial report quote endpoint
    
    * Tune rate limiter for report quote
    
    * Tune rate limiter
    
    * Increase max comment size
    
    * Add supported languages to schema validation
    
    * Fix naming

--- backend/api/routes/quotes.js
+++ backend/api/routes/quotes.js
@@ -1,8 +1,11 @@
+const joi = require("joi");
 const { authenticateRequest } = require("../../middlewares/auth");
 const { Router } = require("express");
 const NewQuotesController = require("../controllers/new-quotes");
 const QuoteRatingsController = require("../controllers/quote-ratings");
 const RateLimit = require("../../middlewares/rate-limit");
+const { requestValidation } = require("../../middlewares/apiUtils");
+const SUPPORTED_QUOTE_LANGUAGES = require("../../constants/quoteLanguages");
 
 const quotesRouter = Router();
 
@@ -46,6 +49,33 @@ quotesRouter.post(
   RateLimit.quoteRatingsSubmit,
   authenticateRequest,
   QuoteRatingsController.submitRating
+);
+
+quotesRouter.post(
+  "/report",
+  RateLimit.quoteReportSubmit,
+  authenticateRequest,
+  requestValidation({
+    body: {
+      quoteId: joi.string().required(),
+      quoteLanguage: joi
+        .string()
+        .valid(...SUPPORTED_QUOTE_LANGUAGES)
+        .required(),
+      reason: joi
+        .string()
+        .valid(
+          "Grammatical error",
+          "Inappropriate content",
+          "Low quality content"
+        )
+        .required(),
+      comment: joi.string().allow("").max(250).required(),
+    },
+  }),
+  (req, res) => {
+    res.sendStatus(200);
+  }
 );
 
 module.exports = quotesRouter;
