7e7fefbb0c423d72035c72365bdeba4caaaee862
Miodec <bartnikjack@gmail.com>
2022-02-07 18:34:34 +0100

    connected two files into one

--- src/js/index.js
+++ src/js/index.js
@@ -25,3 +25,25 @@ import * as TestStats from "./test/test-stats";
 import * as Replay from "./test/replay";
 import * as TestTimer from "./test/test-timer";
 import * as Result from "./test/test-timer";
+
+//try to keep this list short because we need to eliminate it eventually
+global.getuid = Misc.getuid;
+
+//these exports are just for debugging in the browser
+global.snapshot = DB.getSnapshot;
+global.config = Config;
+// global.addnotif = Notifications.add;
+
+global.glarsesMode = toggleGlarses;
+
+global.stats = TestStats.getStats;
+
+global.replay = Replay.getReplayExport;
+
+global.enableTimerDebug = TestTimer.enableTimerDebug;
+
+global.getTimerStats = TestTimer.getTimerStats;
+
+global.toggleUnsmoothedRaw = Result.toggleUnsmoothedRaw;
+
+global.enableSpacingDebug = TestStats.enableSpacingDebug;
