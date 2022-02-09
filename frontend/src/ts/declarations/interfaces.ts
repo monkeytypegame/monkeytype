809ea5874ba95ecefe5e0d56ebbc02000b8fa355
Ferotiq <evanrayweigel@outlook.com>
2022-02-09 11:43:17 -0600

    Add ready.ts

--- frontend/src/ts/declarations/interfaces.ts
+++ frontend/src/ts/declarations/interfaces.ts
@@ -273,6 +273,7 @@ export declare interface Config {
   customBackground: string;
   customBackgroundSize: CustomBackgroundSize;
   customBackgroundFilter: CustomBackgroundFilter;
+  // @ts-ignore
   customLayoutfluid: LayoutFluidLayoutConfig;
   monkeyPowerLevel: MonkeyPowerLevel;
   minBurst: MinBurst;
@@ -398,4 +399,49 @@ export declare interface TimerStats {
   now: number;
   expected: number;
   nextDelay: number;
+}
+
+export declare interface GithubRelease {
+  url: string,
+  assets_url: string,
+  upload_url: string,
+  html_url: string,
+  id: number,
+  author: {
+    login: string,
+    id: number,
+    node_id: string,
+    avatar_url: string,
+    gravatar_id: string,
+    url: string,
+    html_url: string,
+    followers_url: string,
+    following_url: string,
+    gists_url: string,
+    starred_url: string,
+    subscriptions_url: string,
+    organizations_url: string,
+    repos_url: string,
+    events_url: string,
+    received_events_url: string,
+    type: string,
+    site_admin: boolean
+  },
+  node_id: string,
+  tag_name: string,
+  target_commitish: string,
+  name: string,
+  draft: boolean,
+  prerelease: boolean,
+  created_at: string,
+  published_at: string,
+  assets: any[],
+  tarball_url: string,
+  zipball_url: string,
+  body: string,
+  reactions: {
+    url: string,
+    total_count: number,
+    [reaction: string]: number | string
+  }
 }
