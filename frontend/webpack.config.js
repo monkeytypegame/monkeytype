5a3112ea91c095b07cc08981495f7447c5e5b090
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-02-10 19:14:55 +0500

    Added fallback for crypto-browserify and stream-browserify

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,10 +1,13 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development", // Change to 'production' for production
+  mode: "production", // Change to 'production' for production
   entry: "./src/js/index.js",
   resolve: {
-    fallback: { crypto: false },
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+    },
   },
   output: {
     path: path.resolve(__dirname, "public/js/"),
