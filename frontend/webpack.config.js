1aa1dafb352f65544c1b235e6ee97a4463a24b94
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-02-11 07:03:42 +0500

    Added babel loader and plugins to webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -11,6 +11,21 @@ module.exports = {
       stream: require.resolve("stream-browserify"),
     },
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
   output: {
     path: path.resolve(__dirname, "public/js/"),
     filename: "monkeytype.js",
