c0330f22dbdc31eade159d009abbe9d729e227e0
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2022-02-13 01:39:42 +0530

    Feat:Update response structure (#2427)
    * Fix:response and error structure
    
    * update:response message
    
    * update:response class
    
    * update
    
    * Update response message
    
    Co-authored-by: Mustafiz Kaifee Mumtaz <mustafiz.mumtaz@freecharge.com>

--- backend/api/controllers/result.js
+++ backend/api/controllers/result.js
@@ -9,6 +9,8 @@ const Logger = require("../../handlers/logger");
 const path = require("path");
 const { config } = require("dotenv");
 config({ path: path.join(__dirname, ".env") });
+const MonkeyError = require("../../handlers/error");
+const { MonkeyResponse } = require("../../handlers/response");
 
 let validateResult;
 let validateKeys;
@@ -35,8 +37,8 @@ try {
 class ResultController {
   static async getResults(req, _res) {
     const { uid } = req.ctx.decodedToken;
-
-    return await ResultDAO.getResults(uid);
+    const results = await ResultDAO.getResults(uid);
+    return new MonkeyResponse("Result retrieved", results);
   }
 
   static async deleteAll(req, res) {
@@ -44,8 +46,7 @@ class ResultController {
 
     await ResultDAO.deleteAll(uid);
     Logger.log("user_results_deleted", "", uid);
-
-    return res.sendStatus(200);
+    return new MonkeyResponse("All results deleted");
   }
 
   static async updateTags(req, res) {
@@ -53,8 +54,7 @@ class ResultController {
     const { tags, resultid } = req.body;
 
     await ResultDAO.updateTags(uid, resultid, tags);
-
-    return res.sendStatus(200);
+    return new MonkeyResponse("Result tags updated");
   }
 
   static async addResult(req, res) {
@@ -62,9 +62,9 @@ class ResultController {
     const { result } = req.body;
     result.uid = uid;
     if (validateObjectValues(result) > 0)
-      return res.status(400).json({ message: "Bad input" });
+      throw new MonkeyError(400, "Bad input");
     if (result.wpm === result.raw && result.acc !== 100) {
-      return res.status(400).json({ message: "Bad input" });
+      throw new MonkeyError(400, "Bad input");
     }
     if (
       (result.mode === "time" && result.mode2 < 15 && result.mode2 > 0) ||
@@ -91,7 +91,7 @@ class ResultController {
         result.customText.isTimeRandom &&
         result.customText.time < 15)
     ) {
-      return res.status(400).json({ message: "Test too short" });
+      throw new MonkeyError(400, "Test too short");
     }
 
     let resulthash = result.hash;
@@ -112,15 +112,13 @@ class ResultController {
           },
           uid
         );
-        return res.status(400).json({ message: "Incorrect result hash" });
+        throw new MonkeyError(400, "Incorrect result hash");
       }
     }
 
     if (validateResult) {
       if (!validateResult(result)) {
-        return res
-          .status(400)
-          .json({ message: "Result data doesn't make sense" });
+        throw new MonkeyError(400, "Result data doesn't make sense");
       }
     } else {
       if (process.env.MODE === "dev") {
@@ -185,7 +183,7 @@ class ResultController {
         },
         uid
       );
-      return res.status(400).json({ message: "Invalid result spacing" });
+      throw new MonkeyError(400, "Invalid result spacing");
     }
 
     try {
@@ -226,7 +224,7 @@ class ResultController {
         ) {
           if (validateKeys) {
             if (!validateKeys(result, uid)) {
-              return res.status(400).json({ message: "Possible bot detected" });
+              throw new MonkeyError(400, "Possible bot detected");
             }
           } else {
             if (process.env.MODE === "dev") {
@@ -238,7 +236,7 @@ class ResultController {
             }
           }
         } else {
-          return res.status(400).json({ message: "Missing key data" });
+          throw new MonkeyError(400, "Missing key data");
         }
       }
     }
@@ -318,13 +316,14 @@ class ResultController {
       );
     }
 
-    return res.status(200).json({
-      message: "Result saved",
+    const data = {
       isPb,
       name: result.name,
       tagPbs,
       insertedId: addedResult.insertedId,
-    });
+    };
+
+    return new MonkeyResponse("Result saved", data);
   }
 }
 
