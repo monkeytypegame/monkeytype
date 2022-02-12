66997a1788dcbce2629ee3073bea483266167f33
Miodec <bartnikjack@gmail.com>
2022-02-13 00:10:19 +0100

    index to ts

--- frontend/webpack-production.config.js
+++ frontend/webpack-production.config.js
@@ -5,7 +5,7 @@ let circularImportNum = 0;
 
 module.exports = {
   mode: "production",
-  entry: path.resolve(__dirname, "src/scripts/index.js"),
+  entry: path.resolve(__dirname, "src/scripts/index.ts"),
   resolve: {
     fallback: {
       crypto: require.resolve("crypto-browserify"),
