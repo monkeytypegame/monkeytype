2408cba9acac20033eb44973a737d0fc19096d30
Bruce Berrios <58147810+Bruception@users.noreply.github.com>
2022-02-13 15:24:06 -0500

    Api changes (#2492)
    * API changes
    
    * Remove unused import

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -1,20 +1,45 @@
+const { asyncHandler } = require("../../middlewares/api-utils");
+const { MonkeyResponse } = require("../../handlers/monkey-response");
+
 const pathOverride = process.env.API_PATH_OVERRIDE;
 const BASE_ROUTE = pathOverride ? `/${pathOverride}` : "";
+const APP_START_TIME = Date.now();
+let requestsProcessed = 0;
 
 const API_ROUTE_MAP = {
-  "/user": require("./user"),
-  "/config": require("./config"),
-  "/results": require("./result"),
-  "/presets": require("./preset"),
-  "/psa": require("./psa"),
-  "/leaderboard": require("./leaderboards"),
+  "/users": require("./users"),
+  "/configs": require("./configs"),
+  "/results": require("./results"),
+  "/presets": require("./presets"),
+  "/psas": require("./psas"),
+  "/leaderboards": require("./leaderboards"),
   "/quotes": require("./quotes"),
 };
 
 function addApiRoutes(app) {
-  app.get("/", (req, res) => {
-    res.status(200).json({ message: "OK" });
+  app.use((req, res, next) => {
+    const inMaintenance =
+      process.env.MAINTENANCE === "true" || req.ctx.configuration.maintenance;
+
+    if (inMaintenance) {
+      return res
+        .status(503)
+        .json({ message: "Server is down for maintenance" });
+    }
+
+    requestsProcessed++;
+    return next();
   });
+
+  app.get(
+    "/",
+    asyncHandler((_req, _res) => {
+      return new MonkeyResponse("ok", {
+        uptime: Date.now() - APP_START_TIME,
+        requestsProcessed,
+      });
+    })
+  );
 
   Object.keys(API_ROUTE_MAP).forEach((route) => {
     const apiRoute = `${BASE_ROUTE}${route}`;
