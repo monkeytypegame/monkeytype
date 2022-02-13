c55b467cdd4fd49d50c123098103f9d44750db3d
Bruce Berrios <58147810+Bruception@users.noreply.github.com>
2022-02-13 15:24:06 -0500

    Api changes (#2492)
    * API changes
    
    * Remove unused import

--- backend/server.js
+++ backend/server.js
@@ -1,94 +1,45 @@
-const express = require("express");
 const { config } = require("dotenv");
 const path = require("path");
-const MonkeyError = require("./handlers/error");
 config({ path: path.join(__dirname, ".env") });
-const cors = require("cors");
+
+const app = require("./app");
+const jobs = require("./jobs");
+const db = require("./init/db");
 const admin = require("firebase-admin");
-const Logger = require("./handlers/logger.js");
+const ConfigurationDAO = require("./dao/configuration");
 // eslint-disable-next-line
 const serviceAccount = require("./credentials/serviceAccountKey.json");
-const db = require("./init/db");
-const jobs = require("./jobs");
-const addApiRoutes = require("./api/routes");
-const contextMiddleware = require("./middlewares/context");
-const ConfigurationDAO = require("./dao/configuration");
+
+async function bootServer(port) {
+  try {
+    console.log("Connecting to database...");
+    await db.connect();
+    console.log("Connected to database");
+
+    console.log("Initializing Firebase app instance...");
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+    console.log("Firebase app initialized");
+
+    console.log("Fetching live configuration...");
+    await ConfigurationDAO.getLiveConfiguration();
+    console.log("Live configuration fetched");
+
+    console.log("Starting cron jobs...");
+    jobs.forEach((job) => job.start());
+    console.log("Cron jobs started");
+  } catch (error) {
+    console.error("Failed to boot server");
+    console.error(error);
+    return process.exit(1);
+  }
+
+  return app.listen(PORT, () => {
+    console.log(`API server listening on port ${port}`);
+  });
+}
 
 const PORT = process.env.PORT || 5005;
 
-// MIDDLEWARE & SETUP
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
-
-app.set("trust proxy", 1);
-
-app.use(contextMiddleware);
-
-app.use((req, res, next) => {
-  if (process.env.MAINTENANCE === "true" || req.ctx.configuration.maintenance) {
-    res.status(503).json({ message: "Server is down for maintenance" });
-  } else {
-    next();
-  }
-});
-
-addApiRoutes(app);
-
-//DO NOT REMOVE NEXT, EVERYTHING WILL EXPLODE
-app.use(function (e, req, res, _next) {
-  if (/ECONNREFUSED.*27017/i.test(e.message)) {
-    e.message = "Could not connect to the database. It may have crashed.";
-    delete e.stack;
-  }
-
-  let monkeyError;
-  if (e.errorID) {
-    //its a monkey error
-    monkeyError = e;
-  } else {
-    //its a server error
-    monkeyError = new MonkeyError(e.status, e.message, e.stack);
-  }
-  if (!monkeyError.uid && req.ctx?.decodedToken) {
-    monkeyError.uid = req.ctx.decodedToken.uid;
-  }
-  if (process.env.MODE !== "dev" && monkeyError.status > 400) {
-    Logger.log(
-      "system_error",
-      `${monkeyError.status} ${monkeyError.message} ${monkeyError.stack}`,
-      monkeyError.uid
-    );
-    db.collection("errors").insertOne({
-      _id: monkeyError.errorID,
-      timestamp: Date.now(),
-      status: monkeyError.status,
-      uid: monkeyError.uid,
-      message: monkeyError.message,
-      stack: monkeyError.stack,
-    });
-    monkeyError.stack = undefined;
-  } else {
-    console.error(monkeyError.message);
-  }
-  return res.status(monkeyError.status || 500).json(monkeyError);
-});
-
-console.log("Starting server...");
-app.listen(PORT, async () => {
-  console.log(`Listening on port ${PORT}`);
-
-  console.log("Connecting to database...");
-  await db.connect();
-  console.log("Database connected");
-
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-
-  await ConfigurationDAO.getLiveConfiguration();
-
-  console.log("Starting cron jobs...");
-  jobs.forEach((job) => job.start());
-});
+bootServer(PORT);
