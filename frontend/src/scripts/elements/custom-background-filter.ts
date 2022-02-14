03c2d1db50357c9f68759e8043f4b6696bd32bf1
Miodec <bartnikjack@gmail.com>
2022-02-14 16:57:47 +0100

    converting to float just in case

--- frontend/src/scripts/elements/custom-background-filter.ts
+++ frontend/src/scripts/elements/custom-background-filter.ts
@@ -127,6 +127,7 @@ $(".section.customBackgroundFilter  .save.button").click(() => {
 
 ConfigEvent.subscribe((eventKey, eventValue) => {
   if (eventKey === "customBackgroundFilter") {
+    ((eventValue as unknown) as any[]).map((ev) => parseFloat(ev));
     loadConfig((eventValue as unknown) as number[]);
     apply();
   }
