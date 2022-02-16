9df2111f7dde07f0c44318728e35c416cf57d955
Bruce Berrios <58147810+Bruception@users.noreply.github.com>
2022-02-16 06:47:07 -0500

    Add all endpoints (#2514)

--- frontend/src/scripts/ape/endpoints/results.ts
+++ frontend/src/scripts/ape/endpoints/results.ts
@@ -2,10 +2,27 @@ const BASE_PATH = "/results";
 
 export default function getResultsEndpoints(
   apeClient: Ape.Client
-): Ape.Endpoints.Results {
-  async function getResults(): Ape.EndpointData {
+): Ape.Endpoints["results"] {
+  async function get(): Ape.EndpointData {
     return await apeClient.get(BASE_PATH);
   }
 
-  return { getResults };
+  async function save(result: MonkeyTypes.Result): Ape.EndpointData {
+    return await apeClient.post(BASE_PATH, { payload: { result } });
+  }
+
+  async function updateTags(
+    resultId: string,
+    tagIds: string[]
+  ): Ape.EndpointData {
+    return await apeClient.patch(`${BASE_PATH}/tags`, {
+      payload: { resultId, tagIds },
+    });
+  }
+
+  async function deleteAll(): Ape.EndpointData {
+    return await apeClient.delete(BASE_PATH);
+  }
+
+  return { get, save, updateTags, deleteAll };
 }
