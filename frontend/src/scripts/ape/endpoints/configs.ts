e0509dd4d322241f598401dc337cc52b32648ee4
Bruce Berrios <58147810+Bruception@users.noreply.github.com>
2022-02-16 06:47:07 -0500

    Add all endpoints (#2514)

--- frontend/src/scripts/ape/endpoints/configs.ts
+++ frontend/src/scripts/ape/endpoints/configs.ts
@@ -2,12 +2,17 @@ const BASE_PATH = "/configs";
 
 export default function getConfigsEndpoints(
   apeClient: Ape.Client
-): Ape.Endpoints.Configs {
-  async function getConfig(): Ape.EndpointData {
+): Ape.Endpoints["configs"] {
+  async function get(): Ape.EndpointData {
     return await apeClient.get(BASE_PATH);
   }
 
+  async function save(config: MonkeyTypes.Config): Ape.EndpointData {
+    return await apeClient.patch(BASE_PATH, { payload: { config } });
+  }
+
   return {
-    getConfig,
+    get,
+    save,
   };
 }
