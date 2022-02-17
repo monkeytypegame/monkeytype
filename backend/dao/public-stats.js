6ed27d2705506c68db9021d34bc3a3e5a08d74e1
Brian Evans <53117772+mrbrianevans@users.noreply.github.com>
2022-02-17 00:20:49 +0000

    Merged backend typescript into api overhaul (#2515)
    * Install typescript and add backend tsconfig
    
    Cannot yet build due to a number of compilation errors in JS code
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    * Fix typescript compilation errors
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    * Migrated backend to ES modules
    
    Switched to import export syntax
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    * Add typescript declaration for anticheat
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    * Rename top level files to .ts
    
    Fix service account json file typing
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    * Add dev build scripts for backend typescript
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    * Removed empty lines and switched to using db
    
    Cleaned up imports by removing needless empty lines and migrated to the new db.js instead of mongodb.js.
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    * Fixed backend commonjs syntax to ES module syntax
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    * Add build to backend start script
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- backend/dao/public-stats.js
+++ backend/dao/public-stats.js
@@ -1,26 +1,24 @@
 // const MonkeyError = require("../handlers/error");
-const { mongoDB } = require("../init/mongodb");
-const { roundTo2 } = require("../handlers/misc");
+import db from "../init/db";
+import { roundTo2 } from "../handlers/misc";
 
 class PublicStatsDAO {
   //needs to be rewritten, this is public stats not user stats
   static async updateStats(restartCount, time) {
     time = roundTo2(time);
-    await mongoDB()
-      .collection("public")
-      .updateOne(
-        { type: "stats" },
-        {
-          $inc: {
-            testsCompleted: 1,
-            testsStarted: restartCount + 1,
-            timeTyping: time,
-          },
+    await db.collection("public").updateOne(
+      { type: "stats" },
+      {
+        $inc: {
+          testsCompleted: 1,
+          testsStarted: restartCount + 1,
+          timeTyping: time,
         },
-        { upsert: true }
-      );
+      },
+      { upsert: true }
+    );
     return true;
   }
 }
 
-module.exports = PublicStatsDAO;
+export default PublicStatsDAO;
