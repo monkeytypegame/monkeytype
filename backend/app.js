1ad89b2ec4cb823a4cd03a8c9760a75a5fff3435
Bruce Berrios <58147810+Bruception@users.noreply.github.com>
2022-02-18 09:43:38 -0500

    Ape migration p2 (#2522)
    * Migrate leaderboard endpoints to ape
    
    * Fixed comment
    
    * Init backend types
    
    * Fail
    
    * Return

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
-import express, { urlencoded, json } from "express";
 import cors from "cors";
+import helmet from "helmet";
 import addApiRoutes from "./api/routes";
+import express, { urlencoded, json } from "express";
 import contextMiddleware from "./middlewares/context";
 import errorHandlingMiddleware from "./middlewares/error";
 
@@ -10,6 +11,7 @@ function buildApp() {
   app.use(urlencoded({ extended: true }));
   app.use(json());
   app.use(cors());
+  app.use(helmet());
 
   app.set("trust proxy", 1);
 
