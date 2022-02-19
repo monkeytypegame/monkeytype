f4c12dc19b477d6693bdcb74383290ea7b211d16
Miodec <bartnikjack@gmail.com>
2022-02-19 21:36:03 +0100

    converted preset controller to ts

--- frontend/src/scripts/controllers/preset-controller.ts
+++ frontend/src/scripts/controllers/preset-controller.ts
@@ -4,9 +4,10 @@ import * as Notifications from "../elements/notifications";
 import * as TestLogic from "../test/test-logic";
 import * as TagController from "./tag-controller";
 
-export function apply(_id) {
+export function apply(_id: string): void {
   // console.log(DB.getSnapshot().presets);
-  DB.getSnapshot().presets.forEach((preset) => {
+  const snapshot = DB.getSnapshot();
+  snapshot.presets?.forEach((preset) => {
     if (preset._id == _id) {
       Config.apply(JSON.parse(JSON.stringify(preset.config)));
       TagController.clear(true);
