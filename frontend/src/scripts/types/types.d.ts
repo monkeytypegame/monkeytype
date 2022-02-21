1bca0a3734e03de79272ddcac4297408c138e88a
Miodec <bartnikjack@gmail.com>
2022-02-21 01:24:18 +0100

    moved challenge type to MonkeyTypes, fixed challenge type

--- frontend/src/scripts/types/types.d.ts
+++ frontend/src/scripts/types/types.d.ts
@@ -572,4 +572,18 @@ declare namespace MonkeyTypes {
     [row4: string]: string[];
     [row5: string]: string[];
   }
+
+  interface Challenge {
+    name: string;
+    display: string;
+    autoRole: boolean;
+    type: string;
+    parameters: (string | number | boolean)[];
+    message: string;
+    requirements: {
+      [requirement: string]: {
+        [parameter: string]: string | number | boolean;
+      };
+    };
+  }
 }
