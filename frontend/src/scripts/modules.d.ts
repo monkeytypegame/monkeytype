99cb8a1181a8ef0b9cf61be300df1b828803a292
Ferotiq <evanrayweigel@outlook.com>
2022-02-20 19:41:16 -0600

    add test-ui.ts

--- frontend/src/scripts/modules.d.ts
+++ frontend/src/scripts/modules.d.ts
@@ -1,3 +1,4 @@
-declare let firebase: any; //typeof import("firebase").default;
+declare let firebase: any; // typeof import("firebase").default;
 declare let moment: typeof import("moment");
 declare let grecaptcha: ReCaptchaV2.ReCaptcha;
+declare let html2canvas: typeof import("html2canvas").default;
