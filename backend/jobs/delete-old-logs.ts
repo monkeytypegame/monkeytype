8c5b88ef5dee476f71e066eb13da370fd708ec90
Bruce Berrios <58147810+Bruception@users.noreply.github.com>
2022-02-23 23:17:17 -0500

    Migrate files to ts (#2581)

--- backend/jobs/delete-old-logs.ts
+++ backend/jobs/delete-old-logs.ts
@@ -6,7 +6,7 @@ const CRON_SCHEDULE = "0 0 0 * * *";
 const LOG_MAX_AGE_DAYS = 7;
 const LOG_MAX_AGE_MILLISECONDS = LOG_MAX_AGE_DAYS * 24 * 60 * 60 * 1000;
 
-async function deleteOldLogs() {
+async function deleteOldLogs(): Promise<void> {
   const data = await db
     .collection("logs")
     .deleteMany({ timestamp: { $lt: Date.now() - LOG_MAX_AGE_MILLISECONDS } });
