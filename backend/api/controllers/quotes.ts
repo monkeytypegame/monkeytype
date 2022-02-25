552bcd316eff31a1b1c4c1c565fafee2c8e9e6d4
Bruception <bberr022@fiu.edu>
2022-02-25 11:28:13 -0500

    Fix consistency

--- backend/api/controllers/quotes.ts
+++ backend/api/controllers/quotes.ts
@@ -97,7 +97,7 @@ class QuotesController {
       details: newReport.details,
     });
 
-    return new MonkeyResponse("Quote reported successfully");
+    return new MonkeyResponse("Quote reported");
   }
 }
 
