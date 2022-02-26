acc5d147f2490257b1d9dd20876bad7e000eba40
Miodec <bartnikjack@gmail.com>
2022-02-26 23:37:05 +0100

    removed any

--- frontend/src/scripts/config.ts
+++ frontend/src/scripts/config.ts
@@ -1876,9 +1876,9 @@ export function apply(configObj: MonkeyTypes.Config | null | "null"): void {
         $("#ad_about1").remove();
         $("#ad_about2").remove();
       }
-    } catch (e: any) {
-      Notifications.add("Error initialising ads: " + e.message);
-      console.log("error initialising ads " + e.message);
+    } catch (e) {
+      Notifications.add("Error initialising ads: " + (e as Error).message);
+      console.log("error initialising ads " + (e as Error).message);
       $(".footerads").remove();
       $("#ad_left").remove();
       $("#ad_right").remove();
