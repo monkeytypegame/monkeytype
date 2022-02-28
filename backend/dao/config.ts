0c7758e755b5dfd34d0ec27dbf890326743921bf
Miodec <bartnikjack@gmail.com>
2022-02-28 16:44:54 +0100

    saving key by key instead to support partial config

--- backend/dao/config.ts
+++ backend/dao/config.ts
@@ -3,9 +3,16 @@ import db from "../init/db";
 
 class ConfigDAO {
   static async saveConfig(uid, config): Promise<UpdateResult> {
+    //https://stackoverflow.com/questions/10290621/how-do-i-partially-update-an-object-in-mongodb-so-the-new-object-will-overlay
+    const temp = {};
+    for (const key in config) {
+      const value = config[key];
+      temp["config." + key] = value;
+    }
+
     return await db
       .collection("configs")
-      .updateOne({ uid }, { $set: { config } }, { upsert: true });
+      .updateOne({ uid }, { $set: temp }, { upsert: true });
   }
 
   static async getConfig(uid): Promise<object> {
