e21adf434ea7f107515619e8a2bb146e3a9eb28a
Ferotiq <evanrayweigel@outlook.com>
2022-03-01 05:26:28 -0600

    Run in parallel

--- frontend/src/scripts/index.ts
+++ frontend/src/scripts/index.ts
@@ -5,61 +5,123 @@ import chartTrendline from "chartjs-plugin-trendline";
 // @ts-ignore
 import chartAnnotation from "chartjs-plugin-annotation";
 
+Chart.plugins.register(chartTrendline);
+Chart.plugins.register(chartAnnotation);
+
 import firebase from "firebase";
 
 fetch("/__/firebase/init.json")
   .then(async (res) => firebase.initializeApp(await res.json()))
-  .finally(async () => {
-    import("./controllers/account-controller");
-    import("./test/caps-warning");
-    import("./popups/support-popup");
-    import("./popups/contact-popup");
-    import("./popups/version-popup");
-    import("./popups/edit-preset-popup");
-    import("./popups/simple-popups");
-    import("./controllers/input-controller");
-    import("./ready");
-    import("./ui");
-    import("./pages/about");
-    import("./popups/pb-tables-popup");
-    import("./elements/scroll-to-top");
-    import("./popups/mobile-test-config-popup");
-    import("./popups/edit-tags-popup");
-
-    const DB = await import("./db");
-    const Config = (await import("./config")).default;
-    const TestStats = await import("./test/test-stats");
-    const Replay = await import("./test/replay");
-    const TestTimer = await import("./test/test-timer");
-    const Result = await import("./test/result");
-    const TestInput = await import("./test/test-input");
-    const { enable } = await import("./states/glarses-mode");
-    const Account = await import("./pages/account");
-
-    Chart.plugins.register(chartTrendline);
-    Chart.plugins.register(chartAnnotation);
+  .then(() => {
+    console.log("firebase initialized");
 
     type ExtendedGlobal = typeof globalThis & MonkeyTypes.Global;
 
     const extendedGlobal = global as ExtendedGlobal;
 
-    extendedGlobal.snapshot = DB.getSnapshot;
+    import("./controllers/account-controller").then(() => {
+      console.log("imported account-controller");
+    });
 
-    extendedGlobal.config = Config;
+    import("./test/caps-warning").then(() => {
+      console.log("imported caps-warning");
+    });
 
-    extendedGlobal.toggleFilterDebug = Account.toggleFilterDebug;
+    import("./popups/support-popup").then(() => {
+      console.log("imported support-popup");
+    });
 
-    extendedGlobal.glarsesMode = enable;
+    import("./popups/contact-popup").then(() => {
+      console.log("imported contact-popup");
+    });
 
-    extendedGlobal.stats = TestStats.getStats;
+    import("./popups/version-popup").then(() => {
+      console.log("imported version-popup");
+    });
 
-    extendedGlobal.replay = Replay.getReplayExport;
+    import("./popups/edit-preset-popup").then(() => {
+      console.log("imported edit-preset-popup");
+    });
 
-    extendedGlobal.enableTimerDebug = TestTimer.enableTimerDebug;
+    import("./popups/simple-popups").then(() => {
+      console.log("imported simple-popups");
+    });
 
-    extendedGlobal.getTimerStats = TestTimer.getTimerStats;
+    import("./controllers/input-controller").then(() => {
+      console.log("imported input-controller");
+    });
 
-    extendedGlobal.toggleUnsmoothedRaw = Result.toggleUnsmoothedRaw;
+    import("./ready").then(() => {
+      console.log("imported ready");
+    });
 
-    extendedGlobal.enableSpacingDebug = TestInput.enableSpacingDebug;
-  });
+    import("./ui").then(() => {
+      console.log("imported ui");
+    });
+
+    import("./pages/about").then(() => {
+      console.log("imported about");
+    });
+
+    import("./popups/pb-tables-popup").then(() => {
+      console.log("imported pb-tables-popup");
+    });
+
+    import("./elements/scroll-to-top").then(() => {
+      console.log("imported scroll-to-top");
+    });
+
+    import("./popups/mobile-test-config-popup").then(() => {
+      console.log("imported mobile-test-config-popup");
+    });
+
+    import("./popups/edit-tags-popup").then(() => {
+      console.log("imported edit-tags-popup");
+    });
+
+    import("./db").then((DB) => {
+      extendedGlobal.snapshot = DB.getSnapshot;
+    });
+
+    import("./config").then(({ default: Config }) => {
+      console.log("imported config");
+      extendedGlobal.config = Config;
+    });
+
+    import("./test/test-stats").then((TestStats) => {
+      console.log("imported test-stats");
+      extendedGlobal.stats = TestStats.getStats;
+    });
+
+    import("./test/replay").then((Replay) => {
+      console.log("imported replay");
+      extendedGlobal.replay = Replay.getReplayExport;
+    });
+
+    import("./test/test-timer").then((TestTimer) => {
+      console.log("imported test-timer");
+      extendedGlobal.enableTimerDebug = TestTimer.enableTimerDebug;
+      extendedGlobal.getTimerStats = TestTimer.getTimerStats;
+    });
+
+    import("./test/result").then((Result) => {
+      console.log("imported result");
+      extendedGlobal.toggleUnsmoothedRaw = Result.toggleUnsmoothedRaw;
+    });
+
+    import("./test/test-input").then((TestInput) => {
+      console.log("imported test-input");
+      extendedGlobal.enableSpacingDebug = TestInput.enableSpacingDebug;
+    });
+
+    import("./states/glarses-mode").then(({ enable }) => {
+      console.log("imported glarses-mode");
+      extendedGlobal.glarsesMode = enable;
+    });
+
+    import("./pages/account").then((Account) => {
+      console.log("imported account");
+      extendedGlobal.toggleFilterDebug = Account.toggleFilterDebug;
+    });
+  })
+  .catch(console.error);
