0e3c654fb97bbd9a6d0e367b15ca4c69348f58b5
Ferotiq <evanrayweigel@outlook.com>
2022-03-01 22:08:14 -0600

    Update modules.d.ts

--- frontend/src/scripts/modules.d.ts
+++ frontend/src/scripts/modules.d.ts
@@ -1,5 +1,2 @@
 import "grecaptcha";
-
-// // declare let firebase: any; // typeof import("firebase").default;
-// declare let moment: typeof import("moment");
 declare let grecaptcha: ReCaptchaV2.ReCaptcha;
