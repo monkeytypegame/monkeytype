0eba666d69b3421f9f89faefd7afeabe852825b1
Bruception <bberr022@fiu.edu>
2022-03-04 15:18:42 -0500

    Migrate some DAO to ts

--- backend/dao/public-stats.ts
+++ backend/dao/public-stats.ts
@@ -3,15 +3,17 @@ import { roundTo2 } from "../utils/misc";
 
 class PublicStatsDAO {
   //needs to be rewritten, this is public stats not user stats
-  static async updateStats(restartCount, time) {
-    time = roundTo2(time);
-    await db.collection("public").updateOne(
+  static async updateStats(
+    restartCount: number,
+    time: number
+  ): Promise<boolean> {
+    await db.collection<MonkeyTypes.PublicStats>("public").updateOne(
       { type: "stats" },
       {
         $inc: {
           testsCompleted: 1,
           testsStarted: restartCount + 1,
-          timeTyping: time,
+          timeTyping: roundTo2(time),
         },
       },
       { upsert: true }
