ff9ee5632e3d8ddc5fbaf5cc83118e45e6c2d77c
Bruception <bberr022@fiu.edu>
2022-03-08 20:25:11 -0500

    Specify parse base

--- backend/api/controllers/leaderboards.ts
+++ backend/api/controllers/leaderboards.ts
@@ -7,13 +7,13 @@ class LeaderboardsController {
     const { language, mode, mode2, skip, limit = 50 } = req.query;
     const { uid } = req.ctx.decodedToken;
 
-    const queryLimit = Math.min(parseInt(limit as string), 50);
+    const queryLimit = Math.min(parseInt(limit as string, 10), 50);
 
     const leaderboard = await LeaderboardsDAO.get(
       mode,
       mode2,
       language,
-      parseInt(skip as string),
+      parseInt(skip as string, 10),
       queryLimit
     );
 
