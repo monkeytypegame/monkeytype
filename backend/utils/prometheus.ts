9ef6fecaf0ffac647036f1c02176f9a6e03e0bd0
Miodec <bartnikjack@gmail.com>
2022-03-12 15:06:37 +0100

    turns out there is no need for basic auth

--- backend/utils/prometheus.ts
+++ backend/utils/prometheus.ts
@@ -1,10 +1,5 @@
 import "dotenv/config";
-import { Counter, register } from "prom-client";
-import { Application } from "express";
-import basicAuth from "express-basic-auth";
-
-const METRICS_PASSWORD =
-  process.env.MODE === "dev" ? "stats" : process.env.METRICS_PASSWORD;
+import { Counter } from "prom-client";
 
 const auth = new Counter({
   name: "auth_counter",
@@ -14,17 +9,4 @@ const auth = new Counter({
 
 export function incrementAuth(type: "Bearer" | "ApeKey" | "None"): void {
   auth.inc({ type });
-}
-
-export function addRoute(app: Application): void {
-  if (!METRICS_PASSWORD) return;
-  app.get(
-    "/metrics",
-    basicAuth({
-      users: { stats: METRICS_PASSWORD },
-    }),
-    async (_req, res) => {
-      res.send(await register.metrics());
-    }
-  );
 }
