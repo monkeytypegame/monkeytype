d9ecbfe12c84b691eeffa40504373d5cb56d9d63
Miodec <bartnikjack@gmail.com>
2022-03-13 23:26:12 +0100

    lowered threshold to be slightly more responsive

--- frontend/src/scripts/test/focus.ts
+++ frontend/src/scripts/test/focus.ts
@@ -1,7 +1,7 @@
 import * as Caret from "./caret";
 import * as ActivePage from "../states/active-page";
 
-const unfocusPx = 5;
+const unfocusPx = 3;
 let state = false;
 
 export function set(foc: boolean, withCursor = false): void {
@@ -30,7 +30,8 @@ $(document).mousemove(function (event) {
     $("#top").hasClass("focus") &&
     event.originalEvent &&
     // To avoid mouse/desk vibration from creating a flashy effect, we'll unfocus @ >5px instead of >0px
-    (event.originalEvent.movementX > unfocusPx || event.originalEvent.movementY > unfocusPx)
+    (event.originalEvent.movementX > unfocusPx ||
+      event.originalEvent.movementY > unfocusPx)
   ) {
     set(false);
   }
