9dee0f3261cbaef669c97f24b14126e170c8ed25
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-03-20 08:18:10 +0500

    Rename Config

--- frontend/webpack/config.audit.js
+++ frontend/webpack/config.audit.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const BASE_CONFIG = require("./config.prod");
+const PROD_CONFIG = require("./config.prod");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
@@ -7,4 +7,4 @@ const AUDIT_CONFIG = {
   plugins: [new BundleAnalyzerPlugin({ openAnalyzer: false })],
 };
 
-module.exports = merge(BASE_CONFIG, AUDIT_CONFIG);
+module.exports = merge(PROD_CONFIG, AUDIT_CONFIG);
