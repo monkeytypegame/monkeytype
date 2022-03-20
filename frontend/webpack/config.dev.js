352a6fbfc0374d54500de7b1db6a4d0064c3afff
Miodec <bartnikjack@gmail.com>
2022-03-21 00:10:33 +0100

    using inline source map so that code is debuggable

--- frontend/webpack/config.dev.js
+++ frontend/webpack/config.dev.js
@@ -4,7 +4,7 @@ const BASE_CONFIG = require("./config.base");
 /** @type { import('webpack').Configuration } */
 const DEV_CONFIG = {
   mode: "development",
-  devtool: "eval",
+  devtool: "inline-source-map",
   devServer: {
     compress: true,
     port: 5000,
