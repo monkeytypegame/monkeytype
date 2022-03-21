ab00c5879912eddca6c63557418164dced36791f
Bruception <bberr022@fiu.edu>
2022-03-21 15:59:08 -0400

    Actually save version

--- backend/version.ts
+++ backend/version.ts
@@ -1,25 +1,33 @@
+import { join } from "path";
 import { padNumbers } from "./utils/misc";
+import { readFileSync, writeFileSync, existsSync } from "fs";
+
+const SERVER_VERSION_FILE_PATH = join(__dirname, "./server.version");
 
 function getDateVersion(): string {
   const date = new Date();
 
-  const versionPrefix = padNumbers(
-    [date.getFullYear(), date.getMonth() + 1, date.getDate()],
-    2,
-    "0"
-  ).join(".");
-  const versionSuffix = padNumbers(
-    [date.getHours(), date.getMinutes()],
-    2,
-    "0"
-  ).join(".");
+  const versionPrefix = [
+    date.getFullYear(),
+    date.getMonth() + 1,
+    date.getDate(),
+  ];
+  const versionSuffix = [date.getHours(), date.getMinutes()];
 
-  return [versionPrefix, versionSuffix].join("_");
+  return [versionPrefix, versionSuffix]
+    .map((versionPart) => padNumbers(versionPart, 2, "0").join("."))
+    .join("_");
 }
 
 function getVersion(): string {
   if (process.env.MODE !== "dev") {
-    return getDateVersion();
+    if (existsSync(SERVER_VERSION_FILE_PATH)) {
+      return readFileSync(SERVER_VERSION_FILE_PATH, "utf-8");
+    }
+
+    const serverVersion = getDateVersion();
+    writeFileSync(SERVER_VERSION_FILE_PATH, serverVersion);
+    return serverVersion;
   }
 
   return "DEVELOPMENT-VERSION";
