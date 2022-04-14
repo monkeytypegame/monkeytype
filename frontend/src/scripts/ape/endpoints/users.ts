7b4ce202732449a400279c6f53f2cc385f1238ef
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-04-14 22:40:31 +0500

    Renamed routes

--- frontend/src/scripts/ape/endpoints/users.ts
+++ frontend/src/scripts/ape/endpoints/users.ts
@@ -136,20 +136,20 @@ export default function getUsersEndpoints(
     return await apeClient.post(`${BASE_PATH}/discord/unlink`);
   }
 
-  async function addToFavorites(
+  async function addQuoteToFavorites(
     language: string,
     quoteId: string
   ): Ape.EndpointData {
     const payload = { language, quoteId };
-    return await apeClient.post(`${BASE_PATH}/favorites`, { payload });
+    return await apeClient.post(`${BASE_PATH}/favoriteQuotes`, { payload });
   }
 
-  async function removeFromFavorites(
+  async function removeQuoteFromFavorites(
     language: string,
     quoteId: string
   ): Ape.EndpointData {
     const payload = { language, quoteId };
-    return await apeClient.delete(`${BASE_PATH}/favorites`, { payload });
+    return await apeClient.delete(`${BASE_PATH}/favoriteQuotes`, { payload });
   }
 
   return {
@@ -172,7 +172,7 @@ export default function getUsersEndpoints(
     addCustomTheme,
     editCustomTheme,
     deleteCustomTheme,
-    addQuoteToFavorites: addToFavorites,
-    removeQuoteFromFavorites: removeFromFavorites,
+    addQuoteToFavorites,
+    removeQuoteFromFavorites,
   };
 }
