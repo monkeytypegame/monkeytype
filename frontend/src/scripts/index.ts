39e4d7a08105621877707f720372a00a83023191
Miodec <bartnikjack@gmail.com>
2022-04-22 21:40:10 +0200

    deleting account if popup was closed uncommented sign up process addd functions to enable and disable input

--- frontend/src/scripts/index.ts
+++ frontend/src/scripts/index.ts
@@ -26,6 +26,7 @@ import "./popups/pb-tables-popup";
 import "./elements/scroll-to-top";
 import "./popups/mobile-test-config-popup";
 import "./popups/edit-tags-popup";
+import "./popups/google-sign-up-popup";
 import * as Account from "./pages/account";
 
 type ExtendedGlobal = typeof globalThis & MonkeyTypes.Global;
