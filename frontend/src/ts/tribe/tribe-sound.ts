4e5122ee4d16f52451b8be8d3da33d9ee3cba877
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-04-27 15:22:52 +0500

    Update sound refs

--- frontend/src/ts/tribe/tribe-sound.ts
+++ frontend/src/ts/tribe/tribe-sound.ts
@@ -2,16 +2,16 @@ import { Howl } from "howler";
 import * as Tribe from "./tribe"; // Rizwan TODO: This should work as soon as tribe files wiill be converted to typescript
 
 const sounds: Record<string, Howl> = {
-  join: new Howl({ src: "../sound/tribe_ui/join.wav" }),
-  leave: new Howl({ src: "../sound/tribe_ui/leave.wav" }),
-  start: new Howl({ src: "../sound/tribe_ui/start.wav" }),
-  chat: new Howl({ src: "../sound/tribe_ui/chat.wav" }),
-  chat_mention: new Howl({ src: "../sound/tribe_ui/chat_mention.wav" }),
-  finish: new Howl({ src: "../sound/tribe_ui/finish.wav" }),
-  finish_win: new Howl({ src: "../sound/tribe_ui/finish_win.wav" }),
-  glow: new Howl({ src: "../sound/tribe_ui/glow.wav" }),
-  cd: new Howl({ src: "../sound/tribe_ui/cd2.wav" }),
-  cd_go: new Howl({ src: "../sound/tribe_ui/cd_go2.wav" }),
+  join: new Howl({ src: "./sounds/join.wav" }),
+  leave: new Howl({ src: "./sounds/leave.wav" }),
+  start: new Howl({ src: "./sounds/start.wav" }),
+  chat: new Howl({ src: "./sounds/chat.wav" }),
+  chat_mention: new Howl({ src: "./sounds/chat_mention.wav" }),
+  finish: new Howl({ src: "./sounds/finish.wav" }),
+  finish_win: new Howl({ src: "./sounds/finish_win.wav" }),
+  glow: new Howl({ src: "./sounds/glow.wav" }),
+  cd: new Howl({ src: "./sounds/cd2.wav" }),
+  cd_go: new Howl({ src: "./sounds/cd_go2.wav" }),
 };
 
 export function play(name: string): void {
