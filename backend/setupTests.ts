b1de2fd6d93a2ee9b2e798ae70dee8688ee7ad04
Sondre Kjempekjenn <sondre.kjempekjenn@gmail.com>
2022-05-04 18:33:18 +0200

    Basic controller test

--- backend/setupTests.ts
+++ backend/setupTests.ts
@@ -5,6 +5,45 @@ jest.mock("./init/db2", () => ({
   dbService: (): unknown => ({ getDb: () => db }),
 }));
 
+jest.mock("./utils/logger", () => ({
+  __esModule: true,
+  default: {
+    error: console.error,
+    warning: console.warn,
+    info: console.info,
+    success: console.info,
+    logToDb: console.info,
+  },
+}));
+
+jest.mock("swagger-stats", () => ({
+  getMiddleware:
+    () =>
+    (_: unknown, __: unknown, next: () => unknown): void => {
+      next();
+    },
+}));
+
+// TODO: better approach for this when needed
+// https://firebase.google.com/docs/rules/unit-tests#run_local_unit_tests_with_the_version_9_javascript_sdk
+jest.mock("firebase-admin", () => ({
+  __esModule: true,
+  default: {
+    auth: (): unknown => ({
+      verifyIdToken: (
+        _token: string,
+        _checkRevoked: boolean
+      ): unknown /* Promise<DecodedIdToken> */ =>
+        Promise.resolve({
+          aud: "mockFirebaseProjectId",
+          auth_time: 123,
+          exp: 1000,
+          uid: "mockUid",
+        }),
+    }),
+  },
+}));
+
 const collectionsForCleanUp = ["users"];
 
 let db: Db;
