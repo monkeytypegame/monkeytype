207562b785c267260417d315d873dbb9f2771c97
Miodec <bartnikjack@gmail.com>
2022-05-08 09:33:05 +0200

    refactor

--- frontend/src/ts/elements/commandline-lists.ts
+++ frontend/src/ts/elements/commandline-lists.ts
@@ -219,84 +219,81 @@ const commandsTags: MonkeyTypes.CommandsGroup = {
 };
 
 export function updateTagCommands(): void {
+  const snapshot = DB.getSnapshot();
   commandsTags.list = [];
-  if (DB.getSnapshot()?.tags?.length ?? 0 > 0) {
-    commandsTags.list.push({
-      id: "clearTags",
-      display: `Clear tags`,
-      icon: "fa-times",
-      exec: (): void => {
-        const snapshot = DB.getSnapshot();
+  if (!snapshot || !snapshot.tags || snapshot.tags.length === 0) return;
+  commandsTags.list.push({
+    id: "clearTags",
+    display: `Clear tags`,
+    icon: "fa-times",
+    exec: (): void => {
+      const snapshot = DB.getSnapshot();
 
-        snapshot.tags = snapshot.tags?.map((tag) => {
-          tag.active = false;
+      snapshot.tags = snapshot.tags?.map((tag) => {
+        tag.active = false;
 
-          return tag;
-        });
-
-        DB.setSnapshot(snapshot);
-        ModesNotice.update();
-        TagController.saveActiveToLocalStorage();
-      },
-    });
-
-    DB.getSnapshot().tags?.forEach((tag) => {
-      let dis = tag.display;
-
-      if (tag.active === true) {
-        dis = '<i class="fas fa-fw fa-check"></i>' + dis;
-      } else {
-        dis = '<i class="fas fa-fw"></i>' + dis;
-      }
-
-      commandsTags.list.push({
-        id: "toggleTag" + tag._id,
-        noIcon: true,
-        display: dis,
-        sticky: true,
-        exec: (): void => {
-          TagController.toggle(tag._id);
-          ModesNotice.update();
-
-          if (Config.paceCaret === "average") {
-            PaceCaret.init();
-            ModesNotice.update();
-          }
-
-          let txt = tag.display;
-
-          if (tag.active === true) {
-            txt = '<i class="fas fa-fw fa-check"></i>' + txt;
-          } else {
-            txt = '<i class="fas fa-fw"></i>' + txt;
-          }
-          if ($("#commandLine").hasClass("allCommands")) {
-            $(
-              `#commandLine .suggestions .entry[command='toggleTag${tag._id}']`
-            ).html(
-              `<div class="icon"><i class="fas fa-fw fa-tag"></i></div><div>Tags  > ` +
-                txt
-            );
-          } else {
-            $(
-              `#commandLine .suggestions .entry[command='toggleTag${tag._id}']`
-            ).html(txt);
-          }
-        },
+        return tag;
       });
-    });
-    // defaultCommands.list[4].visible = true;
-  }
-  if (Auth.currentUser) {
+
+      DB.setSnapshot(snapshot);
+      ModesNotice.update();
+      TagController.saveActiveToLocalStorage();
+    },
+  });
+
+  DB.getSnapshot().tags?.forEach((tag) => {
+    let dis = tag.display;
+
+    if (tag.active === true) {
+      dis = '<i class="fas fa-fw fa-check"></i>' + dis;
+    } else {
+      dis = '<i class="fas fa-fw"></i>' + dis;
+    }
+
     commandsTags.list.push({
-      id: "createTag",
-      display: "Create tag",
-      icon: "fa-plus",
+      id: "toggleTag" + tag._id,
+      noIcon: true,
+      display: dis,
+      sticky: true,
       exec: (): void => {
-        EditTagPopup.show("add");
+        TagController.toggle(tag._id);
+        ModesNotice.update();
+
+        if (Config.paceCaret === "average") {
+          PaceCaret.init();
+          ModesNotice.update();
+        }
+
+        let txt = tag.display;
+
+        if (tag.active === true) {
+          txt = '<i class="fas fa-fw fa-check"></i>' + txt;
+        } else {
+          txt = '<i class="fas fa-fw"></i>' + txt;
+        }
+        if ($("#commandLine").hasClass("allCommands")) {
+          $(
+            `#commandLine .suggestions .entry[command='toggleTag${tag._id}']`
+          ).html(
+            `<div class="icon"><i class="fas fa-fw fa-tag"></i></div><div>Tags  > ` +
+              txt
+          );
+        } else {
+          $(
+            `#commandLine .suggestions .entry[command='toggleTag${tag._id}']`
+          ).html(txt);
+        }
       },
     });
-  }
+  });
+  commandsTags.list.push({
+    id: "createTag",
+    display: "Create tag",
+    icon: "fa-plus",
+    exec: (): void => {
+      EditTagPopup.show("add");
+    },
+  });
 }
 
 const commandsPresets: MonkeyTypes.CommandsGroup = {
