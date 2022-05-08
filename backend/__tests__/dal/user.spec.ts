0a4a7eabe780df6b631a23bcbf7013e7c053f65e
Miodec <bartnikjack@gmail.com>
2022-05-08 14:54:15 +0200

    added test to make sure users cant take an unavailable name

--- backend/__tests__/dal/user.spec.ts
+++ backend/__tests__/dal/user.spec.ts
@@ -1,4 +1,4 @@
-import { addUser, getUser } from "../../src/dal/user";
+import { addUser, getUser, updateName } from "../../src/dal/user";
 
 describe("UserDal", () => {
   it("should be able to insert users", async () => {
@@ -29,5 +29,24 @@ describe("UserDal", () => {
     await expect(
       addUser(newUser.name, newUser.email, newUser.uid)
     ).rejects.toThrow("User document already exists");
+  });
+
+  it("updatename should not allow unavailable usernames", async () => {
+    const mockUsers = [...Array(3).keys()]
+      .map((id) => ({
+        name: `Test${id}`,
+        email: `mockemail@email.com${id}`,
+        uid: `userId${id}`,
+      }))
+      .map(({ name, email, uid }) => addUser(name, email, uid));
+
+    await Promise.all(mockUsers);
+
+    const userToUpdateNameFor = await getUser("userId0", "test");
+    const userWithNameTaken = await getUser("userId1", "test");
+
+    await expect(
+      updateName(userToUpdateNameFor.uid, userWithNameTaken.name)
+    ).rejects.toThrow("Username already taken");
   });
 });
