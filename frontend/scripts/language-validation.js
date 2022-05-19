b68d355eae1cc97e052d3ef867e837e54baf14e5
Maximilian Kronenbürger <maximilian.kronenbuerger@1und1.de>
2022-05-19 16:18:27 +0200

    run prettier

--- frontend/scripts/language-validation.js
+++ frontend/scripts/language-validation.js
@@ -1,10 +1,9 @@
 const fs = require("fs");
 
+const MAX_WORDS_LENGTH = 10000;
+const DUPLICATE_PERCENTAGE_QUALITY_GATE = 10;
 
-const MAX_WORDS_LENGTH = 10000
-const DUPLICATE_PERCENTAGE_QUALITY_GATE = 15
-
-function findDuplicates (arr) {
+function findDuplicates(arr) {
   return arr.filter((item, index) => {
     let indexOf = arr.indexOf(item.toLowerCase());
     return indexOf !== index && indexOf !== -1;
@@ -18,8 +17,11 @@ function validateLanguageContents(languageNameFilter) {
         encoding: "utf8",
         flag: "r",
       })
-    ).filter(language => languageNameFilter === undefined || language.startsWith(languageNameFilter));
-
+    ).filter(
+      (language) =>
+        languageNameFilter === undefined ||
+        language.startsWith(languageNameFilter)
+    );
 
     const results = [];
     let allLanguageFilesOk = true;
@@ -35,21 +37,27 @@ function validateLanguageContents(languageNameFilter) {
       // Skip the validation of large language lists
       if (languageFileData.words.length > MAX_WORDS_LENGTH) return;
 
-
       let duplicates = findDuplicates(languageFileData.words);
 
-      let duplicatePercentage = Math.round(duplicates.length / languageFileData.words.length * 100)
+      let duplicatePercentage = Math.round(
+        (duplicates.length / languageFileData.words.length) * 100
+      );
       if (duplicatePercentage > DUPLICATE_PERCENTAGE_QUALITY_GATE) {
-        console.warn(`Language '${languageFileData.name}' may contain ${duplicates.length} (${duplicatePercentage}%) duplicate(s):`);
-        console.warn(duplicates)
-        results.push({'name': languageFileData.name, 'duplicates': duplicates})
+        console.warn(
+          `Language '${languageFileData.name}' may contain ${duplicates.length} (${duplicatePercentage}%) duplicate(s):`
+        );
+        console.warn(duplicates);
+        results.push({ name: languageFileData.name, duplicates: duplicates });
         allLanguageFilesOk = false;
       }
-
     });
 
-    if(!allLanguageFilesOk) {
-      return reject(new Error(`Detected ${results.length} languages with duplicate percentage over ${DUPLICATE_PERCENTAGE_QUALITY_GATE}% !`));
+    if (!allLanguageFilesOk) {
+      return reject(
+        new Error(
+          `Detected ${results.length} languages with duplicate percentage over ${DUPLICATE_PERCENTAGE_QUALITY_GATE}% !`
+        )
+      );
     }
 
     resolve();
