30cf2db1c6b55a77a308ca0fbffaaef825a59ccf
Miodec <bartnikjack@gmail.com>
2022-05-23 20:42:43 +0200

    fixed charts (commented some stuff temporairly)

--- frontend/src/ts/tribe/tribe-chart-controller.ts
+++ frontend/src/ts/tribe/tribe-chart-controller.ts
@@ -1,11 +1,12 @@
-import ChartJS from "chart.js";
+import { Chart, ChartConfiguration } from "chart.js";
 import * as Tribe from "./tribe";
 import * as ThemeColors from "../elements/theme-colors";
 import * as Notifications from "../elements/notifications";
+import { createErrorMessage } from "../utils/misc";
 
-const charts: Record<string, unknown> = {};
+const charts: Record<string, Chart> = {};
 
-const settings = {
+const settings: ChartConfiguration = {
   type: "line",
   data: {
     labels: [1, 2, 3],
@@ -36,17 +37,16 @@ const settings = {
         borderWidth: 1,
         order: 1,
         yAxisID: "error",
-        maxBarThickness: 10,
         type: "scatter",
         pointStyle: "crossRot",
-        radius: function (context): 0 | 2 {
+        pointRadius: function (context): 0 | 2 {
           const index = context.dataIndex;
-          const value = context.dataset.data[index];
+          const value = context.dataset.data[index] ?? 0;
           return value <= 0 ? 0 : 2;
         },
         pointHoverRadius: function (context): 0 | 3 {
           const index = context.dataIndex;
-          const value = context.dataset.data[index];
+          const value = context.dataset.data[index] ?? 0;
           return value <= 0 ? 0 : 3;
         },
       },
@@ -61,197 +61,191 @@ const settings = {
         bottom: 5,
       },
     },
-    tooltips: {
-      titleFontFamily: "Roboto Mono",
-      bodyFontFamily: "Roboto Mono",
-      mode: "index",
-      intersect: false,
-      enabled: false,
-      custom: function (tooltipModel) {
-        // Tooltip Element
-        let tooltipEl = document.getElementById("tribeMiniChartCustomTooltip");
+    // tooltips: {
+    //   titleFontFamily: "Roboto Mono",
+    //   bodyFontFamily: "Roboto Mono",
+    //   mode: "index",
+    //   intersect: false,
+    //   enabled: false,
+    //   custom: function (tooltipModel: unknown) {
+    //     // Tooltip Element
+    //     let tooltipEl = document.getElementById("tribeMiniChartCustomTooltip");
 
-        // Create element on first render
-        if (!tooltipEl) {
-          tooltipEl = document.createElement("div");
-          tooltipEl.id = "tribeMiniChartCustomTooltip";
-          tooltipEl.innerHTML = "<div></div>";
-          document.body.appendChild(tooltipEl);
-        }
+    //     // Create element on first render
+    //     if (!tooltipEl) {
+    //       tooltipEl = document.createElement("div");
+    //       tooltipEl.id = "tribeMiniChartCustomTooltip";
+    //       tooltipEl.innerHTML = "<div></div>";
+    //       document.body.appendChild(tooltipEl);
+    //     }
 
-        // Hide if no tooltip
-        if (tooltipModel.opacity === 0) {
-          tooltipEl.style.opacity = "0";
-          return;
-        }
+    //     // Hide if no tooltip
+    //     if (tooltipModel.opacity === 0) {
+    //       tooltipEl.style.opacity = "0";
+    //       return;
+    //     }
 
-        // Set caret Position
-        tooltipEl.classList.remove("above", "below", "no-transform");
-        if (tooltipModel.yAlign) {
-          tooltipEl.classList.add(tooltipModel.yAlign);
-        } else {
-          tooltipEl.classList.add("no-transform");
-        }
+    //     // Set caret Position
+    //     tooltipEl.classList.remove("above", "below", "no-transform");
+    //     if (tooltipModel.yAlign) {
+    //       tooltipEl.classList.add(tooltipModel.yAlign);
+    //     } else {
+    //       tooltipEl.classList.add("no-transform");
+    //     }
 
-        function getBody(bodyItem) {
-          return bodyItem.lines;
-        }
+    //     function getBody(bodyItem) {
+    //       return bodyItem.lines;
+    //     }
 
-        // Set Text
-        if (tooltipModel.body) {
-          const titleLines = tooltipModel.title || [];
-          const bodyLines = tooltipModel.body.map(getBody);
+    //     // Set Text
+    //     if (tooltipModel.body) {
+    //       const titleLines = tooltipModel.title || [];
+    //       const bodyLines = tooltipModel.body.map(getBody);
 
-          let innerHtml = "";
+    //       let innerHtml = "";
 
-          titleLines.forEach(function (title: string) {
-            innerHtml += "<div>" + title + "</div>";
-          });
-          // innerHtml += '</thead><tbody>';
+    //       titleLines.forEach(function (title: string) {
+    //         innerHtml += "<div>" + title + "</div>";
+    //       });
+    //       // innerHtml += '</thead><tbody>';
 
-          bodyLines.forEach(function (body, _i) {
-            // var colors = tooltipModel.labelColors[i];
-            // var style = 'background:' + colors.backgroundColor;
-            // style += '; border-color:' + colors.borderColor;
-            // style += '; border-width: 2px';
-            // var span = '<span style="' + style + '"></span>';
-            innerHtml += "<div>" + body + "</div>";
-            // innerHtml += '<tr><td>' + span + body + '</td></tr>';
-          });
-          // innerHtml += '</tbody>';
+    //       bodyLines.forEach(function (body, _i) {
+    //         // var colors = tooltipModel.labelColors[i];
+    //         // var style = 'background:' + colors.backgroundColor;
+    //         // style += '; border-color:' + colors.borderColor;
+    //         // style += '; border-width: 2px';
+    //         // var span = '<span style="' + style + '"></span>';
+    //         innerHtml += "<div>" + body + "</div>";
+    //         // innerHtml += '<tr><td>' + span + body + '</td></tr>';
+    //       });
+    //       // innerHtml += '</tbody>';
 
-          const tableRoot = tooltipEl.querySelector("div");
-          tableRoot.innerHTML = innerHtml;
-        }
+    //       const tableRoot = tooltipEl.querySelector("div");
+    //       tableRoot.innerHTML = innerHtml;
+    //     }
 
-        // `this` will be the overall tooltip
-        const position = this._chart.canvas.getBoundingClientRect();
+    //     // `this` will be the overall tooltip
+    //     const position = this._chart.canvas.getBoundingClientRect();
 
-        // Display, position, and set styles for font
-        tooltipEl.style.opacity = "1";
-        tooltipEl.style.position = "absolute";
-        tooltipEl.style.left =
-          position.left +
-          window.pageXOffset +
-          tooltipModel.caretX -
-          tooltipEl.offsetWidth +
-          "px";
-        tooltipEl.style.top =
-          position.top + window.pageYOffset + tooltipModel.caretY + "px";
-        // tooltipEl.style.fontFamily = tooltipModel._bodyFontFamily;
-        tooltipEl.style.fontSize = "0.75rem";
-        tooltipEl.style.fontStyle = tooltipModel._bodyFontStyle;
-        tooltipEl.style.padding =
-          tooltipModel.yPadding + "px " + tooltipModel.xPadding + "px";
-        tooltipEl.style.pointerEvents = "none";
-        tooltipEl.style.background = "rgba(0,0,0,.75)";
-        tooltipEl.style.borderRadius = "0.5rem";
-        tooltipEl.style.color = "white";
-        tooltipEl.style.zIndex = "999";
-        tooltipEl.style.transition = "left 0.25s, top 0.25s, opacity 0.25s";
-      },
-    },
-    legend: {
-      display: false,
-      labels: {
-        defaultFontFamily: "Roboto Mono",
-      },
-    },
+    //     // Display, position, and set styles for font
+    //     tooltipEl.style.opacity = "1";
+    //     tooltipEl.style.position = "absolute";
+    //     tooltipEl.style.left =
+    //       position.left +
+    //       window.pageXOffset +
+    //       tooltipModel.caretX -
+    //       tooltipEl.offsetWidth +
+    //       "px";
+    //     tooltipEl.style.top =
+    //       position.top + window.pageYOffset + tooltipModel.caretY + "px";
+    //     // tooltipEl.style.fontFamily = tooltipModel._bodyFontFamily;
+    //     tooltipEl.style.fontSize = "0.75rem";
+    //     tooltipEl.style.fontStyle = tooltipModel._bodyFontStyle;
+    //     tooltipEl.style.padding =
+    //       tooltipModel.yPadding + "px " + tooltipModel.xPadding + "px";
+    //     tooltipEl.style.pointerEvents = "none";
+    //     tooltipEl.style.background = "rgba(0,0,0,.75)";
+    //     tooltipEl.style.borderRadius = "0.5rem";
+    //     tooltipEl.style.color = "white";
+    //     tooltipEl.style.zIndex = "999";
+    //     tooltipEl.style.transition = "left 0.25s, top 0.25s, opacity 0.25s";
+    //   },
+    // },
+    // legend: {
+    //   display: false,
+    //   labels: {
+    //     defaultFontFamily: "Roboto Mono",
+    //   },
+    // },
     responsive: true,
     maintainAspectRatio: false,
     scales: {
-      xAxes: [
-        {
-          ticks: {
-            fontFamily: "Roboto Mono",
-            autoSkip: true,
-            autoSkipPadding: 40,
-            display: false,
-          },
-          display: true,
-          scaleLabel: {
-            display: false,
-            labelString: "Seconds",
-            fontFamily: "Roboto Mono",
-          },
-          gridLines: {
-            display: true,
-            drawTicks: false,
-            tickMarkLength: 0,
-          },
-        },
-      ],
-      yAxes: [
-        {
-          id: "wpm",
-          display: true,
-          scaleLabel: {
-            display: false,
-            labelString: "Words per Minute",
-            fontFamily: "Roboto Mono",
-          },
-          ticks: {
-            fontFamily: "Roboto Mono",
-            beginAtZero: true,
-            min: 0,
-            autoSkip: true,
-            autoSkipPadding: 40,
-            display: false,
-          },
-          gridLines: {
-            display: true,
-            drawTicks: false,
-            tickMarkLength: 0,
-          },
-        },
-        {
-          id: "raw",
+      x: {
+        axis: "x",
+        ticks: {
+          // fontFamily: "Roboto Mono",
+          autoSkip: true,
+          autoSkipPadding: 40,
           display: false,
-          scaleLabel: {
-            display: true,
-            labelString: "Raw Words per Minute",
-            fontFamily: "Roboto Mono",
-          },
-          ticks: {
-            fontFamily: "Roboto Mono",
-            beginAtZero: true,
-            min: 0,
-            autoSkip: true,
-            autoSkipPadding: 40,
-          },
-          gridLines: {
-            display: false,
-          },
         },
-        {
-          id: "error",
+        display: true,
+        // scaleLabel: {
+        //   display: false,
+        //   labelString: "Seconds",
+        //   fontFamily: "Roboto Mono",
+        // },
+        grid: {
+          display: true,
+          drawTicks: false,
+          tickLength: 0,
+        },
+      },
+      wpm: {
+        axis: "y",
+        display: true,
+        // scaleLabel: {
+        //   display: false,
+        //   labelString: "Words per Minute",
+        //   fontFamily: "Roboto Mono",
+        // },
+        ticks: {
+          // fontFamily: "Roboto Mono",
+          // beginAtZero: true,
+          // min: 0,
+          autoSkip: true,
+          autoSkipPadding: 40,
           display: false,
-          position: "right",
-          scaleLabel: {
-            display: true,
-            labelString: "Errors",
-            fontFamily: "Roboto Mono",
-          },
-          ticks: {
-            precision: 0,
-            fontFamily: "Roboto Mono",
-            beginAtZero: true,
-            autoSkip: true,
-            autoSkipPadding: 40,
-          },
-          gridLines: {
-            display: false,
-          },
         },
-      ],
-    },
-    annotation: {
-      annotations: [],
+        grid: {
+          display: true,
+          drawTicks: false,
+          // tickMarkLength: 0,
+        },
+      },
+      raw: {
+        axis: "y",
+        display: false,
+        // scaleLabel: {
+        //   display: true,
+        //   labelString: "Raw Words per Minute",
+        //   fontFamily: "Roboto Mono",
+        // },
+        ticks: {
+          // fontFamily: "Roboto Mono",
+          // beginAtZero: true,
+          // min: 0,
+          autoSkip: true,
+          autoSkipPadding: 40,
+        },
+        grid: {
+          display: false,
+        },
+      },
+      error: {
+        axis: "y",
+        display: false,
+        position: "right",
+        // scaleLabel: {
+        //   display: true,
+        //   labelString: "Errors",
+        //   fontFamily: "Roboto Mono",
+        // },
+        ticks: {
+          // precision: 0,
+          // fontFamily: "Roboto Mono",
+          // beginAtZero: true,
+          autoSkip: true,
+          autoSkipPadding: 40,
+        },
+        grid: {
+          display: false,
+        },
+      },
     },
   },
 };
 
-async function fillData(chart, userId: string): Promise<void> {
+async function fillData(chart: Chart, userId: string): Promise<void> {
   const labels: number[] = [];
   if (!Tribe.room) return;
   const result = Tribe.room.users[userId].result;
@@ -259,41 +253,44 @@ async function fillData(chart, userId: string): Promise<void> {
     labels.push(i);
   }
 
-  const chartmaxval = Math.max(
-    Math.max(...result.chartData.wpm),
-    Math.max(...result.chartData.raw)
-  );
+  // const chartmaxval = Math.max(
+  //   Math.max(...result.chartData.wpm),
+  //   Math.max(...result.chartData.raw)
+  // );
 
   chart.data.labels = labels;
   chart.data.datasets[0].data = result.chartData.wpm;
   chart.data.datasets[1].data = result.chartData.raw;
   chart.data.datasets[2].data = result.chartData.err;
 
-  chart.options.scales.yAxes[0].ticks.max = Math.round(chartmaxval);
-  chart.options.scales.yAxes[1].ticks.max = Math.round(chartmaxval);
+  // chart.options.scales["wpm"].ticks.max = Math.round(chartmaxval);
+  // chart.options.scales["raw"].ticks.max = Math.round(chartmaxval);
 
   if (userId == Tribe.socket.id) {
     chart.data.datasets[0].borderColor = await ThemeColors.get("main");
-    chart.data.datasets[0].pointBackgroundColor = await ThemeColors.get("main");
+    chart.data.datasets[0].backgroundColor = await ThemeColors.get("main");
   } else {
     chart.data.datasets[0].borderColor = await ThemeColors.get("text");
-    chart.data.datasets[0].pointBackgroundColor = await ThemeColors.get("text");
+    chart.data.datasets[0].backgroundColor = await ThemeColors.get("text");
   }
   chart.data.datasets[1].borderColor = await ThemeColors.get("sub");
-  chart.data.datasets[1].pointBackgroundColor = await ThemeColors.get("sub");
+  chart.data.datasets[1].backgroundColor = await ThemeColors.get("sub");
 
-  chart.update({ duration: 0 });
+  chart.update();
 }
 
 export async function drawChart(userId: string): Promise<void> {
   try {
     const element = $(
       `.pageTest #result #tribeResults table tbody tr#${userId} .minichart canvas`
-    )[0];
+    )[0] as HTMLCanvasElement;
 
     if (!Tribe.room || !Tribe.room.users[userId].result) return;
 
-    const chart = new ChartJS(element, $.extend(true, {}, settings));
+    const chart = new Chart(
+      element,
+      $.extend(true, {}, settings) as ChartConfiguration
+    );
 
     await fillData(chart, userId);
 
@@ -306,7 +303,7 @@ export async function drawChart(userId: string): Promise<void> {
     ).addClass("hidden");
     return;
   } catch (e) {
-    Notifications.add("Error drawing mini chart: " + e.message, -1);
+    Notifications.add(createErrorMessage(e, "Error drawing mini chart"), -1);
     return;
   }
 }
@@ -339,17 +336,17 @@ export async function updateChartMaxValues(): Promise<void> {
       maxRaw = maxUserRaw;
     }
   }
-  const chartmaxval = Math.max(maxWpm, maxRaw);
+  // const chartmaxval = Math.max(maxWpm, maxRaw);
 
   const list = Object.keys(Tribe.room.users);
   for (let i = 0; i < list.length; i++) {
     const userId = list[i];
     if (charts[userId]) {
-      charts[userId].options.scales.yAxes[0].ticks.max =
-        Math.round(chartmaxval);
-      charts[userId].options.scales.yAxes[1].ticks.max =
-        Math.round(chartmaxval);
-      await charts[userId].update({ duration: 0 });
+      // charts[userId].options.scales.yAxes[0].ticks.max =
+      //   Math.round(chartmaxval);
+      // charts[userId].options.scales.yAxes[1].ticks.max =
+      //   Math.round(chartmaxval);
+      await charts[userId].update();
     }
   }
 }
