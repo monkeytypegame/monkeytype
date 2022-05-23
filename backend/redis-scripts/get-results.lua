5eb6a40d4c740682ecc87ef947c5a3a409733021
Bruception <bberr022@fiu.edu>
2022-05-23 14:30:37 -0400

    Remove with scores

--- backend/redis-scripts/get-results.lua
+++ backend/redis-scripts/get-results.lua
@@ -5,7 +5,7 @@ local min_rank = tonumber(ARGV[1])
 local max_rank = tonumber(ARGV[2])
 
 local results = {}
-local scores_in_range = redis_call('ZRANGE', leaderboard_scores_key, min_rank, max_rank, 'REV', 'WITHSCORES')
+local scores_in_range = redis_call('ZRANGE', leaderboard_scores_key, min_rank, max_rank, 'REV')
 
 for _, user_id in ipairs(scores_in_range) do
     local result_data = redis_call('HGET', leaderboard_results_key, user_id)
