2513f028b633d82108c613e4141a8104a2e6d374
Evan <64989416+Ferotiq@users.noreply.github.com>
2022-05-24 10:50:29 -0500

    Update custom-text.ts

--- frontend/src/ts/test/custom-text.ts
+++ frontend/src/ts/test/custom-text.ts
@@ -39,9 +39,7 @@ export function setDelimiter(val: string): void {
   delimiter = val;
 }
 
-interface CustomTextObject {
-  [key: string]: string;
-}
+type CustomTextObject = Record<string, string>;
 
 export function getCustomText(name: string): string[] {
   const customText = getCustomTextObject();
