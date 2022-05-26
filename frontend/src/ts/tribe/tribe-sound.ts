2e1846b71a4016a2fc4ad5a7dffcedd0319ef056
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-05-26 19:41:05 +0500

    Fix sounds disappearing

--- frontend/src/ts/tribe/tribe-sound.ts
+++ frontend/src/ts/tribe/tribe-sound.ts
@@ -2,16 +2,16 @@ import { Howl } from "howler";
 import * as Tribe from "./tribe"; // Rizwan TODO: This should work as soon as tribe files wiill be converted to typescript
 
 const sounds: Record<string, Howl> = {
-  join: new Howl({ src: "./sounds/join.wav" }),
-  leave: new Howl({ src: "./sounds/leave.wav" }),
-  start: new Howl({ src: "./sounds/start.wav" }),
-  chat: new Howl({ src: "./sounds/chat.wav" }),
-  chat_mention: new Howl({ src: "./sounds/chat_mention.wav" }),
-  finish: new Howl({ src: "./sounds/finish.wav" }),
-  finish_win: new Howl({ src: "./sounds/finish_win.wav" }),
-  glow: new Howl({ src: "./sounds/glow.wav" }),
-  cd: new Howl({ src: "./sounds/cd2.wav" }),
-  cd_go: new Howl({ src: "./sounds/cd_go2.wav" }),
+  join: new Howl({ src: "/sound/tribe-sounds/join.wav" }),
+  leave: new Howl({ src: "/sound/tribe-sounds/leave.wav" }),
+  start: new Howl({ src: "/sound/tribe-sounds/start.wav" }),
+  chat: new Howl({ src: "/sound/tribe-sounds/chat.wav" }),
+  chat_mention: new Howl({ src: "/sound/tribe-sounds/chat_mention.wav" }),
+  finish: new Howl({ src: "/sound/tribe-sounds/finish.wav" }),
+  finish_win: new Howl({ src: "./sound/tribe-ounds/finish_win.wav" }),
+  glow: new Howl({ src: "/sound/tribe-sounds/glow.wav" }),
+  cd: new Howl({ src: "/sound/tribe-sounds/cd2.wav" }),
+  cd_go: new Howl({ src: "/sound/tribe-sounds/cd_go2.wav" }),
 };
 
 export function play(name: string): void {
