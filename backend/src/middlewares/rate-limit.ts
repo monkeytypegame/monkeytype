97d5c259c126d5b43adb8997439f424f012a71a4
Malo Hamon <hamon.malo@gmail.com>
2022-06-11 16:43:18 +1000

    Add and Remove functions for `ResultFilters` in user controller

--- backend/src/middlewares/rate-limit.ts
+++ backend/src/middlewares/rate-limit.ts
@@ -249,6 +249,20 @@ export const userClearPB = rateLimit({
   handler: customHandler,
 });
 
+export const userCustomFilterAdd = rateLimit({
+  windowMs: ONE_HOUR,
+  max: 60 * REQUEST_MULTIPLIER,
+  keyGenerator: getAddress,
+  handler: customHandler,
+});
+
+export const userCustomFilterRemove = rateLimit({
+  windowMs: ONE_HOUR,
+  max: 60 * REQUEST_MULTIPLIER,
+  keyGenerator: getAddress,
+  handler: customHandler,
+});
+
 export const userTagsGet = rateLimit({
   windowMs: ONE_HOUR,
   max: 60 * REQUEST_MULTIPLIER,
