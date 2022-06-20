629c15941bccbb7be731a3f7c563b78149b10e95
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-06-20 15:43:27 +0500

    Fix webpack hot reload not working on html file changes. (#3167) rizwanmustafa devkennyy
    * Modify config to allow hot reload on html
    
    * Now inject monkeytype.js bundle only in development mode
    
    * Remove asset injunction in production

--- frontend/webpack/config.dev.js
+++ frontend/webpack/config.dev.js
@@ -1,5 +1,22 @@
+const { resolve } = require("path");
 const { merge } = require("webpack-merge");
 const BASE_CONFIG = require("./config.base");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugins = [
+  "terms-of-service",
+  "security-policy",
+  "privacy-policy",
+  "email-handler",
+  "das",
+].map((name) => {
+  return new HtmlWebpackPlugin({
+    filename: `${name}.html`,
+    template: resolve(__dirname, `../static/${name}.html`),
+    inject: "body",
+    cache: false,
+  });
+});
 
 /** @type { import('webpack').Configuration } */
 const DEV_CONFIG = {
@@ -9,11 +26,15 @@ const DEV_CONFIG = {
     compress: true,
     port: 3000,
     open: true,
+    hot: false,
+    liveReload: true,
     historyApiFallback: true,
     client: {
       overlay: false,
     },
   },
+
+  plugins: htmlWebpackPlugins,
 };
 
 module.exports = merge(BASE_CONFIG, DEV_CONFIG);
