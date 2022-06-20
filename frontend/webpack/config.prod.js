6668575ceaafc7637b4574198cb797ab22de7a48
Bruce Berrios <58147810+Bruception@users.noreply.github.com>
2022-06-20 09:06:22 -0400

    Refactor webpack configs (#3170)
    * Refactor webpack configs
    
    * Remove unused import

--- frontend/webpack/config.prod.js
+++ frontend/webpack/config.prod.js
@@ -3,23 +3,8 @@ const { merge } = require("webpack-merge");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
 const JsonMinimizerPlugin = require("json-minimizer-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const BASE_CONFIG = require("./config.base");
-
-const htmlWebpackPlugins = [
-  "terms-of-service",
-  "security-policy",
-  "privacy-policy",
-  "email-handler",
-  "das",
-].map((name) => {
-  return new HtmlWebpackPlugin({
-    filename: `${name}.html`,
-    template: resolve(__dirname, `../static/${name}.html`),
-    inject: false,
-  });
-});
 
 function pad(numbers, maxLength, fillString) {
   return numbers.map((number) =>
@@ -79,7 +64,6 @@ const PRODUCTION_CONFIG = {
       new CssMinimizerPlugin(),
     ],
   },
-  plugins: htmlWebpackPlugins,
 };
 
 module.exports = merge(BASE_CONFIG, PRODUCTION_CONFIG);
