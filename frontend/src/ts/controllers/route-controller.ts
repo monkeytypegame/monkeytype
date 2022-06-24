caa0fb75e99dd086b0d1951e444c7106a8be3764
Miodec <bartnikjack@gmail.com>
2022-06-24 20:30:21 +0200

    removed comments

--- frontend/src/ts/controllers/route-controller.ts
+++ frontend/src/ts/controllers/route-controller.ts
@@ -7,63 +7,9 @@ import * as PageAccount from "../pages/account";
 import * as PageLogin from "../pages/login";
 import * as Page404 from "../pages/404";
 import * as PageProfile from "../pages/profile";
-
 import * as Leaderboards from "../elements/leaderboards";
-// import Config from "../config";
 import * as ActivePage from "../states/active-page";
 import { Auth } from "../firebase";
-// import { Auth } from "../firebase";
-
-// const mappedRoutes = {
-//   "/": "pageLoading",
-//   "/login": "pageLoading",
-//   "/settings": "pageLoading",
-//   "/about": "pageLoading",
-//   "/account": "pageAccount",
-//   "/verify": "pageLoading",
-//   "/profile": "pageLoading",
-// };
-
-// export function handleInitialPageClasses(pathname: string): void {
-//   if (!mappedRoutes[pathname as keyof typeof mappedRoutes]) {
-//     pathname = "/";
-//   }
-//   const el = $(".page." + mappedRoutes[pathname as keyof typeof mappedRoutes]);
-//   $(el).removeClass("hidden");
-//   $(el).addClass("active");
-//   let pageName = "loading";
-//   if (pathname === "/account") pageName = "account";
-//   ActivePage.set(pageName as MonkeyTypes.Page);
-// }
-
-// honestly im not sure what this does
-// (function (history): void {
-//   const pushState = history.pushState;
-//   history.pushState = function (state): void {
-//     if (Config.funbox === "memory" && state !== "/") {
-//       Funbox.resetMemoryTimer();
-//     }
-//     // @ts-ignore
-//     return pushState.apply(history, arguments);
-//   };
-// })(window.history);
-
-// $(window).on("popstate", (e) => {
-//   const state = (e.originalEvent as unknown as PopStateEvent).state;
-//   if (state == "" || state == "/") {
-//     // show test
-//     PageController.change("test");
-//   } else if (state == "about") {
-//     // show about
-//     PageController.change("about");
-//   } else if (state === "account" || state === "login") {
-//     if (Auth.currentUser) {
-//       PageController.change("account");
-//     } else {
-//       PageController.change("login");
-//     }
-//   }
-// });
 
 //source: https://www.youtube.com/watch?v=OstALBk-jTc
 // https://www.youtube.com/watch?v=OstALBk-jTc
