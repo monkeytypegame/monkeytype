99e84645d374bc6e6186f9a4438aa134f4a6f0e1
YummyOreo <bobgim20@gmail.com>
2022-06-23 21:41:08 -0500

    unit tests ape key deletion

--- backend/__tests__/dal/ape-keys.spec.ts
+++ backend/__tests__/dal/ape-keys.spec.ts
@@ -1,5 +1,5 @@
 import { ObjectId } from "mongodb";
-import { addApeKey } from "../../src/dal/ape-keys";
+import { addApeKey, deleteAllApeKeys } from "../../src/dal/ape-keys";
 
 describe("ApeKeysDal", () => {
   it("should be able to add a new ape key", async () => {
@@ -18,5 +18,38 @@ describe("ApeKeysDal", () => {
     const apeKeyId = await addApeKey(apeKey);
 
     expect(apeKeyId).toBe(apeKey._id.toHexString());
+
+    const apeKeys = [
+      {
+        _id: new ObjectId(),
+        uid: "123",
+        name: "test",
+        hash: "11111",
+        createdOn: Date.now(),
+        modifiedOn: Date.now(),
+        lastUsedOn: Date.now(),
+        useCount: 0,
+        enabled: true,
+      },
+      {
+        _id: new ObjectId(),
+        uid: "123",
+        name: "test",
+        hash: "11113",
+        createdOn: Date.now(),
+        modifiedOn: Date.now(),
+        lastUsedOn: Date.now(),
+        useCount: 0,
+        enabled: true,
+      },
+    ];
+
+    const apeKeyId0 = await addApeKey(apeKeys[0]);
+    const apeKeyId1 = await addApeKey(apeKeys[1]);
+
+    await deleteAllApeKeys("123");
+
+    expect(apeKeyId0).toBe(undefined);
+    expect(apeKeyId1).toBe(undefined);
   });
 });
