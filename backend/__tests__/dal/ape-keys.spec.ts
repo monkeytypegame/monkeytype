2794256bbe9978d9dd9dfc0d9605952893127d87
YummyOreo <bobgim20@gmail.com>
2022-07-01 06:16:52 -0500

    removes the delete all for ape unit test

--- backend/__tests__/dal/ape-keys.spec.ts
+++ backend/__tests__/dal/ape-keys.spec.ts
@@ -1,9 +1,5 @@
 import { ObjectId } from "mongodb";
-import {
-  addApeKey,
-  deleteAll,
-  getApeKeysCollection,
-} from "../../src/dal/ape-keys";
+import { addApeKey } from "../../src/dal/ape-keys";
 
 describe("ApeKeysDal", () => {
   it("should be able to add a new ape key", async () => {
@@ -22,42 +18,5 @@ describe("ApeKeysDal", () => {
     const apeKeyId = await addApeKey(apeKey);
 
     expect(apeKeyId).toBe(apeKey._id.toHexString());
-  });
-  it("should be able to delete all ape keys", async () => {
-    const apeKeys = [
-      {
-        _id: new ObjectId(),
-        uid: "123",
-        name: "test",
-        hash: "11111",
-        createdOn: Date.now(),
-        modifiedOn: Date.now(),
-        lastUsedOn: Date.now(),
-        useCount: 0,
-        enabled: true,
-      },
-      {
-        _id: new ObjectId(),
-        uid: "123",
-        name: "test",
-        hash: "11113",
-        createdOn: Date.now(),
-        modifiedOn: Date.now(),
-        lastUsedOn: Date.now(),
-        useCount: 0,
-        enabled: true,
-      },
-    ];
-
-    const results = apeKeys.map(addApeKey);
-
-    await Promise.all(results);
-
-    await deleteAll("123");
-
-    const exists =
-      (await getApeKeysCollection().countDocuments({ uid: "123" })) > 0;
-
-    expect(exists).toBe(false);
   });
 });
