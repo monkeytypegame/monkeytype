38ab0a7e2ab88af40be605b969d82d98fe110cca
Bruception <bberr022@fiu.edu>
2022-07-06 14:37:19 -0400

    Bump coverage

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -7,10 +7,10 @@ export default {
   coverageThreshold: {
     global: {
       // These percentages should never decrease
-      statements: 39,
-      branches: 38,
+      statements: 40,
+      branches: 40,
       functions: 23,
-      lines: 42,
+      lines: 43,
     },
   },
 };
