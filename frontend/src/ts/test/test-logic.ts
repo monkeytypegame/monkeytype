2e98da7f3f85fb89a4cce78bf486dbc3cedfd98d
egorguslyan <electromagneticcyclone@disroot.org>
2022-09-04 13:38:20 +0300

    C rework (#3483) egorguslyan
    * C wordlist extension
    
    * C-like syntax
    
    * Fix arduino tab width
    
    * Fix Quake 3 quote
    
    * Public domain C quotes
    
    * IDs
    
    * Quake 3
    
    * Punctuation
    
    * Prettier
    
    * startWith
    
    * css
    
    * prettier
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/ts/test/test-logic.ts
+++ frontend/src/ts/test/test-logic.ts
@@ -259,8 +259,50 @@ export async function punctuateWord(
       }
     } else if (Math.random() < 0.25 && currentLanguage == "code") {
       const specials = ["{", "}", "[", "]", "(", ")", ";", "=", "+", "%", "/"];
+      const specialsC = [
+        "{",
+        "}",
+        "[",
+        "]",
+        "(",
+        ")",
+        ";",
+        "=",
+        "+",
+        "%",
+        "/",
+        "/*",
+        "*/",
+        "//",
+        "!=",
+        "==",
+        "<=",
+        ">=",
+        "||",
+        "&&",
+        "<<",
+        ">>",
+        "%=",
+        "&=",
+        "*=",
+        "++",
+        "+=",
+        "--",
+        "-=",
+        "/=",
+        "^=",
+        "|=",
+      ];
 
-      word = Misc.randomElementFromArray(specials);
+      if (
+        (Config.language.startsWith("code_c") &&
+          !Config.language.startsWith("code_css")) ||
+        Config.language.startsWith("code_arduino")
+      ) {
+        word = Misc.randomElementFromArray(specialsC);
+      } else {
+        word = Misc.randomElementFromArray(specials);
+      }
     } else if (
       Math.random() < 0.5 &&
       currentLanguage === "english" &&
