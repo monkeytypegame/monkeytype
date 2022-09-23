c29ac5ff446be1ce76ecfbca3ad58e65d12f779e
Miodec <jack@monkeytype.com>
2022-09-23 20:27:28 +0200

    reverted to baseurl and route path

--- backend/src/utils/prometheus.ts
+++ backend/src/utils/prometheus.ts
@@ -216,7 +216,7 @@ export function recordRequestCountry(
   country: string,
   req: MonkeyTypes.Request
 ): void {
-  const reqPath = req.url;
+  const reqPath = req.baseUrl + req.route.path;
 
   let normalizedPath = "/";
   if (reqPath !== "/") {
