52405f3ccc6b5b3bfb8a89bdc01707b7f79ecb3f
riyu <66673988+riyuzenn@users.noreply.github.com>
2022-09-26 14:37:19 +0200

    include confetti

--- frontend/src/ts/test/test-logic.ts
+++ frontend/src/ts/test/test-logic.ts
@@ -1738,6 +1738,7 @@ async function saveResult(
 
   if (response?.data?.isPb) {
     //new pb
+    Result.showConfetti();
     Result.showCrown();
     Result.updateCrown();
     DB.saveLocalPB(
@@ -1768,6 +1769,7 @@ async function saveResult(
   if (!response?.data?.dailyLeaderboardRank) {
     $("#result .stats .dailyLeaderboard").addClass("hidden");
   } else {
+    Result.showConfetti();
     $("#result .stats .dailyLeaderboard")
       .css({
         maxWidth: "13rem",
