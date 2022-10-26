ea63e73b6133f1d7b10e06fbfec66fa44b623b01
Miodec <jack@monkeytype.com>
2022-10-26 14:01:56 +0200

    using correct variable

--- frontend/src/ts/pages/account.ts
+++ frontend/src/ts/pages/account.ts
@@ -713,7 +713,7 @@ function fillContent(): void {
     labels.push(`${bucket} - ${bucket + 9}`);
     if (bucket + 10 != parseInt(keys[i + 1])) {
       for (let j = bucket + 10; j < parseInt(keys[i + 1]); j += 10) {
-        histogramChartDataBucketed.push({ x: i, y: 0 });
+        histogramChartDataBucketed.push({ x: j, y: 0 });
         labels.push(`${j} - ${j + 9}`);
       }
     }
