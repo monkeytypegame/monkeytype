c4750b06fd5aba38806b56ff7c5899b45dcad373
Miodec <jack@monkeytype.com>
2023-01-13 20:18:32 +0100

    created file which holds skeletons for popups

---
+++ frontend/src/ts/popups/skeleton.ts
@@ -0,0 +1,22 @@
+const parent = document.getElementById("popups") as HTMLElement;
+const skeletons = new Map<string, HTMLElement>();
+
+export function save(id: string, removeAfter = true): void {
+  const el = document.getElementById(id) as HTMLElement;
+  skeletons.set(id, el);
+  if (removeAfter) remove(id);
+}
+
+export function remove(id: string): void {
+  const popup = skeletons.get(id);
+  if (popup) {
+    popup.remove();
+  } else {
+    save(id, true);
+  }
+}
+
+export function append(id: string): void {
+  const popup = skeletons.get(id) as HTMLElement;
+  parent.append(popup);
+}
