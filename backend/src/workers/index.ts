ca8e11a8fe69f9a40ca1d9241be0711e2f200aea
Miodec <jack@monkeytype.com>
2023-01-31 23:50:14 +0100

    name change

--- backend/src/workers/index.ts
+++ backend/src/workers/index.ts
@@ -1,4 +1,4 @@
 import LaterWorker from "./later-worker";
-import Emailer from "./email-worker";
+import EmailWorker from "./email-worker";
 
-export default [LaterWorker, Emailer];
+export default [LaterWorker, EmailWorker];
