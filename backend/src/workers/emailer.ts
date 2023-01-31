311db4ad31e446a05a7f7839d5b39df3562b2276
Miodec <jack@monkeytype.com>
2023-01-31 15:10:03 +0100

    email worker

---
+++ backend/src/workers/emailer.ts
@@ -0,0 +1,28 @@
+import _ from "lodash";
+import IORedis from "ioredis";
+import { Worker, Job } from "bullmq";
+import Logger from "../utils/logger";
+import EmailQueue, { EmailTask } from "../queues/email-queue";
+import { fillTemplate, sendMail } from "../utils/email";
+
+async function jobHandler(job: Job): Promise<void> {
+  const { type, email, args }: EmailTask = job.data;
+  Logger.info(`Starting job: ${type}`);
+
+  const start = performance.now();
+
+  if (type === "verify") {
+    const html = fillTemplate("verify", args);
+    await sendMail(email, "Verify your Monkeytype account", html);
+  }
+
+  const elapsed = performance.now() - start;
+
+  Logger.success(`Job: ${type} - completed in ${elapsed}ms`);
+}
+
+export default (redisConnection?: IORedis.Redis): Worker =>
+  new Worker(EmailQueue.queueName, jobHandler, {
+    autorun: false,
+    connection: redisConnection,
+  });
