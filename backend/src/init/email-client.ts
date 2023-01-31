ec74b1dd179d600b83700412e2a29940b27767c2
Miodec <jack@monkeytype.com>
2023-01-31 23:53:43 +0100

    moved templates

--- backend/src/init/email-client.ts
+++ backend/src/init/email-client.ts
@@ -147,7 +147,7 @@ export async function sendMail(
   }
 }
 
-const EMAIL_TEMPLATES_DIRECTORY = join(__dirname, "../email-templates");
+const EMAIL_TEMPLATES_DIRECTORY = join(__dirname, "../../email-templates");
 
 const cachedTemplates: Record<string, string> = {};
 
