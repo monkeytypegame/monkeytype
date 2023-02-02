f420282e6e790a4a197afbbc5d8149984ea3929e
Miodec <jack@monkeytype.com>
2023-02-02 17:42:07 +0100

    added route

--- frontend/src/ts/tribe/tribe-socket/routes/user.ts
+++ frontend/src/ts/tribe/tribe-socket/routes/user.ts
@@ -4,8 +4,14 @@ function setName(newName: string, confirm = false): void {
   Socket.emit("user_set_name", { name: newName, confirm });
 }
 
+function updateName(callback: (data: { name: string }) => void): void {
+  Socket.on("user_update_name", callback);
+}
+
 export default {
-  in: {},
+  in: {
+    updateName,
+  },
   out: {
     setName,
   },
