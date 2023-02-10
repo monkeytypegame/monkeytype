9cff1a1c18764017dccc9de951e10e5967019e5f
Miodec <jack@monkeytype.com>
2023-02-10 21:38:54 +0100

    version

--- frontend/src/ts/tribe/tribe.ts
+++ frontend/src/ts/tribe/tribe.ts
@@ -33,7 +33,7 @@ import * as TribeCarets from "./tribe-carets";
 const defaultName = "Guest";
 let name = "Guest";
 
-export const expectedVersion = "0.12.1";
+export const expectedVersion = "0.12.2";
 
 let autoJoin: string | undefined = undefined;
 
