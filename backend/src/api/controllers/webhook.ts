3f93813737923760627ce070e17978ae14c4f5cd
Ferotiq <evanrayweigel@outlook.com>
2023-02-19 17:26:00 -0600

    add secret functionality

--- backend/src/api/controllers/webhook.ts
+++ backend/src/api/controllers/webhook.ts
@@ -1,9 +1,20 @@
+import crypto from "crypto";
 import georgeQueue from "../../queues/george-queue";
 import { MonkeyResponse } from "../../utils/monkey-response";
+
+const secretHash = crypto
+  .createHash("sha256")
+  .update(process.env.GITHUB_SECRET ?? "")
+  .digest("hex");
 
 export async function sendRelease(
   req: MonkeyTypes.Request
 ): Promise<MonkeyResponse> {
+  const { "x-hub-signature-256": signature } = req.headers;
+  if (signature && signature !== `sha256=${secretHash}`) {
+    return new MonkeyResponse("Unauthorized", 401);
+  }
+
   const { release } = req.body;
   const releaseId = release.id;
 
