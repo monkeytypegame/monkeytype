cb231cd2f334326f819436ff2c02711ef79acd5e
albertying <albertlying@gmail.com>
2023-02-21 13:06:17 -0500

    Add word filter presets

--- frontend/src/ts/popups/word-filter-popup.ts
+++ frontend/src/ts/popups/word-filter-popup.ts
@@ -11,7 +11,13 @@ async function init(): Promise<void> {
   if (!initialised) {
     $("#wordFilterPopup .languageInput").empty();
 
+    $("#wordFilterPopup .layoutInput").empty();
+
+    $("wordFilterPopup .presetInput").empty();
+
     let LanguageList;
+    let LayoutList;
+
     try {
       LanguageList = await Misc.getLanguageList();
     } catch (e) {
@@ -31,6 +37,39 @@ async function init(): Promise<void> {
         <option value=${language}>${prettyLang}</option>
       `);
     });
+
+    try {
+      LayoutList = await Misc.getLayoutsList();
+    } catch (e) {
+      console.error(
+        Misc.createErrorMessage(
+          e,
+          "Failed to initialise word filter popup preset list"
+        )
+      );
+      return;
+    }
+
+    for (let layout in LayoutList) {
+      $("#wordFilterPopup .layoutInput").append(`
+      <option value=${layout}>${layout}</option>
+    `);
+    }
+
+    let PresetList = [
+      "left hand",
+      "right hand",
+      "home row",
+      "top row",
+      "bottom row",
+    ];
+
+    PresetList.map((preset) => {
+      $("#wordFilterPopup .presetInput").append(`
+      <option value=${preset}>${preset}</option>
+    `);
+    });
+
     initialised = true;
   }
 }
@@ -46,6 +85,14 @@ export async function show(
     await init();
     callbackFuncOnHide = callbackOnHide;
     $("#wordFilterPopup .languageInput").select2({
+      width: "100%",
+    });
+
+    $("#wordFilterPopup .layoutInput").select2({
+      width: "100%",
+    });
+
+    $("#wordFilterPopup .presetInput").select2({
       width: "100%",
     });
     $("#wordFilterPopupWrapper .loadingIndicator").addClass("hidden");
@@ -158,6 +205,110 @@ $("#wordFilterPopupWrapper .button").on("mousedown", (e) => {
   setTimeout(() => {
     apply($(e.target).is("#set"));
   }, 1);
+});
+
+$("#wordFilterPopup .generateButton").on("click", function () {
+  const presetName = $("#wordFilterPopup .presetInput").val() as string;
+  const layoutName = $("#wordFilterPopup .layoutInput").val() as string;
+
+  const combine = (include: string[], exclude: string[]) => {
+    let includeCharacters = "";
+    let excludeCharacters = "";
+
+    include.forEach((letter) => {
+      includeCharacters += letter[0] + " ";
+    });
+
+    exclude.forEach((letter) => {
+      excludeCharacters += letter[0] + " ";
+    });
+
+    $("#wordIncludeInput").val(includeCharacters);
+    $("#wordExcludeInput").val(excludeCharacters);
+  };
+
+  const left = async () => {
+    const layout = await Misc.getLayout(layoutName);
+
+    const topRowInclude = layout.keys.row2.slice(0, 5);
+    const homeRowInclude = layout.keys.row3.slice(0, 5);
+    const bottomRowInclude = layout.keys.row4.slice(0, 5);
+
+    const topRowExclude = layout.keys.row2.slice(5);
+    const homeRowExclude = layout.keys.row3.slice(5);
+    const bottomRowExclude = layout.keys.row4.slice(5);
+
+    const include = topRowInclude.concat(homeRowInclude, bottomRowInclude);
+    const exclude = topRowExclude.concat(homeRowExclude, bottomRowExclude);
+
+    combine(include, exclude);
+  };
+  const right = async () => {
+    const layout = await Misc.getLayout(layoutName);
+    const topRowInclude = layout.keys.row2.slice(5);
+    const homeRowInclude = layout.keys.row3.slice(5);
+    const bottomRowInclude = layout.keys.row4.slice(5);
+
+    const topRowExclude = layout.keys.row2.slice(0, 5);
+    const homeRowExclude = layout.keys.row3.slice(0, 5);
+    const bottomRowExclude = layout.keys.row4.slice(0, 5);
+
+    const include = topRowInclude.concat(homeRowInclude, bottomRowInclude);
+    const exclude = topRowExclude.concat(homeRowExclude, bottomRowExclude);
+
+    combine(include, exclude);
+  };
+
+  const home = async () => {
+    const layout = await Misc.getLayout(layoutName);
+
+    const homeRow = layout.keys.row3;
+    const topRow = layout.keys.row2;
+    const bottomRow = layout.keys.row4;
+
+    const include = homeRow;
+    const exclude = topRow.concat(bottomRow);
+
+    combine(include, exclude);
+  };
+
+  const top = async () => {
+    const layout = await Misc.getLayout(layoutName);
+
+    const homeRow = layout.keys.row3;
+    const topRow = layout.keys.row2;
+    const bottomRow = layout.keys.row4;
+
+    const include = topRow;
+    const exclude = homeRow.concat(bottomRow);
+
+    combine(include, exclude);
+  };
+
+  const bottom = async () => {
+    const layout = await Misc.getLayout(layoutName);
+
+    const homeRow = layout.keys.row3;
+    const topRow = layout.keys.row2;
+    const bottomRow = layout.keys.row4;
+
+    const include = bottomRow;
+    const exclude = homeRow.concat(topRow);
+
+    combine(include, exclude);
+  };
+
+  if (presetName == "left") {
+    left();
+  } else if (presetName == "right") {
+    right();
+  } else if (presetName == "home") {
+    home();
+  } else if (presetName == "top") {
+    top();
+  } else if (presetName == "bottom") {
+    bottom();
+  }
 });
 
 Skeleton.save(wrapperId);
