3da33733b276817e67623cc5ebe427789928b166
trevorwrightdev <trevor.wright.710@gmail.com>
2023-02-26 18:28:31 -0700

    added -example firebase config back in

---
+++ frontend/src/ts/constants/firebase-config-example.ts
@@ -0,0 +1,14 @@
+// To find your config, go to https://console.firebase.google.com/ and select your project
+// Go to (top left) Settings > Project Settings > General
+// scroll down to Your apps > Web Apps (if it doesnt exist, create one) > SDK setup and configuration > select npm
+// your config should be visible there
+
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
