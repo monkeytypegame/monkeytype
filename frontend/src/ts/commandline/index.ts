3bea55bb939aa99bca5e881c4b460d3265d2f068
Evan <evanrayweigel@outlook.com>
2023-03-18 14:31:59 -0500

    prettier

--- frontend/src/ts/commandline/index.ts
+++ frontend/src/ts/commandline/index.ts
@@ -454,7 +454,9 @@ $(document).ready(() => {
         (event.key?.toLowerCase() === "p" &&
           (event.metaKey || event.ctrlKey) &&
           event.shiftKey) ||
-        ((event.key === "Tab" || event.key === "Escape" || event.key === "Enter") &&
+        ((event.key === "Tab" ||
+          event.key === "Escape" ||
+          event.key === "Enter") &&
           Config.quickRestart === "esc")) &&
       isPopupVisible(wrapperId)
     ) {
