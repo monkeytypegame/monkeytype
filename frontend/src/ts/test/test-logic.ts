7ef3c1c2e02501c06b894d68ddfc7615a239e24d
Evan <evanrayweigel@outlook.com>
2023-03-27 20:29:26 -0500

    rename

--- frontend/src/ts/test/test-logic.ts
+++ frontend/src/ts/test/test-logic.ts
@@ -1005,7 +1005,7 @@ export async function finish(difficultyFailed = false): Promise<void> {
   let afkDetected = kps.every((second) => second.afk);
   if (TestInput.bailout) afkDetected = false;
 
-  const mode2Value = parseInt(completedEvent.mode2);
+  const mode2Number = parseInt(completedEvent.mode2);
 
   let tooShort = false;
   //fail checks
@@ -1021,13 +1021,13 @@ export async function finish(difficultyFailed = false): Promise<void> {
     Notifications.add("Test invalid - repeated", 0);
     dontSave = true;
   } else if (
-    (Config.mode === "time" && mode2Value < 15 && mode2Value > 0) ||
+    (Config.mode === "time" && mode2Number < 15 && mode2Number > 0) ||
     (Config.mode === "time" &&
-      mode2Value === 0 &&
+      mode2Number === 0 &&
       completedEvent.testDuration < 15) ||
-    (Config.mode === "words" && mode2Value < 10 && mode2Value > 0) ||
+    (Config.mode === "words" && mode2Number < 10 && mode2Number > 0) ||
     (Config.mode === "words" &&
-      mode2Value === 0 &&
+      mode2Number === 0 &&
       completedEvent.testDuration < 15) ||
     (Config.mode === "custom" &&
       !CustomText.isWordRandom &&
