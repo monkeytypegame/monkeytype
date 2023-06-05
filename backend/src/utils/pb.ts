01146b71fce6f031281bea265c2e814913773703
Miodec <jack@monkeytype.com>
2023-06-05 13:11:39 +0200

    updated funbox structure added frontend properties and functions updated type

--- backend/src/utils/pb.ts
+++ backend/src/utils/pb.ts
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import FunboxesMetadata from "../constants/funbox";
+import FunboxesMetadata from "../constants/funbox-list";
 
 interface CheckAndUpdatePbResult {
   isPb: boolean;
