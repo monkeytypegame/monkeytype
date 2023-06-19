3cedcda8b1a816bd33f737b99fdd04a7ad47e60d
Miodec <jack@monkeytype.com>
2023-06-19 18:15:25 +0200

    missing extends

--- frontend/src/ts/settings/settings-group.ts
+++ frontend/src/ts/settings/settings-group.ts
@@ -1,6 +1,6 @@
 import Config from "../config";
 
-export default class SettingsGroup<T> {
+export default class SettingsGroup<T extends MonkeyTypes.ConfigValues> {
   public configName: string;
   public configValue: T;
   public configFunction: (param: T, nosave?: boolean) => boolean;
