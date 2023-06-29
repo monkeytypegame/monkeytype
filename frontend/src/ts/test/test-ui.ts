080732c092c99960a0c76a9c67d876fd13b1cc40
Sanidhya Singh <sanidhyas3s@gmail.com>
2023-06-29 22:20:01 +0530

    Fix Cookie Popup from appearing in Screenshots (#4426) sanidhyas3s

--- frontend/src/ts/test/test-ui.ts
+++ frontend/src/ts/test/test-ui.ts
@@ -384,7 +384,10 @@ export async function screenshot(): Promise<void> {
   let revealReplay = false;
 
   let revertCookie = false;
-  if (Misc.isElementVisible("#cookiePopupWrapper")) {
+  if (
+    Misc.isElementVisible("#cookiePopupWrapper") ||
+    document.contains(document.querySelector("#cookiePopupWrapper"))
+  ) {
     revertCookie = true;
   }
 
