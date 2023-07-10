54f89e00dee7e9f8c08f44125f900c42c278395f
Miodec <jack@monkeytype.com>
2023-07-10 12:02:30 +0200

    fixed indentation

--- frontend/src/ts/pages/login.ts
+++ frontend/src/ts/pages/login.ts
@@ -5,7 +5,6 @@ import * as Notifications from "../elements/notifications";
 import { InputIndicator } from "../elements/input-indicator";
 import * as Skeleton from "../popups/skeleton";
 import * as Misc from "../utils/misc";
-import typo_list from "../utils/misc";
 
 export function enableSignUpButton(): void {
   $(".page.pageLogin .register.side .button").removeClass("disabled");
@@ -86,7 +85,7 @@ const checkNameDebounced = debounce(1000, async () => {
 const checkEmail = (): void => {
   const emailRegex =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  const emailTypoRegex = typo_list;
+  const emailTypoRegex = Misc.typo_list;
 
   const email = $(".page.pageLogin .register.side .emailInput").val() as string;
   if (emailRegex.test(email)) {
@@ -118,246 +117,240 @@ const checkEmail = (): void => {
 
     updateSignupButton();
   }
-
-  const checkEmailsMatch = (): void => {
-    const email = $(".page.pageLogin .register.side .emailInput").val();
-    const verifyEmail = $(
-      ".page.pageLogin .register.side .verifyEmailInput"
-    ).val();
-    if (email === verifyEmail) {
-      verifyEmailIndicator.show("match");
-    } else {
-      verifyEmailIndicator.show("mismatch");
-    }
-
-    updateSignupButton();
-  };
-
-  const checkPassword = (): void => {
-    const password = $(
-      ".page.pageLogin .register.side .passwordInput"
-    ).val() as string;
-
-    // Force user to use a capital letter, number, special character and reasonable length when setting up an account and changing password
-    if (!Misc.isLocalhost() && !Misc.isPasswordStrong(password)) {
-      if (password.length < 8) {
-        passwordIndicator.show(
-          "short",
-          "Password must be at least 8 characters"
-        );
-      } else if (password.length > 64) {
-        passwordIndicator.show(
-          "long",
-          "Password must be at most 64 characters"
-        );
-      } else {
-        passwordIndicator.show(
-          "weak",
-          "Password must contain at least one capital letter, number, and special character"
-        );
-      }
-    } else {
-      passwordIndicator.show("good", "Password is good");
-    }
-    updateSignupButton();
-  };
-
-  const checkPasswordsMatch = (): void => {
-    const password = $(".page.pageLogin .register.side .passwordInput").val();
-    const verifyPassword = $(
-      ".page.pageLogin .register.side .verifyPasswordInput"
-    ).val();
-    if (password === verifyPassword) {
-      verifyPasswordIndicator.show("match");
-    } else {
-      verifyPasswordIndicator.show("mismatch");
-    }
-
-    updateSignupButton();
-  };
-
-  const nameIndicator = new InputIndicator(
-    $(".page.pageLogin .register.side input.usernameInput"),
-    {
-      available: {
-        icon: "fa-check",
-        level: 1,
-      },
-      unavailable: {
-        icon: "fa-times",
-        level: -1,
-      },
-      taken: {
-        icon: "fa-times",
-        level: -1,
-      },
-      checking: {
-        icon: "fa-circle-notch",
-        spinIcon: true,
-        level: 0,
-      },
-    }
-  );
-
-  const emailIndicator = new InputIndicator(
-    $(".page.pageLogin .register.side input.emailInput"),
-    {
-      valid: {
-        icon: "fa-check",
-        level: 1,
-      },
-      invalid: {
-        icon: "fa-times",
-        level: -1,
-      },
-      emailTypoIndicator: {
-        icon: "fa-exclamation-triangle",
-        level: 1,
-      },
-      edu: {
-        icon: "fa-exclamation-triangle",
-        level: 1,
-      },
-    }
-  );
-
-  const verifyEmailIndicator = new InputIndicator(
-    $(".page.pageLogin .register.side input.verifyEmailInput"),
-    {
-      match: {
-        icon: "fa-check",
-        level: 1,
-      },
-      mismatch: {
-        icon: "fa-times",
-        level: -1,
-      },
-    }
-  );
-
-  const passwordIndicator = new InputIndicator(
-    $(".page.pageLogin .register.side input.passwordInput"),
-    {
-      good: {
-        icon: "fa-check",
-        level: 1,
-      },
-      short: {
-        icon: "fa-times",
-        level: -1,
-      },
-      long: {
-        icon: "fa-times",
-        level: -1,
-      },
-      weak: {
-        icon: "fa-times",
-        level: -1,
-      },
-    }
-  );
-
-  const verifyPasswordIndicator = new InputIndicator(
-    $(".page.pageLogin .register.side input.verifyPasswordInput"),
-    {
-      match: {
-        icon: "fa-check",
-        level: 1,
-      },
-      mismatch: {
-        icon: "fa-times",
-        level: -1,
-      },
-    }
-  );
-
-  $(".page.pageLogin .register.side .usernameInput").on("input", () => {
-    setTimeout(() => {
-      const val = $(
-        ".page.pageLogin .register.side .usernameInput"
-      ).val() as string;
-      if (val === "") {
-        return nameIndicator.hide();
-      } else {
-        nameIndicator.show("checking");
-        checkNameDebounced();
-      }
-    }, 1);
-  });
-
-  $(".page.pageLogin .register.side .emailInput").on("input", () => {
-    if (
-      !$(".page.pageLogin .register.side .emailInput").val() &&
-      !$(".page.pageLogin .register.side .verifyEmailInput").val()
-    ) {
-      emailIndicator.hide();
-      verifyEmailIndicator.hide();
-      return;
-    }
-    checkEmail();
-    checkEmailsMatch();
-  });
-
-  $(".page.pageLogin .register.side .verifyEmailInput").on("input", () => {
-    if (
-      !$(".page.pageLogin .register.side .emailInput").val() &&
-      !$(".page.pageLogin .register.side .verifyEmailInput").val()
-    ) {
-      emailIndicator.hide();
-      verifyEmailIndicator.hide();
-      return;
-    }
-    checkEmailsMatch();
-  });
-
-  $(".page.pageLogin .register.side .passwordInput").on("input", () => {
-    if (
-      !$(".page.pageLogin .register.side .passwordInput").val() &&
-      !$(".page.pageLogin .register.side .verifyPasswordInput").val()
-    ) {
-      passwordIndicator.hide();
-      verifyPasswordIndicator.hide();
-      return;
-    }
-    checkPassword();
-    checkPasswordsMatch();
-  });
-
-  $(".page.pageLogin .register.side .verifyPasswordInput").on("input", () => {
-    if (
-      !$(".page.pageLogin .register.side .passwordInput").val() &&
-      !$(".page.pageLogin .register.side .verifyPasswordInput").val()
-    ) {
-      passwordIndicator.hide();
-      verifyPasswordIndicator.hide();
-      return;
-    }
-    checkPassword();
-    checkPasswordsMatch();
-  });
-
-  export const page = new Page(
-    "login",
-    $(".page.pageLogin"),
-    "/login",
-    async () => {
-      //
-    },
-    async () => {
-      $(".pageLogin input").val("");
-      Skeleton.remove("pageLogin");
-    },
-    async () => {
-      Skeleton.append("pageLogin", "middle");
-      $(".pageLogin .button").removeClass("disabled");
-      $(".pageLogin input").prop("disabled", false);
-    },
-    async () => {
-      //
-    }
-  );
-
-  $(() => {
-    Skeleton.save("pageLogin");
-  });
 };
+
+const checkEmailsMatch = (): void => {
+  const email = $(".page.pageLogin .register.side .emailInput").val();
+  const verifyEmail = $(
+    ".page.pageLogin .register.side .verifyEmailInput"
+  ).val();
+  if (email === verifyEmail) {
+    verifyEmailIndicator.show("match");
+  } else {
+    verifyEmailIndicator.show("mismatch");
+  }
+
+  updateSignupButton();
+};
+
+const checkPassword = (): void => {
+  const password = $(
+    ".page.pageLogin .register.side .passwordInput"
+  ).val() as string;
+
+  // Force user to use a capital letter, number, special character and reasonable length when setting up an account and changing password
+  if (!Misc.isLocalhost() && !Misc.isPasswordStrong(password)) {
+    if (password.length < 8) {
+      passwordIndicator.show("short", "Password must be at least 8 characters");
+    } else if (password.length > 64) {
+      passwordIndicator.show("long", "Password must be at most 64 characters");
+    } else {
+      passwordIndicator.show(
+        "weak",
+        "Password must contain at least one capital letter, number, and special character"
+      );
+    }
+  } else {
+    passwordIndicator.show("good", "Password is good");
+  }
+  updateSignupButton();
+};
+
+const checkPasswordsMatch = (): void => {
+  const password = $(".page.pageLogin .register.side .passwordInput").val();
+  const verifyPassword = $(
+    ".page.pageLogin .register.side .verifyPasswordInput"
+  ).val();
+  if (password === verifyPassword) {
+    verifyPasswordIndicator.show("match");
+  } else {
+    verifyPasswordIndicator.show("mismatch");
+  }
+
+  updateSignupButton();
+};
+
+const nameIndicator = new InputIndicator(
+  $(".page.pageLogin .register.side input.usernameInput"),
+  {
+    available: {
+      icon: "fa-check",
+      level: 1,
+    },
+    unavailable: {
+      icon: "fa-times",
+      level: -1,
+    },
+    taken: {
+      icon: "fa-times",
+      level: -1,
+    },
+    checking: {
+      icon: "fa-circle-notch",
+      spinIcon: true,
+      level: 0,
+    },
+  }
+);
+
+const emailIndicator = new InputIndicator(
+  $(".page.pageLogin .register.side input.emailInput"),
+  {
+    valid: {
+      icon: "fa-check",
+      level: 1,
+    },
+    invalid: {
+      icon: "fa-times",
+      level: -1,
+    },
+    emailTypoIndicator: {
+      icon: "fa-exclamation-triangle",
+      level: 1,
+    },
+    edu: {
+      icon: "fa-exclamation-triangle",
+      level: 1,
+    },
+  }
+);
+
+const verifyEmailIndicator = new InputIndicator(
+  $(".page.pageLogin .register.side input.verifyEmailInput"),
+  {
+    match: {
+      icon: "fa-check",
+      level: 1,
+    },
+    mismatch: {
+      icon: "fa-times",
+      level: -1,
+    },
+  }
+);
+
+const passwordIndicator = new InputIndicator(
+  $(".page.pageLogin .register.side input.passwordInput"),
+  {
+    good: {
+      icon: "fa-check",
+      level: 1,
+    },
+    short: {
+      icon: "fa-times",
+      level: -1,
+    },
+    long: {
+      icon: "fa-times",
+      level: -1,
+    },
+    weak: {
+      icon: "fa-times",
+      level: -1,
+    },
+  }
+);
+
+const verifyPasswordIndicator = new InputIndicator(
+  $(".page.pageLogin .register.side input.verifyPasswordInput"),
+  {
+    match: {
+      icon: "fa-check",
+      level: 1,
+    },
+    mismatch: {
+      icon: "fa-times",
+      level: -1,
+    },
+  }
+);
+
+$(".page.pageLogin .register.side .usernameInput").on("input", () => {
+  setTimeout(() => {
+    const val = $(
+      ".page.pageLogin .register.side .usernameInput"
+    ).val() as string;
+    if (val === "") {
+      return nameIndicator.hide();
+    } else {
+      nameIndicator.show("checking");
+      checkNameDebounced();
+    }
+  }, 1);
+});
+
+$(".page.pageLogin .register.side .emailInput").on("input", () => {
+  if (
+    !$(".page.pageLogin .register.side .emailInput").val() &&
+    !$(".page.pageLogin .register.side .verifyEmailInput").val()
+  ) {
+    emailIndicator.hide();
+    verifyEmailIndicator.hide();
+    return;
+  }
+  checkEmail();
+  checkEmailsMatch();
+});
+
+$(".page.pageLogin .register.side .verifyEmailInput").on("input", () => {
+  if (
+    !$(".page.pageLogin .register.side .emailInput").val() &&
+    !$(".page.pageLogin .register.side .verifyEmailInput").val()
+  ) {
+    emailIndicator.hide();
+    verifyEmailIndicator.hide();
+    return;
+  }
+  checkEmailsMatch();
+});
+
+$(".page.pageLogin .register.side .passwordInput").on("input", () => {
+  if (
+    !$(".page.pageLogin .register.side .passwordInput").val() &&
+    !$(".page.pageLogin .register.side .verifyPasswordInput").val()
+  ) {
+    passwordIndicator.hide();
+    verifyPasswordIndicator.hide();
+    return;
+  }
+  checkPassword();
+  checkPasswordsMatch();
+});
+
+$(".page.pageLogin .register.side .verifyPasswordInput").on("input", () => {
+  if (
+    !$(".page.pageLogin .register.side .passwordInput").val() &&
+    !$(".page.pageLogin .register.side .verifyPasswordInput").val()
+  ) {
+    passwordIndicator.hide();
+    verifyPasswordIndicator.hide();
+    return;
+  }
+  checkPassword();
+  checkPasswordsMatch();
+});
+
+export const page = new Page(
+  "login",
+  $(".page.pageLogin"),
+  "/login",
+  async () => {
+    //
+  },
+  async () => {
+    $(".pageLogin input").val("");
+    Skeleton.remove("pageLogin");
+  },
+  async () => {
+    Skeleton.append("pageLogin", "middle");
+    $(".pageLogin .button").removeClass("disabled");
+    $(".pageLogin input").prop("disabled", false);
+  },
+  async () => {
+    //
+  }
+);
+
+$(() => {
+  Skeleton.save("pageLogin");
+});
