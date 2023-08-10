c993d20e6efec347d949d05bafe27c86c1f0dbfa
Miodec <jack@monkeytype.com>
2023-08-10 14:00:39 +0200

    added a third entry to the british english list this list contains a list of possible previous words - when matched, the replacement will not happen

--- frontend/src/ts/test/british-english.ts
+++ frontend/src/ts/test/british-english.ts
@@ -1,8 +1,16 @@
+import Config from "../config";
 import { capitalizeFirstLetterOfEachWord } from "../utils/misc";
+import * as CustomText from "../test/custom-text";
 
-let list: string[] = [];
+interface BritishEnglishReplacement {
+  0: string;
+  1: string;
+  2?: string[];
+}
 
-export async function getList(): Promise<string[]> {
+let list: BritishEnglishReplacement[] = [];
+
+export async function getList(): Promise<BritishEnglishReplacement[]> {
   if (list.length === 0) {
     return $.getJSON("languages/britishenglish.json", function (data) {
       list = data;
@@ -13,31 +21,50 @@ export async function getList(): Promise<string[]> {
   }
 }
 
-export async function replace(word: string): Promise<string> {
+export async function replace(
+  word: string,
+  previousWord: string
+): Promise<string> {
   const list = await getList();
 
   if (word.includes("-")) {
     //this handles hyphenated words (for example "cream-colored") to make sure
     //we don't have to add every possible combination to the list
     return (
-      await Promise.all(word.split("-").map(async (w) => replace(w)))
+      await Promise.all(
+        word.split("-").map(async (w) => replace(w, previousWord))
+      )
     ).join("-");
   } else {
     const replacement = list.find((a) =>
       word.match(RegExp(`^([\\W]*${a[0]}[\\W]*)$`, "gi"))
     );
-    return replacement
-      ? word.replace(
-          RegExp(`^(?:([\\W]*)(${replacement[0]})([\\W]*))$`, "gi"),
-          (_, $1, $2, $3) =>
-            $1 +
-            ($2.charAt(0) === $2.charAt(0).toUpperCase()
-              ? $2 === $2.toUpperCase()
-                ? replacement[1].toUpperCase()
-                : capitalizeFirstLetterOfEachWord(replacement[1])
-              : replacement[1]) +
-            $3
-        )
-      : word;
+
+    console.log(list);
+
+    if (!replacement) return word;
+
+    if (
+      (Config.mode === "quote" ||
+        (Config.mode === "custom" &&
+          !CustomText.isTimeRandom &&
+          !CustomText.isWordRandom &&
+          !CustomText.isSectionRandom)) &&
+      replacement[2]?.includes(previousWord)
+    ) {
+      return word;
+    }
+
+    return word.replace(
+      RegExp(`^(?:([\\W]*)(${replacement[0]})([\\W]*))$`, "gi"),
+      (_, $1, $2, $3) =>
+        $1 +
+        ($2.charAt(0) === $2.charAt(0).toUpperCase()
+          ? $2 === $2.toUpperCase()
+            ? replacement[1].toUpperCase()
+            : capitalizeFirstLetterOfEachWord(replacement[1])
+          : replacement[1]) +
+        $3
+    );
   }
 }
