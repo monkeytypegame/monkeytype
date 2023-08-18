d5be596e2f6b09e52a128e012b037e89ca012b57
Danish arora <93310647+HyperDanisH@users.noreply.github.com>
2023-08-18 14:23:07 +0530

    Update daily-leaderboards.ts

--- backend/src/utils/daily-leaderboards.ts
+++ backend/src/utils/daily-leaderboards.ts
@@ -168,8 +168,8 @@ export class DailyLeaderboard {
       .zrange(leaderboardScoresKey, 0, 1, "WITHSCORES")
       .exec();
 
-    const minWpm = minScore.length > 0 ? (parseInt(minScore[1].slice(1, 6)) / 100) : 0;
-    
+    const minWpm =
+      minScore.length > 0 ? parseInt(minScore[1].slice(1, 6)) / 100 : 0;
     if (rank === null) {
       return { minWpm };
     }
