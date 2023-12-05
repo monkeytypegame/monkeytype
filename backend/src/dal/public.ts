6ec4a94fb01a6eb0e4878bdac8ec5ff38634cdac
Christian Fehmer <cfe@sexy-developer.com>
2023-12-05 14:57:46 +0100

    perf: improve leaderboard aggregation
    Update the leaderboard and public speedStats directly in mongodb without importing the data in the nodejs process first

--- backend/src/dal/public.ts
+++ backend/src/dal/public.ts
@@ -29,9 +29,10 @@ export async function getSpeedHistogram(
   mode2: string
 ): Promise<Record<string, number>> {
   const key = `${language}_${mode}_${mode2}`;
+
   const stats = await db
     .collection<MonkeyTypes.PublicSpeedStats>("public")
-    .findOne({ type: "speedStats" }, { projection: { [key]: 1 } });
+    .findOne({ _id: "speedStatsHistogram" }, { projection: { [key]: 1 } });
   return stats?.[key] ?? {};
 }
 
