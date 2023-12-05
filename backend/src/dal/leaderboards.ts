050cb7ab8a0a84e611b4e04d6bd82f6699d6f788
Christian Fehmer <fehmer@users.noreply.github.com>
2023-12-05 14:42:14 +0100

    perf(leaderboards): better index and aggregation code (fehmer) (#4836)
    * perf: Improve leaderboard performance by adding better index
    
    * review comments
    
    * Allow aggregation to use disk if needed

--- backend/src/dal/leaderboards.ts
+++ backend/src/dal/leaderboards.ts
@@ -73,25 +73,6 @@ export async function update(
     .aggregate<MonkeyTypes.LeaderboardEntry>(
       [
         {
-          $project: {
-            _id: 0,
-            [`${key}.wpm`]: 1,
-            [`${key}.acc`]: 1,
-            [`${key}.raw`]: 1,
-            [`${key}.consistency`]: 1,
-            [`${key}.timestamp`]: 1,
-            banned: 1,
-            lbOptOut: 1,
-            needsToChangeName: 1,
-            timeTyping: 1,
-            uid: 1,
-            name: 1,
-            discordId: 1,
-            discordAvatar: 1,
-            inventory: 1,
-          },
-        },
-        {
           $match: {
             [`${key}.wpm`]: {
               $gt: 0,
@@ -121,6 +102,25 @@ export async function update(
             [`${key}.wpm`]: -1,
             [`${key}.acc`]: -1,
             [`${key}.timestamp`]: -1,
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            [`${key}.wpm`]: 1,
+            [`${key}.acc`]: 1,
+            [`${key}.raw`]: 1,
+            [`${key}.consistency`]: 1,
+            [`${key}.timestamp`]: 1,
+            banned: 1,
+            lbOptOut: 1,
+            needsToChangeName: 1,
+            timeTyping: 1,
+            uid: 1,
+            name: 1,
+            discordId: 1,
+            discordAvatar: 1,
+            inventory: 1,
           },
         },
         {
@@ -233,4 +233,39 @@ export async function update(
       message: "Successfully updated leaderboard",
     };
   }
+}
+
+async function createIndex(key: string): Promise<void> {
+  const index = {
+    [`${key}.wpm`]: -1,
+    [`${key}.acc`]: -1,
+    [`${key}.timestamp`]: -1,
+    [`${key}.raw`]: -1,
+    [`${key}.consistency`]: -1,
+    banned: 1,
+    lbOptOut: 1,
+    needsToChangeName: 1,
+    timeTyping: 1,
+    uid: 1,
+    name: 1,
+    discordId: 1,
+    discordAvatar: 1,
+    inventory: 1,
+  };
+  const partial = {
+    partialFilterExpression: {
+      [`${key}.wpm`]: {
+        $gt: 0,
+      },
+      timeTyping: {
+        $gt: isDevEnvironment() ? 0 : 7200,
+      },
+    },
+  };
+  await db.collection("users").createIndex(index, partial);
+}
+
+export async function createIndicies(): Promise<void> {
+  await createIndex("lbPersonalBests.time.15.english");
+  await createIndex("lbPersonalBests.time.60.english");
 }
