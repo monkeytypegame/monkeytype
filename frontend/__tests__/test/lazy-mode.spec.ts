633f12e66452aff6d8cbf8d19e1ecc39c15cce27
Christian Fehmer <cfe@sexy-developer.com>
2023-12-29 10:57:28 +0100

    add tests

--- frontend/__tests__/test/lazy-mode.spec.ts
+++ frontend/__tests__/test/lazy-mode.spec.ts
@@ -1,14 +1,38 @@
 import { replaceAccents } from "../../src/ts/test/lazy-mode";
 
+let germanAccents = [
+  ["ö", "oe"],
+  ["ä", "ae"],
+  ["ü", "ue"],
+] as [string, string][];
+
 describe("lazy-mode", () => {
   describe("replaceAccents", () => {
     it("should replace common accents", () => {
       const result = replaceAccents("Héllö");
       expect(result).toBe("Hello");
     });
-    it("should replace common accents with override", () => {
+    it("should extend common accents with additional accents", () => {
       const result = replaceAccents("Héllö", [["ö", "oe"]]);
-      expect(result).toBe("Hélloe");
+      expect(result).toBe("Helloe");
+    });
+    it("should ignore empty word", () => {
+      const result = replaceAccents("");
+      expect(result).toBe("");
+    });
+    describe("german accents", () => {
+      it("should replace additional accents", () => {
+        const result = replaceAccents("Tränenüberströmt", germanAccents);
+        expect(result).toBe("Traenenueberstroemt");
+      });
+      it("should replace starting with uppercase accent", () => {
+        const result = replaceAccents("Äpfel", germanAccents);
+        expect(result).toBe("Aepfel");
+      });
+      it("should replace common accents", () => {
+        const result = replaceAccents("äße", germanAccents);
+        expect(result).toBe("aesse");
+      });
     });
   });
 });
