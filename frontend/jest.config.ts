ba582cd825cd174dcd496204fee932fedb4dd06f
Christian Fehmer <cfe@sexy-developer.com>
2024-01-04 22:46:32 +0100

    remove test coverage thresholds, cleanup

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -2,19 +2,8 @@ import { defaults as tsjPreset } from "ts-jest/presets";
 
 export default {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
   transform: tsjPreset.transform,
   setupFilesAfterEnv: ["<rootDir>/__tests__/setup-tests.ts"],
   modulePathIgnorePatterns: ["<rootDir>/__tests__/setup-tests.ts"],
-  testRegex: "(/__tests__/.*|(\\.|/)(spec))\\.[jt]sx?$",
-
-  coverageThreshold: {
-    global: {
-      // These percentages should never decrease
-      statements: 40,
-      branches: 37,
-      functions: 25,
-      lines: 43,
-    },
-  },
+  testRegex: "(/__tests__/.*|(\\.|/)(spec))\\.[jt]sx?$", //prevent files like test.ts outside the __tests__ folder to be detected as test
 };
