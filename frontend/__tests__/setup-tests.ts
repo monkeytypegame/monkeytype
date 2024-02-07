9d7e5b1ddbdad899ba0b1e91d4fbbcef6f1498be
Christian Fehmer <fehmer@users.noreply.github.com>
2024-02-07 16:11:07 +0100

    test: add jquery and mocks to frontend tests (#5013)

--- frontend/__tests__/setup-tests.ts
+++ frontend/__tests__/setup-tests.ts
@@ -1,4 +1,19 @@
+import { jest } from "@jest/globals";
+
+import $ from "jquery";
+// @ts-ignore
+global["$"] = $;
+// @ts-ignore
+global["jQuery"] = $;
+
 jest.mock("../src/ts/constants/env-config", () => ({
-  backendUrl: "invalid",
-  isDevelopment: true,
+  envConfig: {
+    backendUrl: "invalid",
+    isDevelopment: true,
+  },
+}));
+
+jest.mock("../src/ts/firebase", () => ({
+  app: undefined,
+  Auth: undefined,
 }));
