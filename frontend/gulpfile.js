acd980cf819380fee227e27c5d1656fd437f59fb
Miodec <jack@monkeytype.com>
2024-02-17 19:27:56 +0100

    remove unnecessary gulp scripts, move everything to vite

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,9 +1,5 @@
-const { webpack } = require("webpack");
 const eslint = require("gulp-eslint-new");
-const { task, src, series, watch } = require("gulp");
-const webpackDevConfig = require("./webpack/config.dev.js");
-const webpackProdConfig = require("./webpack/config.prod.js");
-
+const { task, src, series } = require("gulp");
 const JSONValidation = require("./scripts/json-validation");
 const eslintConfig = "../.eslintrc.json";
 
@@ -25,64 +21,16 @@ task("validate-json-schema", function () {
   return JSONValidation.validateAll();
 });
 
-const taskWithWebpackConfig = (webpackConfig) => {
-  return async () => {
-    return new Promise((resolve, reject) => {
-      webpack(webpackConfig, (err, stats) => {
-        if (err) {
-          return reject(err);
-        }
-        if (stats.hasErrors()) {
-          return reject(new Error(stats.compilation.errors.join("\n")));
-        }
-        console.log(
-          `Finished building in ${
-            (stats.endTime - stats.startTime) / 1000
-          } second(s)`
-        );
-        resolve();
-      });
-    });
-  };
-};
-
-task("webpack", taskWithWebpackConfig(webpackDevConfig));
-task("webpack-production", taskWithWebpackConfig(webpackProdConfig));
-
-task("compile", series("lint", "lint-json", "webpack"));
-
-task(
-  "compile-production",
-  series("lint", "lint-json", "validate-json-schema", "webpack-production")
-);
-
-task("watch", function () {
-  watch(["./src/ts/**/*.ts", "./src/ts/*.ts"], series("lint"));
-  watch(["./static/**/*.*", "./static/*.*"], series("lint-json"));
-});
-
-task("build", series("compile"));
-
-task("build-production", series("compile-production"));
-
 //PR CHECK
 
-task("validate-quote-json-schema", function () {
+task("pr-check-quote-json", function () {
   return JSONValidation.validateQuotes();
 });
-
-task("validate-language-json-schema", function () {
+task("pr-check-language-json", function () {
   return JSONValidation.validateLanguages();
 });
-
-task("validate-other-json-schema", function () {
+task("pr-check-other-json", function () {
   return JSONValidation.validateOthers();
 });
 
-task("pr-check-quote-json", series("validate-quote-json-schema"));
-task("pr-check-language-json", series("validate-language-json-schema"));
-task("pr-check-other-json", series("validate-other-json-schema"));
-
 task("pr-check-lint", series("lint"));
-
-task("pr-check-ts", series("webpack-production"));
