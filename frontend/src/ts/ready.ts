7f4814fd123a17e29a15c94eb1a2bb75515c0e7e
Miodec <jack@monkeytype.com>
2024-02-18 14:17:44 +0100

    remove workbox window

--- frontend/src/ts/ready.ts
+++ frontend/src/ts/ready.ts
@@ -8,7 +8,6 @@ import * as Focus from "./test/focus";
 import * as CookiePopup from "./popups/cookie-popup";
 import * as PSA from "./elements/psa";
 import * as ConnectionState from "./states/connection";
-import { Workbox } from "workbox-window";
 import * as FunboxList from "./test/funbox/funbox-list";
 //@ts-expect-error
 import Konami from "konami";
@@ -94,67 +93,6 @@ $(document).ready(() => {
 
   new Konami("https://keymash.io/");
 });
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    // disabling service workers on localhost - they dont really work well with local development
-    // and cause issues with hot reloading
-    if (Misc.isDevEnvironment()) {
-      void navigator.serviceWorker
-        .getRegistrations()
-        .then(function (registrations) {
-          for (const registration of registrations) {
-            // if (registration.scope !== "https://monkeytype.com/")
-            void registration.unregister();
-          }
-        });
-    } else {
-      const wb = new Workbox("/service-worker.js");
-
-      let updateBannerId: number;
-
-      // Add an event listener to detect when the registered
-      // service worker has installed but is waiting to activate.
-      wb.addEventListener("waiting", (event) => {
-        // set up a listener that will show a banner as soon as
-        // the previously waiting service worker has taken control.
-        wb.addEventListener("controlling", (event2) => {
-          if (
-            (event.isUpdate || event2.isUpdate) &&
-            updateBannerId === undefined
-          ) {
-            // updateBannerId = Notifications.addBanner(
-            //   "Update ready - please refresh",
-            //   1,
-            //   "gift",
-            //   true
-            // );
-          }
-        });
-
-        wb.messageSkipWaiting();
-      });
-
-      wb.register()
-        .then((registration) => {
-          // if (registration?.waiting) {
-          //   //@ts-ignore
-          //   registration?.onupdatefound = (): void => {
-          //     Notifications.add("Downloading update...", 1, 0, "Update");
-          //   };
-          // }
-          console.log("Service worker registration succeeded:", registration);
-
-          setInterval(() => {
-            void wb.update(); //check for updates every 15 minutes
-          }, 900000);
-        })
-        .catch((e) => {
-          console.log("Service worker registration failed:", e);
-        });
-    }
-  });
-}
 
 window.onerror = function (message, url, line, column, error): void {
   void log("error", {
