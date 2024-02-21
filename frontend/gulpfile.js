bfc57e94b2449104afaa554af0da094f94c0015b
Miodec <jack@monkeytype.com>
2024-02-21 16:43:25 +0100

    fix gulp tasks not working

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,36 +1,43 @@
-const eslint = require("gulp-eslint-new");
-const { task, src, series } = require("gulp");
-const JSONValidation = require("./scripts/json-validation");
+import eslint, { format, failAfterError } from "gulp-eslint-new";
+import gulp from "gulp";
+import {
+  validateAll,
+  validateQuotes,
+  validateLanguages,
+  validateOthers,
+} from "./scripts/json-validation.cjs";
 const eslintConfig = "../.eslintrc.json";
+
+const { task, src, series } = gulp;
 
 task("lint", function () {
   return src(["./src/ts/**/*.ts"])
     .pipe(eslint(eslintConfig))
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+    .pipe(format())
+    .pipe(failAfterError());
 });
 
 task("lint-json", function () {
   return src("./static/**/*.json")
     .pipe(eslint(eslintConfig))
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+    .pipe(format())
+    .pipe(failAfterError());
 });
 
 task("validate-json-schema", function () {
-  return JSONValidation.validateAll();
+  return validateAll();
 });
 
 //PR CHECK
 
 task("pr-check-quote-json", function () {
-  return JSONValidation.validateQuotes();
+  return validateQuotes();
 });
 task("pr-check-language-json", function () {
-  return JSONValidation.validateLanguages();
+  return validateLanguages();
 });
 task("pr-check-other-json", function () {
-  return JSONValidation.validateOthers();
+  return validateOthers();
 });
 
 task("pr-check-lint", series("lint"));
