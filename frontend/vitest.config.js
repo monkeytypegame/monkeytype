107ba52aa7e1ad7941944edfbc9e3253443dfcf5
Christian Fehmer <cfe@sexy-developer.com>
2024-02-23 12:28:48 +0100

    vscode config

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: "happy-dom",
     setupFiles: ["__tests__/setup-tests.ts"],
 
     coverage: {
