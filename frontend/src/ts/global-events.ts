2d68cde26cef16842d07a2125baeea4446ddee8f
Miodec <jack@monkeytype.com>
2024-02-25 21:43:54 +0100

    add other ways to show the commandline

--- frontend/src/ts/global-events.ts
+++ frontend/src/ts/global-events.ts
@@ -1,21 +1,30 @@
 import * as Commandline from "./commandline/commandline";
 import * as Misc from "./utils/misc";
 import * as PageTransition from "./states/page-transition";
-
-function handleEscape(e: KeyboardEvent): void {
-  const popupVisible = Misc.isAnyPopupVisible();
-  const miniResultPopupVisible = Misc.isElementVisible(
-    ".pageAccount .miniResultChartWrapper"
-  );
-  if (!popupVisible && !miniResultPopupVisible) {
-    Commandline.show();
-  }
-}
+import Config from "./config";
+import * as TestWords from "./test/test-words";
 
 document.addEventListener("keydown", (e) => {
   if (PageTransition.get()) return;
 
-  if (e.key === "Escape") {
-    handleEscape(e);
+  if (
+    (e.key === "Escape" && Config.quickRestart !== "esc") ||
+    (e.key === "Tab" &&
+      Config.quickRestart === "esc" &&
+      !TestWords.hasTab &&
+      !e.shiftKey) ||
+    (e.key === "Tab" &&
+      Config.quickRestart === "esc" &&
+      TestWords.hasTab &&
+      e.shiftKey) ||
+    (e.key.toLowerCase() === "p" && (e.metaKey || e.ctrlKey) && e.shiftKey)
+  ) {
+    const popupVisible = Misc.isAnyPopupVisible();
+    const miniResultPopupVisible = Misc.isElementVisible(
+      ".pageAccount .miniResultChartWrapper"
+    );
+    if (!popupVisible && !miniResultPopupVisible) {
+      Commandline.show();
+    }
   }
 });
