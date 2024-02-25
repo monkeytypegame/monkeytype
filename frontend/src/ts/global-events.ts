3850e2181a94ff7b32b90531ad4af959106a88a9
Miodec <jack@monkeytype.com>
2024-02-25 21:32:47 +0100

    only showing when nothing is already visible

--- frontend/src/ts/global-events.ts
+++ frontend/src/ts/global-events.ts
@@ -1,7 +1,14 @@
 import * as Commandline from "./commandline/commandline";
+import * as Misc from "./utils/misc";
 
 function handleEscape(e: KeyboardEvent): void {
-  Commandline.show();
+  const popupVisible = Misc.isAnyPopupVisible();
+  const miniResultPopupVisible = Misc.isElementVisible(
+    ".pageAccount .miniResultChartWrapper"
+  );
+  if (!popupVisible && !miniResultPopupVisible) {
+    Commandline.show();
+  }
 }
 
 document.addEventListener("keydown", (e) => {
