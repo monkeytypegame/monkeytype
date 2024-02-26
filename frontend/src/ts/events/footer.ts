31903954239d7e09f955c52e21e3e397c7a34e78
Miodec <jack@monkeytype.com>
2024-02-26 22:34:54 +0100

    add footer events

---
+++ frontend/src/ts/events/footer.ts
@@ -0,0 +1,45 @@
+import * as Commandline from "../commandline/commandline";
+import * as CommandlineLists from "../commandline/commands";
+import Config, * as UpdateConfig from "../config";
+import { isAuthenticated } from "../firebase";
+import * as DB from "../db";
+import * as Notifications from "../elements/notifications";
+
+document
+  .querySelector("footer #commandLineButton")
+  ?.addEventListener("click", () => {
+    Commandline.show({
+      subgroupOverride: CommandlineLists.commands,
+    });
+  });
+
+document
+  .querySelector("footer .right .current-theme")
+  ?.addEventListener("click", (event) => {
+    const e = event as MouseEvent;
+    if (e.shiftKey) {
+      if (!Config.customTheme) {
+        if (isAuthenticated()) {
+          if ((DB.getSnapshot()?.customThemes?.length ?? 0) < 1) {
+            Notifications.add("No custom themes!", 0);
+            UpdateConfig.setCustomTheme(false);
+            // UpdateConfig.setCustomThemeId("");
+            return;
+          }
+          // if (!DB.getCustomThemeById(Config.customThemeId)) {
+          //   // Turn on the first custom theme
+          //   const firstCustomThemeId = DB.getSnapshot().customThemes[0]._id;
+          //   UpdateConfig.setCustomThemeId(firstCustomThemeId);
+          // }
+        }
+        UpdateConfig.setCustomTheme(true);
+      } else UpdateConfig.setCustomTheme(false);
+    } else {
+      const subgroup = Config.customTheme
+        ? CommandlineLists.getList("customThemesList")
+        : CommandlineLists.getList("themes");
+      Commandline.show({
+        subgroupOverride: subgroup,
+      });
+    }
+  });
