2685af28b6f03756d5c5000ba98f52bf07285fde
Miodec <jack@monkeytype.com>
2024-03-04 16:42:36 +0100

    catching errors and notifying user

--- frontend/src/ts/utils/async-modules.ts
+++ frontend/src/ts/utils/async-modules.ts
@@ -1,5 +1,19 @@
+import * as Loader from "../elements/loader";
+import * as Notifications from "../elements/notifications";
+import { createErrorMessage } from "./misc";
+
 export async function getCommandline(): Promise<
   typeof import("../commandline/commandline.js")
 > {
-  return await import("../commandline/commandline.js");
+  try {
+    Loader.show();
+    const module = await import("../commandline/commandline.js");
+    Loader.hide();
+    return module;
+  } catch (e) {
+    Loader.hide();
+    const msg = createErrorMessage(e, "Failed to load commandline module");
+    Notifications.add(msg, -1);
+    throw e;
+  }
 }
