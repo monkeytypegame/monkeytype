8eb3206c8ad2f8137dc79daab9d1f7b6ed3fe818
Christian Fehmer <fehmer@users.noreply.github.com>
2024-03-11 20:29:18 +0100

    refactor: get actual rank with the user data instead of using lbmemory (fehmer) (#5189)
    Co-authored-by: Jack <jack@monkeytype.com>

--- shared-types/types.d.ts
+++ shared-types/types.d.ts
@@ -531,6 +531,7 @@ declare namespace SharedTypes {
     quoteRatings?: UserQuoteRatings;
     favoriteQuotes?: Record<string, string[]>;
     lbMemory?: UserLbMemory;
+    allTimeLbs: AllTimeLbs;
     inventory?: UserInventory;
     banned?: boolean;
     lbOptOut?: boolean;
@@ -578,6 +579,7 @@ declare namespace SharedTypes {
     | "inventory"
     | "uid"
     | "isPremium"
+    | "allTimeLbs"
   > & {
     typingStats: {
       completedTests: User["completedTests"];
@@ -587,9 +589,6 @@ declare namespace SharedTypes {
     streak: UserStreak["length"];
     maxStreak: UserStreak["maxLength"];
     details: UserProfileDetails;
-    allTimeLbs: {
-      time: Record<string, Record<string, number | null>>;
-    };
     personalBests: {
       time: Pick<
         Record<`${number}`, SharedTypes.PersonalBest[]>,
@@ -600,5 +599,9 @@ declare namespace SharedTypes {
         "10" | "25" | "50" | "100"
       >;
     };
+  };
+
+  type AllTimeLbs = {
+    time: Record<string, Record<string, number | null>>;
   };
 }
