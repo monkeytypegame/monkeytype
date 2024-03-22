2de7f63b8f9277f5dac9f1f05bed2aba083d948a
Estifanos Gashawtena <88879001+Estifanos12@users.noreply.github.com>
2024-03-22 18:39:25 +0300

    Update account-controller.ts

--- frontend/src/ts/controllers/account-controller.ts
+++ frontend/src/ts/controllers/account-controller.ts
@@ -593,9 +593,11 @@ async function signUp(): Promise<void> {
     }
     Notifications.add("Account created", 1);
   } catch (e) {
+    
     let message = Misc.createErrorMessage(e, "Failed to create account");
+    
     if(e.code === "auth/email-already-in-use"){
-      message = Misc.createErrorMessage({ message: "Email already exists"}, "Failed to create account");
+      message = Misc.createErrorMessage({message: "Email already exists"}, "Failed to create account");
     }
     Notifications.add(message, -1);
     LoginPage.hidePreloader();
