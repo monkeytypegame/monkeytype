a8deb73196f9e1325caa80fb28d79568827b2085
Christian Fehmer <cfe@sexy-developer.com>
2024-03-25 17:32:48 +0100

    refactor: rewrite page constructor to use an object

--- frontend/src/ts/pages/404.ts
+++ frontend/src/ts/pages/404.ts
@@ -1,22 +1,16 @@
 import Page from "./page";
 import * as Skeleton from "../utils/skeleton";
 
-export const page = new Page(
-  "404",
-  $(".page.page404"),
-  "/404",
-  async () => {
-    //
-  },
-  async () => {
+export const page = new Page({
+  name: "404",
+  element: $(".page.page404"),
+  pathName: "/404",
+  afterHide: async (): Promise<void> => {
     Skeleton.remove("page404");
   },
-  async () => {
+  beforeShow: async (): Promise<void> => {
     Skeleton.append("page404", "main");
   },
-  async () => {
-    //
-  }
-);
+});
 
 Skeleton.save("page404");
