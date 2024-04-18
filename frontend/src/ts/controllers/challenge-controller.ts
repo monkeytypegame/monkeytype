d93bf02549479d16e94896d52168f77e0e329d96
Miodec <jack@monkeytype.com>
2024-04-18 20:30:10 +0200

    fix challenges

--- frontend/src/ts/controllers/challenge-controller.ts
+++ frontend/src/ts/controllers/challenge-controller.ts
@@ -248,17 +248,15 @@ export async function setup(challengeName: string): Promise<boolean> {
       UpdateConfig.setMode("words", true);
       UpdateConfig.setDifficulty("normal", true);
     } else if (challenge.type === "customText") {
-      // CustomText.setDelimiter(" ");
-      CustomText.setPipeDelimiter(false);
       CustomText.setText((challenge.parameters[0] as string).split(" "));
-      // CustomText.setIsTimeRandom(false);
-      // CustomText.setIsSectionRandom(false);
-      // CustomText.setIsWordRandom(challenge.parameters[1] as boolean);
-      // CustomText.setWord(challenge.parameters[2] as number);
-      // CustomText.setTime(-1);
-      // CustomText.setLimitMode()
-      //todo fix this
-      Notifications.add("TODO: Implement customText challenge", 0);
+      CustomText.setMode(
+        challenge.parameters[1] as string as MonkeyTypes.CustomTextMode
+      );
+      CustomText.setLimitValue(challenge.parameters[2] as number);
+      CustomText.setLimitMode(
+        challenge.parameters[3] as string as MonkeyTypes.CustomTextLimitMode
+      );
+      CustomText.setPipeDelimiter(challenge.parameters[4] as boolean);
       UpdateConfig.setMode("custom", true);
       UpdateConfig.setDifficulty("normal", true);
     } else if (challenge.type === "script") {
@@ -272,15 +270,10 @@ export async function setup(challengeName: string): Promise<boolean> {
       let text = scriptdata.trim();
       text = text.replace(/[\n\r\t ]/gm, " ");
       text = text.replace(/ +/gm, " ");
-      // CustomText.setDelimiter(" ");
-      // CustomText.setText(text.split(" "));
-      // CustomText.setIsWordRandom(false);
-      // CustomText.setIsSectionRandom(false);
-      // CustomText.setIsTimeRandom(false);
-      // CustomText.setTime(-1);
-      // CustomText.setWord(-1);
-      //todo fix this
-      Notifications.add("TODO: Implement customText challenge", 0);
+      CustomText.setText(text.split(" "));
+      CustomText.setMode("repeat");
+      CustomText.setLimitMode("word");
+      CustomText.setPipeDelimiter(false);
       UpdateConfig.setMode("custom", true);
       UpdateConfig.setDifficulty("normal", true);
       if (challenge.parameters[1] !== null) {
