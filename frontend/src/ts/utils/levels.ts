af85f6f0bb12f7afb60b03c9453feacc8fc9d3a5
Nad Alaba <37968805+NadAlaba@users.noreply.github.com>
2024-05-09 20:19:26 +0300

    new xp details calculation based on same level-xp-formula

--- frontend/src/ts/utils/levels.ts
+++ frontend/src/ts/utils/levels.ts
@@ -15,3 +15,31 @@ export function getLevel(xp: number): number {
 export function getXpForLevel(level: number): number {
   return 49 * (level - 1) + 100;
 }
+
+/**
+ * Calculates the total experience points to reach the given level.
+ * @param level The level.
+ * @returns The total experience points required to reach the level.
+ */
+function getTotalXpOfLastLevelling(level: number): number {
+  return (49 * Math.pow(level, 2) + 53 * level - 102) / 2;
+}
+
+/**
+ * Calculates the details of experience points based on given total xp.
+ * @param totalXp The total experience points.
+ * @returns An object with the following properties: calculated level (floored),
+ *  partial xp of the level, and required xp for level completion.
+ */
+export function getXpDetails(totalXp: number): {
+  level: number;
+  levelXp: number;
+  requiredXpForLevel: number;
+} {
+  const level = Math.floor(getLevel(totalXp));
+  return {
+    level: level,
+    levelXp: totalXp - getTotalXpOfLastLevelling(level),
+    requiredXpForLevel: getXpForLevel(level),
+  };
+}
