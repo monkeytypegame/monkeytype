d733996b092ccf4795a2d014ada5d40543ed77d3
Christian Fehmer <fehmer@users.noreply.github.com>
2024-06-17 16:35:44 +0200

    impr(custom theme rename modal): automatically select input (@fehmer) (#5487)

--- frontend/src/ts/modals/simple-modals.ts
+++ frontend/src/ts/modals/simple-modals.ts
@@ -469,8 +469,8 @@ class SimpleModal {
     activePopup = this;
     this.parameters = parameters;
     void modal.show({
-      ...showOptions,
       focusFirstInput: true,
+      ...showOptions,
       beforeAnimation: async () => {
         this.beforeInitFn?.(this);
         this.init();
@@ -1926,7 +1926,9 @@ $(".pageSettings").on(
   (e) => {
     const $parentElement = $(e.currentTarget).parent(".customTheme.button");
     const customThemeId = $parentElement.attr("customThemeId") as string;
-    showPopup("updateCustomTheme", [customThemeId]);
+    showPopup("updateCustomTheme", [customThemeId], {
+      focusFirstInput: "focusAndSelect",
+    });
   }
 );
 
