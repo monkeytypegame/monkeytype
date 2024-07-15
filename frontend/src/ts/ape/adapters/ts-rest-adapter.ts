cf7ebf98fcf5053115fda2133080ec20578d0fd2
Christian Fehmer <cfe@sexy-developer.com>
2024-07-15 15:46:09 +0200

    ts-rest-adapter: don't send authorization for public endpoints

--- frontend/src/ts/ape/adapters/ts-rest-adapter.ts
+++ frontend/src/ts/ape/adapters/ts-rest-adapter.ts
@@ -3,6 +3,7 @@ import { Method } from "axios";
 import { getIdToken } from "firebase/auth";
 import { envConfig } from "../../constants/env-config";
 import { getAuthenticatedUser, isAuthenticated } from "../../firebase";
+import type { Metadata } from "shared/schemas/types";
 
 function buildApi(timeout: number): (args: ApiFetcherArgs) => Promise<{
   status: number;
@@ -10,18 +11,25 @@ function buildApi(timeout: number): (args: ApiFetcherArgs) => Promise<{
   headers: Headers;
 }> {
   return async (request: ApiFetcherArgs) => {
-    const token = isAuthenticated()
-      ? await getIdToken(getAuthenticatedUser())
-      : "";
+    const isPublicEndpoint =
+      (request.route.metadata as Metadata | undefined)?.auth?.isPublic ?? false;
+
     try {
+      const headers: HeadersInit = {
+        ...request.headers,
+        "X-Client-Version": envConfig.clientVersion,
+      };
+      if (!isPublicEndpoint) {
+        const token = isAuthenticated()
+          ? await getIdToken(getAuthenticatedUser())
+          : "";
+
+        headers["Authorization"] = `Bearer ${token}`;
+      }
       const response = await fetch(request.path, {
         signal: AbortSignal.timeout(timeout),
         method: request.method as Method,
-        headers: {
-          ...request.headers,
-          Authorization: `Bearer ${token}`,
-          "X-Client-Version": envConfig.clientVersion,
-        },
+        headers,
         body: request.body,
       });
       const body = await response.json();
