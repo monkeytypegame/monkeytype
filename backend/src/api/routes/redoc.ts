1751b1ae5d188f896b58e153eaac4b100cb0d2b8
Christian Fehmer <cfe@sexy-developer.com>
2024-07-17 12:11:20 +0200

    fix redocly

--- backend/src/api/routes/redoc.ts
+++ backend/src/api/routes/redoc.ts
@@ -1,25 +1,19 @@
 import { Application } from "express";
+
 export function addRedocMiddlewares(app: Application): void {
+  const root = __dirname + "../../../../static";
   app.use("/v2/docs-internal", (req, res) => {
-    res.sendFile("api/internal.html", {
-      root: __dirname + "../../../../build/static",
-    });
+    res.sendFile("api/internal.html", { root });
   });
   app.use("/v2/docs-internal.json", (req, res) => {
     res.setHeader("Content-Type", "application/json");
-    res.sendFile("api/openapi.json", {
-      root: __dirname + "../../../../build/static",
-    });
+    res.sendFile("api/openapi.json", { root });
   });
   app.use("/v2/docs", (req, res) => {
-    res.sendFile("api/public.html", {
-      root: __dirname + "../../../../build/static",
-    });
+    res.sendFile("api/public.html", { root });
   });
   app.use("/v2/docs.json", (req, res) => {
     res.setHeader("Content-Type", "application/json");
-    res.sendFile("api/public.json", {
-      root: __dirname + "../../../../build/static",
-    });
+    res.sendFile("api/public.json", { root });
   });
 }
