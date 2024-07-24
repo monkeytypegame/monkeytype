a3cba0601340187c9fa6c922a4a7fb38d4151110
Christian Fehmer <cfe@sexy-developer.com>
2024-07-24 09:30:09 +0200

    fix?

--- frontend/src/ts/utils/config.ts
+++ frontend/src/ts/utils/config.ts
@@ -1,8 +1,9 @@
 import { Config, ConfigValue } from "@monkeytype/shared-types/config";
 import DefaultConfig from "../constants/default-config";
 import { typedKeys } from "./misc";
+import { PartialConfig } from "@monkeytype/contracts/schemas/configs";
 
-export function mergeWithDefaultConfig(config: Partial<Config>): Config {
+export function mergeWithDefaultConfig(config: PartialConfig): Config {
   const mergedConfig = {} as Config;
   for (const key of typedKeys(DefaultConfig)) {
     const newValue = config[key] ?? (DefaultConfig[key] as ConfigValue);
