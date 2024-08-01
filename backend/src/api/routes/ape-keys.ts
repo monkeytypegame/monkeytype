8a09acd8d50c7429204262c27ddbd43fe88bfb4e
Christian Fehmer <fehmer@users.noreply.github.com>
2024-08-01 13:29:25 +0200

    impr: use tsrest for ape-keys endpoint (@fehmer) (#5706)
    !nuf

--- backend/src/api/routes/ape-keys.ts
+++ backend/src/api/routes/ape-keys.ts
@@ -1,91 +1,42 @@
-import joi from "joi";
-import { Router } from "express";
-import { authenticateRequest } from "../../middlewares/auth";
-import * as ApeKeyController from "../controllers/ape-key";
+import { apeKeysContract } from "@monkeytype/contracts/ape-keys";
+import { initServer } from "@ts-rest/express";
 import * as RateLimit from "../../middlewares/rate-limit";
+import * as ApeKeyController from "../controllers/ape-key";
+import { callController } from "../ts-rest-adapter";
 import { checkUserPermissions } from "../../middlewares/permission";
 import { validate } from "../../middlewares/configuration";
-import { asyncHandler } from "../../middlewares/utility";
-import { validateRequest } from "../../middlewares/validation";
 
-const apeKeyNameSchema = joi
-  .string()
-  .regex(/^[0-9a-zA-Z_.-]+$/)
-  .max(20)
-  .messages({
-    "string.pattern.base": "Invalid ApeKey name",
-    "string.max": "ApeKey name exceeds maximum of 20 characters",
-  });
-
-const checkIfUserCanManageApeKeys = checkUserPermissions({
-  criteria: (user) => {
-    // Must be an exact check
-    return user.canManageApeKeys !== false;
-  },
-  invalidMessage: "You have lost access to ape keys, please contact support",
-});
-
-const router = Router();
-
-router.use(
+const commonMiddleware = [
   validate({
     criteria: (configuration) => {
       return configuration.apeKeys.endpointsEnabled;
     },
     invalidMessage: "ApeKeys are currently disabled.",
-  })
-);
-
-router.get(
-  "/",
-  authenticateRequest(),
-  RateLimit.apeKeysGet,
-  checkIfUserCanManageApeKeys,
-  asyncHandler(ApeKeyController.getApeKeys)
-);
-
-router.post(
-  "/",
-  authenticateRequest(),
-  RateLimit.apeKeysGenerate,
-  checkIfUserCanManageApeKeys,
-  validateRequest({
-    body: {
-      name: apeKeyNameSchema.required(),
-      enabled: joi.boolean().required(),
-    },
   }),
-  asyncHandler(ApeKeyController.generateApeKey)
-);
-
-router.patch(
-  "/:apeKeyId",
-  authenticateRequest(),
-  RateLimit.apeKeysUpdate,
-  checkIfUserCanManageApeKeys,
-  validateRequest({
-    params: {
-      apeKeyId: joi.string().token().required(),
+  checkUserPermissions({
+    criteria: (user) => {
+      return user.canManageApeKeys ?? false;
     },
-    body: {
-      name: apeKeyNameSchema,
-      enabled: joi.boolean(),
-    },
+    invalidMessage: "You have lost access to ape keys, please contact support",
   }),
-  asyncHandler(ApeKeyController.editApeKey)
-);
+];
 
-router.delete(
-  "/:apeKeyId",
-  authenticateRequest(),
-  RateLimit.apeKeysDelete,
-  checkIfUserCanManageApeKeys,
-  validateRequest({
-    params: {
-      apeKeyId: joi.string().token().required(),
-    },
-  }),
-  asyncHandler(ApeKeyController.deleteApeKey)
-);
-
-export default router;
+const s = initServer();
+export default s.router(apeKeysContract, {
+  get: {
+    middleware: [...commonMiddleware, RateLimit.apeKeysGet],
+    handler: async (r) => callController(ApeKeyController.getApeKeys)(r),
+  },
+  add: {
+    middleware: [...commonMiddleware, RateLimit.apeKeysGenerate],
+    handler: async (r) => callController(ApeKeyController.generateApeKey)(r),
+  },
+  save: {
+    middleware: [...commonMiddleware, RateLimit.apeKeysUpdate],
+    handler: async (r) => callController(ApeKeyController.editApeKey)(r),
+  },
+  delete: {
+    middleware: [...commonMiddleware, RateLimit.apeKeysDelete],
+    handler: async (r) => callController(ApeKeyController.deleteApeKey)(r),
+  },
+});
