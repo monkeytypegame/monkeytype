b14827f564a5e69f23342003036ca17f3756367d
Christian Fehmer <cfe@sexy-developer.com>
2024-08-09 12:58:37 +0300

    move csp for docs into docs route

--- backend/src/api/routes/index.ts
+++ backend/src/api/routes/index.ts
@@ -139,13 +139,6 @@ function applyApiRoutes(app: Application): void {
         return;
       }
 
-      if (req.path.startsWith("/docs")) {
-        res.setHeader(
-          "Content-Security-Policy",
-          "default-src 'self';base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' monkeytype.com cdn.redoc.ly data:;object-src 'none';script-src 'self' cdn.redoc.ly 'unsafe-inline'; worker-src blob: data;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests"
-        );
-      }
-
       next();
     }
   );
