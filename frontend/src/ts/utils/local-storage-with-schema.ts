0ba1341caf649d8a664ceb86711a6a65cb8a3f1d
Miodec <jack@monkeytype.com>
2024-08-10 22:47:24 +0200

    error, passing zod issues through

--- frontend/src/ts/utils/local-storage-with-schema.ts
+++ frontend/src/ts/utils/local-storage-with-schema.ts
@@ -1,14 +1,16 @@
+import { ZodIssue } from "zod";
+
 export class LocalStorageWithSchema<T> {
   private key: string;
   private schema: Zod.Schema<T>;
   private fallback: T;
-  private migrate?: (value: unknown) => T;
+  private migrate?: (value: unknown, zodIssues: ZodIssue[]) => T;
 
   constructor(
     key: string,
     schema: Zod.Schema<T>,
     fallback: T,
-    migrate?: (value: unknown) => T
+    migrate?: (value: unknown, zodIssues: ZodIssue[]) => T
   ) {
     this.key = key;
     this.schema = schema;
@@ -27,6 +29,10 @@ export class LocalStorageWithSchema<T> {
     try {
       jsonParsed = JSON.parse(value);
     } catch (e) {
+      console.error(
+        `Value from localStorage ${this.key} was not a valid JSON, using fallback`,
+        e
+      );
       window.localStorage.removeItem(this.key);
       return this.fallback;
     }
@@ -41,7 +47,8 @@ export class LocalStorageWithSchema<T> {
       `Value from localStorage ${this.key} failed schema validation, migrating`,
       schemaParsed.error
     );
-    const newValue = this.migrate?.(jsonParsed) ?? this.fallback;
+    const newValue =
+      this.migrate?.(jsonParsed, schemaParsed.error.issues) ?? this.fallback;
     window.localStorage.setItem(this.key, JSON.stringify(newValue));
     return newValue;
   }
