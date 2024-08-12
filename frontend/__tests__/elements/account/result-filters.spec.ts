68f22bb0f9e6c13db408b3a2cee8872a9cb7f025
Miodec <jack@monkeytype.com>
2024-08-12 16:37:01 +0200

    update tests

--- frontend/__tests__/elements/account/result-filters.spec.ts
+++ frontend/__tests__/elements/account/result-filters.spec.ts
@@ -1,25 +1,73 @@
-import {
-  defaultResultFilters,
-  mergeWithDefaultFilters,
-} from "../../../src/ts/elements/account/result-filters";
+import defaultResultFilters from "../../../src/ts/constants/default-result-filters";
+import { mergeWithDefaultFilters } from "../../../src/ts/elements/account/result-filters";
 
 describe("result-filters.ts", () => {
   describe("mergeWithDefaultFilters", () => {
     it("should merge with default filters correctly", () => {
-      const merged = mergeWithDefaultFilters({
-        pb: {
-          no: false,
-          yes: false,
+      const tests = [
+        {
+          input: {
+            pb: {
+              no: false,
+              yes: false,
+            },
+          },
+          expected: () => {
+            const expected = defaultResultFilters;
+            expected.pb.no = false;
+            expected.pb.yes = false;
+            return expected;
+          },
         },
+        {
+          input: {
+            words: {
+              "10": false,
+            },
+          },
+          expected: () => {
+            const expected = defaultResultFilters;
+            expected.words["10"] = false;
+            return expected;
+          },
+        },
+        {
+          input: {
+            blah: true,
+          },
+          expected: () => {
+            return defaultResultFilters;
+          },
+        },
+        {
+          input: 1,
+          expected: () => {
+            return defaultResultFilters;
+          },
+        },
+        {
+          input: null,
+          expected: () => {
+            return defaultResultFilters;
+          },
+        },
+        {
+          input: undefined,
+          expected: () => {
+            return defaultResultFilters;
+          },
+        },
+        {
+          input: {},
+          expected: () => {
+            return defaultResultFilters;
+          },
+        },
+      ];
+      tests.forEach((test) => {
+        const merged = mergeWithDefaultFilters(test.input as any);
+        expect(merged).toEqual(test.expected());
       });
-
-      const expected = JSON.parse(JSON.stringify(defaultResultFilters));
-      expected.pb.no = false;
-      expected.pb.yes = false;
-
-      expect(merged).toEqual(expected);
-
-      console.log(defaultResultFilters);
     });
   });
 });
