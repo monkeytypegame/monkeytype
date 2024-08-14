b50701ee2ab4caa79fade58d043b338b9b7d46b4
Miodec <jack@monkeytype.com>
2024-08-14 13:54:08 +0200

    chore: hide merch banner for now
    !nuf

--- frontend/src/ts/elements/merch-banner.ts
+++ frontend/src/ts/elements/merch-banner.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { LocalStorageWithSchema } from "../utils/local-storage-with-schema";
-import * as Notifications from "./notifications";
+// import * as Notifications from "./notifications";
 
 const closed = new LocalStorageWithSchema({
   key: "merchBannerClosed",
@@ -10,15 +10,15 @@ const closed = new LocalStorageWithSchema({
 
 export function showIfNotClosedBefore(): void {
   if (!closed.get()) {
-    Notifications.addBanner(
-      `Check out our merchandise, available at <a target="_blank" rel="noopener" href="https://monkeytype.store/">monkeytype.store</a>`,
-      1,
-      "./images/merch2.png",
-      false,
-      () => {
-        closed.set(true);
-      },
-      true
-    );
+    // Notifications.addBanner(
+    //   `Check out our merchandise, available at <a target="_blank" rel="noopener" href="https://monkeytype.store/">monkeytype.store</a>`,
+    //   1,
+    //   "./images/merch2.png",
+    //   false,
+    //   () => {
+    //     closed.set(true);
+    //   },
+    //   true
+    // );
   }
 }
