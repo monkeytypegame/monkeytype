495d99c08bc7462514362812c560f43a6b63ce93
Christian Fehmer <cfe@sexy-developer.com>
2024-09-23 13:36:41 +0300

    fix: handling of characters outside the BMP (@fehmer)

--- frontend/src/ts/utils/strings.ts
+++ frontend/src/ts/utils/strings.ts
@@ -149,3 +149,18 @@ export function cleanTypographySymbols(textToClean: string): string {
     (char) => specials[char as keyof typeof specials] || ""
   );
 }
+
+/**
+ * Split a string into characters. This supports multi-byte characters outside of the [Basic Multilinugal Plane](https://en.wikipedia.org/wiki/Plane_(Unicode).
+ * Using  `string.length` and `string[index]` does not work.
+ * @param s string to be tokenized into characters
+ * @returns array of characters
+ */
+export function splitIntoCharacters(s: string): string[] {
+  const result: string[] = [];
+  for (const t of s) {
+    result.push(t);
+  }
+
+  return result;
+}
