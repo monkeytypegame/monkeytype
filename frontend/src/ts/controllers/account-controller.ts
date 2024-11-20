4676d118036ee76aa9dbad2199dd361d86af2957
Miodec <jack@monkeytype.com>
2024-11-20 19:08:32 +0100

    fix imports

--- frontend/src/ts/controllers/account-controller.ts
+++ frontend/src/ts/controllers/account-controller.ts
@@ -20,7 +20,7 @@ import * as URLHandler from "../utils/url-handler";
 import * as Account from "../pages/account";
 import * as Alerts from "../elements/alerts";
 import * as AccountSettings from "../pages/account-settings";
-import { getAllFunboxes } from "@monkeytype/funbox/list";
+import { getList as getAllFunboxes } from "@monkeytype/funbox/list";
 import {
   GoogleAuthProvider,
   GithubAuthProvider,
