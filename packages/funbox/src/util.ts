182c2970a6b65988dc70fd0c2b5b77dec2895445
Miodec <jack@monkeytype.com>
2024-11-21 11:32:45 +0100

    getting names from list

--- packages/funbox/src/util.ts
+++ packages/funbox/src/util.ts
@@ -5,8 +5,9 @@ export function stringToFunboxNames(names: string): FunboxName[] {
   if (names === "none" || names === "") return [];
   const unsafeNames = names.split("#").map((name) => name.trim());
   const out: FunboxName[] = [];
+  const list = getList().map((f) => f.name);
   for (const unsafeName of unsafeNames) {
-    if (unsafeName in getList()) {
+    if (list.includes(unsafeName as FunboxName)) {
       out.push(unsafeName as FunboxName);
     } else {
       throw new Error("Invalid funbox name: " + unsafeName);
