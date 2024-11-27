ea153f71099ce7b7b5834df81dd33ec7cfd71c58
Miodec <jack@monkeytype.com>
2024-11-27 22:46:55 +0100

    refactor test

--- frontend/__tests__/test/funbox.spec.ts
+++ frontend/__tests__/test/funbox.spec.ts
@@ -4,21 +4,20 @@ describe("funbox", () => {
   describe("list", () => {
     it("should have every frontendFunctions function defined", () => {
       for (const funbox of getAllFunboxes()) {
-        const frontendFunctions = funbox.frontendFunctions ?? [];
-        const functionImplementations = Object.keys(funbox.functions ?? {});
+        const packageFunctions = (funbox.frontendFunctions ?? []).sort();
+        const implementations = Object.keys(funbox.functions ?? {}).sort();
 
-        expect(
-          frontendFunctions
-            .map((fn) => functionImplementations.includes(fn))
-            .every((x) => x),
-          `Funbox functions mismatch for ${
-            funbox.name
-          }. Make sure that all frontendFunctions are implemented in the frontend, and that all function keys are added in the package.\nPackage:\t${frontendFunctions.join(
-            ", "
-          )}\nFrontend:\t${functionImplementations.join(
-            ", "
-          )}\n\n(make sure the package rebuilds before rerunning the tests)\n\n`
-        ).toBe(true);
+        let message = "has mismatched functions";
+
+        if (packageFunctions.length > implementations.length) {
+          message = `missing function implementation in frontend`;
+        } else if (implementations.length > packageFunctions.length) {
+          message = `missing properties in frontendFunctions in the package`;
+        }
+
+        expect(packageFunctions, `Funbox ${funbox.name} ${message}`).toEqual(
+          implementations
+        );
       }
     });
   });
