8a41ccee970edfbed34013cd3eb522c1d5ad346f
Christian Fehmer <fehmer@users.noreply.github.com>
2025-02-21 16:52:20 +0100

    fix: past leaderboard not fetching the users rank (@fehmer) (#6289)
    Show the users ranking for the last day on the daily and for the last
    week on the weekly leaderboard correctly.
    
    - Fix request query schema for the [daily
    rank](https://api.monkeytype.com/docs/internal#tag/leaderboards/operation/leaderboards.getDailyRank)
    having pagination
    - Fix request query schema for the [weekly
    rank](https://api.monkeytype.com/docs/internal#tag/leaderboards/operation/leaderboards.getWeeklyXpRank)
    missing the `weeksBefore` parameter
    - Fix frontend to include the `daysBefore` or `weeksBefore` parameter on
    `rank` calls
    
    ---------
    
    Co-authored-by: Miodec <jack@monkeytype.com>

--- frontend/src/ts/pages/leaderboards.ts
+++ frontend/src/ts/pages/leaderboards.ts
@@ -246,6 +246,7 @@ async function requestData(update = false): Promise<void> {
         requests.rank = Ape.leaderboards.getDailyRank({
           query: {
             ...baseQuery,
+            daysBefore: state.yesterday ? 1 : undefined,
           },
         });
       }
@@ -324,7 +325,11 @@ async function requestData(update = false): Promise<void> {
     });
 
     if (isAuthenticated() && state.userData === null) {
-      requests.rank = Ape.leaderboards.getWeeklyXpRank();
+      requests.rank = Ape.leaderboards.getWeeklyXpRank({
+        query: {
+          weeksBefore: state.lastWeek ? 1 : undefined,
+        },
+      });
     }
 
     const [dataResponse, rankResponse] = await Promise.all([
@@ -653,8 +658,14 @@ function fillUser(): void {
   }
 
   if (isAuthenticated() && state.type === "daily" && state.userData === null) {
+    let str = `Not qualified`;
+
+    if (!state.yesterday) {
+      str += ` (min speed required: ${state.minWpm} wpm)`;
+    }
+
     $(".page.pageLeaderboards .bigUser").html(
-      `<div class="warning">Not qualified (min speed required: ${state.minWpm} wpm)</div>`
+      `<div class="warning">${str}</div>`
     );
     return;
   }
@@ -667,15 +678,6 @@ function fillUser(): void {
   }
 
   if (state.data === null) {
-    return;
-  }
-
-  if (
-    (state.type === "weekly" && state.lastWeek) ||
-    (state.type === "daily" && state.yesterday)
-  ) {
-    $(".page.pageLeaderboards .bigUser").addClass("hidden");
-    $(".page.pageLeaderboards .tableAndUser > .divider").removeClass("hidden");
     return;
   }
 
@@ -839,7 +841,7 @@ function fillUser(): void {
           <div class="sub">${formatted.time}</div>
         </div>
         <div class="stat wide">
-          <div class="title">date</div>
+          <div class="title">last activity</div>
           <div class="value">${format(
             userData.lastActivityTimestamp,
             "dd MMM yyyy HH:mm"
