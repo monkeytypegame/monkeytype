675310eb1d007f5727af5442ffaede53ed6044bb
Miodec <jack@monkeytype.com>
2025-03-28 20:33:58 +0100

    remove comments

--- frontend/src/ts/states/composition.ts
+++ frontend/src/ts/states/composition.ts
@@ -8,17 +8,9 @@ export function getComposing(): boolean {
   return compositionState.composing;
 }
 
-// export function getStartPos(): number {
-//   return compositionState.startPos;
-// }
-
 export function setComposing(isComposing: boolean): void {
   compositionState.composing = isComposing;
 }
-
-// export function setStartPos(pos: number): void {
-//   compositionState.startPos = pos;
-// }
 
 export function setData(data: string): void {
   compositionState.data = data;
