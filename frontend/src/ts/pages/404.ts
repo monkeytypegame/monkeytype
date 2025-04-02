bc6777f949cf913688beb8e6a1076ea5306e4612
Miodec <jack@monkeytype.com>
2025-04-02 14:27:01 +0200

    refactor: rename property, add optional property
    also fixes missing space in account settings page title

--- frontend/src/ts/pages/404.ts
+++ frontend/src/ts/pages/404.ts
@@ -2,7 +2,7 @@ import Page from "./page";
 import * as Skeleton from "../utils/skeleton";
 
 export const page = new Page({
-  name: "404",
+  id: "404",
   element: $(".page.page404"),
   path: "/404",
   afterHide: async (): Promise<void> => {
