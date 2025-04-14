11cfc07fa43636de83a88157dd30defc457ef161
Miodec <jack@monkeytype.com>
2025-04-14 16:07:33 +0200

    MOOOOOOORE

--- packages/util/src/arrays.ts
+++ packages/util/src/arrays.ts
@@ -1,3 +1,4 @@
+//eslint-disable @typescript-eslint/no-unsafe-assignment
 /**
  * Returns the intersection of two arrays, i.e., the elements that are present in both arrays.
  * @param a First array.
