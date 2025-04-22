3c6c066dc1a4b85e3901f6af0de1bfcf8c7304da
byseif21 <byseif21@gmail.com>
2025-04-22 17:15:59 +0200

    revert swagger back to JSON.parse

--- backend/src/api/routes/swagger.ts
+++ backend/src/api/routes/swagger.ts
@@ -1,21 +1,14 @@
 import { Application } from "express";
 import { getMiddleware as getSwaggerMiddleware } from "swagger-stats";
 import { isDevEnvironment } from "../../utils/misc";
-import { parseWithSchema as parseJsonWithSchema } from "@monkeytype/util/json";
 import { readFileSync } from "fs";
 import Logger from "../../utils/logger";
-import { z } from "zod";
 
 function addSwaggerMiddlewares(app: Application): void {
   const openApiSpec = __dirname + "/../../../dist/static/api/openapi.json";
   let spec = {};
   try {
-    // a schema that allows any object structure for OpenAPI spec
-    const OpenApiSchema = z.record(z.any());
-    spec = parseJsonWithSchema(
-      readFileSync(openApiSpec, "utf8"),
-      OpenApiSchema
-    );
+    spec = JSON.parse(readFileSync(openApiSpec, "utf8")) as string;
   } catch (err) {
     Logger.warning(
       `Cannot read openApi specification from ${openApiSpec}. Swagger stats will not fully work.`
