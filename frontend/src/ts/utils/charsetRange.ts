9a04dc9060b041e401df2b015b34951680a69215
Manish Suthar <manishsuthar078@gmail.com>
2025-04-26 00:43:56 +0530

    feat: charsetRange added as script unicode collection

---
+++ frontend/src/ts/utils/charsetRange.ts
@@ -0,0 +1,20 @@
+import { CharsetRanges } from "./json-data";
+
+export const charsetRanges: CharsetRanges = {
+  arabic: {
+    start: 1535,
+    end: 1791,
+  },
+  latin: {
+    start: 97,
+    end: 122,
+  },
+  cyrillic: {
+    start: 1024,
+    end: 1279,
+  },
+  devanagari: {
+    start: 2304,
+    end: 2405,
+  },
+};
