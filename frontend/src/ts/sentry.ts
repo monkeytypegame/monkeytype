c51acf39d2326cb14ac1cfeefdc3a79481763d86
Miodec <jack@monkeytype.com>
2025-04-30 18:46:16 +0200

    chore: dont sample random sessions without errors

--- frontend/src/ts/sentry.ts
+++ frontend/src/ts/sentry.ts
@@ -29,7 +29,7 @@ export function init(): void {
     // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
     tracePropagationTargets: ["localhost", /^https:\/\/api\.monkeytype\.com/],
     // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysSessionSampleRate: 0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
     allowUrls: ["https://*.monkeytype.com"],
     ignoreErrors: [
