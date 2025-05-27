796bb05f343e99c4c0908c7e8a792435ee1a6705
BhattAnsh <anshbhatt140@gmail.com>
2025-05-27 10:56:06 +0530

    added whatapp in theme schema

--- packages/contracts/src/schemas/themes.ts
+++ packages/contracts/src/schemas/themes.ts
@@ -187,6 +187,7 @@ export const ThemeNameSchema = z.enum(
     "watermelon",
     "wavez",
     "witch_girl",
+    "whatsapp",
   ],
   {
     errorMap: customEnumErrorHandler("Must be a known theme"),
