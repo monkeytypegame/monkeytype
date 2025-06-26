561150676fcad5aadd18182ec844ef1f006e729b
Miodec <jack@monkeytype.com>
2025-06-26 15:17:18 +0200

    revert to body, add a and button

--- frontend/src/ts/test/focus.ts
+++ frontend/src/ts/test/focus.ts
@@ -14,7 +14,9 @@ export function set(foc: boolean, withCursor = false): void {
     Caret.stopAnimation();
     $("header").addClass("focus");
     $("footer").addClass("focus");
-    if (!withCursor) $("*").css("cursor", "none");
+    if (!withCursor) $("body").css("cursor", "none");
+    $("button").css("cursor", "none");
+    $("a").css("cursor", "none");
     $("main").addClass("focus");
     $("#bannerCenter").addClass("focus");
     $("#notificationCenter").addClass("focus");
@@ -32,7 +34,9 @@ export function set(foc: boolean, withCursor = false): void {
     Caret.startAnimation();
     $("header").removeClass("focus");
     $("footer").removeClass("focus");
-    $("*").css("cursor", "default");
+    $("body").css("cursor", "default");
+    $("button").css("cursor", "default");
+    $("a").css("cursor", "default");
     $("main").removeClass("focus");
     $("#bannerCenter").removeClass("focus");
     $("#notificationCenter").removeClass("focus");
