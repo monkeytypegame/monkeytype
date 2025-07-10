8bb9c53c70e7bd124acb3056bf23907e8dee8a04
Zihafa <118399357+acornsacornshmmm@users.noreply.github.com>
2025-07-10 14:10:21 +0100

    Updated themes.ts

--- packages/contracts/src/schemas/themes.ts
+++ packages/contracts/src/schemas/themes.ts
@@ -187,6 +187,7 @@ export const ThemeNameSchema = z.enum(
     "watermelon",
     "wavez",
     "witch_girl",
+    "serratura_blu",
   ],
   {
     errorMap: customEnumErrorHandler("Must be a known theme"),
