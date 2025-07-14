5fa3ad06c2384ba731f1e15189b4207705c65842
Christian Fehmer <cfe@sexy-developer.com>
2025-07-14 23:23:37 +0200

    friends list first draftw

--- frontend/src/ts/pages/friends.ts
+++ frontend/src/ts/pages/friends.ts
@@ -5,6 +5,12 @@ import Ape from "../ape";
 import { formatDuration } from "date-fns/formatDuration";
 import { intervalToDuration } from "date-fns";
 import * as Notifications from "../elements/notifications";
+import { isSafeNumber } from "@monkeytype/util/numbers";
+import { getHTMLById as getBadgeHTMLbyId } from "../controllers/badge-controller";
+import { getXpDetails } from "../utils/levels";
+import { secondsToString } from "../utils/date-and-time";
+import { PersonalBest } from "@monkeytype/contracts/schemas/shared";
+import Format from "../utils/format";
 
 const pageElement = $(".page.pageFriends");
 
@@ -31,13 +37,6 @@ const addFriendModal = new SimpleModal({
 async function updatePendingRequests(): Promise<void> {
   $(".pageFriends .pendingRequests .loading").removeClass("hidden");
 
-  const format = (timestamp: number): string => {
-    const formatted = formatDuration(
-      intervalToDuration({ start: timestamp, end: Date.now() }),
-      { format: ["days", "hours", "minutes"] }
-    );
-    return formatted !== "" ? formatted : "less then a minute";
-  };
   const result = await Ape.friends.getRequests({
     query: { status: "pending", type: "incoming" },
   });
@@ -57,7 +56,7 @@ async function updatePendingRequests(): Promise<void> {
         .map(
           (item) => `<tr data-id="${item._id}">
         <td>${item.initiatorName}</td>
-        <td>${format(item.addedAt)} ago</td>
+        <td>${formatAge(item.addedAt)}</td>
         <td class="actions">
           <button class="accepted" aria-label="accept friend" data-balloon-pos="top">
             <i class="fas fa-check fa-fw"></i>
@@ -78,6 +77,105 @@ async function updatePendingRequests(): Promise<void> {
   }
 
   $(".pageFriends .pendingRequests .loading").addClass("hidden");
+}
+
+async function updateFriends(): Promise<void> {
+  $(".pageFriends .friends .loading").removeClass("hidden");
+
+  const result = await Ape.friends.getFriends();
+
+  if (result.status !== 200) {
+    $(".pageFriends .friends .error").removeClass("hidden");
+    $(".pageFriends .friends .error p").html(result.body.message);
+  } else {
+    $(".pageFriends .friends .error").addClass("hidden");
+    if (result.body.data.length === 0) {
+      $(".pageFriends .friends table").addClass("hidden");
+      $(".pageFriends .friends .nodata").removeClass("hidden");
+    } else {
+      $(".pageFriends .friends table").removeClass("hidden");
+      $(".pageFriends .friends .nodata").addClass("hidden");
+
+      const html = result.body.data
+        .map((entry) => {
+          let avatar = `<div class="avatarPlaceholder"><i class="fas fa-user-circle"></i></div>`;
+          if (entry.discordAvatar !== undefined) {
+            avatar = `<div class="avatarPlaceholder"><i class="fas fa-circle-notch fa-spin"></i></div>`;
+          }
+          const xpDetails = getXpDetails(entry.xp ?? 0);
+
+          const top15 = formatPb(entry.top15);
+          const top60 = formatPb(entry.top60);
+
+          return `<tr data-id="${entry.friendRequestId}">
+        <td>
+          <div class="avatarNameBadge">
+            <div class="lbav">${avatar}</div>
+              <a href="${location.origin}/profile/${
+            entry.uid
+          }?isUid" class="entryName" uid=${entry.uid} router-link>${
+            entry.name
+          }</a>
+            <div class="flagsAndBadge">
+            ${
+              isSafeNumber(entry.badgeId) ? getBadgeHTMLbyId(entry.badgeId) : ""
+            }
+            </div>
+          </div>
+        </td>
+        <td>${formatAge(entry.addedAt)}</td>
+        <td>${xpDetails.level}</td>
+        <td>${entry.completedTests}/${entry.startedTests}</td>
+        <td>${secondsToString(
+          Math.round(entry.timeTyping ?? 0),
+          true,
+          true
+        )}</td>
+        <td>${entry.streak !== undefined ? entry.streak.length + " days" : ""}
+        <td>${top15?.wpm}</td>
+        <td>${top60?.wpm}</td>
+        <td class="actions">
+          <button class="actions" aria-label="actions" data-balloon-pos="top">
+            <i class="fas fa-ellipsis-v fa-fw"></i>
+          </button> 
+        </td>
+      </tr>`;
+        })
+        .join("\n");
+
+      $(".pageFriends .friends tbody").html(html);
+    }
+  }
+
+  $(".pageFriends .friends .loading").addClass("hidden");
+}
+
+function formatAge(timestamp?: number): string {
+  if (timestamp === undefined) return "";
+  const formatted = formatDuration(
+    intervalToDuration({ start: timestamp, end: Date.now() }),
+    { format: ["days", "hours", "minutes"] }
+  );
+  return (formatted !== "" ? formatted : "less then a minute") + " ago";
+}
+
+function formatPb(entry?: PersonalBest):
+  | {
+      wpm: string;
+      acc: string;
+      raw: string;
+      con: string;
+    }
+  | undefined {
+  if (entry === undefined) {
+    return undefined;
+  }
+  return {
+    wpm: Format.typingSpeed(entry.wpm, { showDecimalPlaces: true }),
+    acc: Format.percentage(entry.acc, { showDecimalPlaces: true }),
+    raw: Format.typingSpeed(entry.raw, { showDecimalPlaces: true }),
+    con: Format.percentage(entry.consistency, { showDecimalPlaces: true }),
+  };
 }
 
 $("#friendAdd").on("click", () => {
@@ -142,6 +240,7 @@ export const page = new Page<undefined>({
     Skeleton.append("pageFriends", "main");
 
     await updatePendingRequests();
+    await updateFriends();
   },
 });
 
