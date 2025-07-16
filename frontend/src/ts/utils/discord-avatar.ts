ef541cec27e656b38552df0f649dd6c4be48f6fa
Christian Fehmer <cfe@sexy-developer.com>
2025-07-16 13:33:19 +0200

    cleanup

--- frontend/src/ts/utils/discord-avatar.ts
+++ frontend/src/ts/utils/discord-avatar.ts
@@ -2,7 +2,7 @@ const cachedAvatarUrlByAvatarId: Map<string, string | null> = new Map();
 
 function buildElement(
   url: string | null,
-  options?: { loading: boolean }
+  options?: { loading?: boolean; size?: number }
 ): HTMLElement {
   if (url === null) {
     const placeholder = document.createElement("div");
@@ -14,10 +14,11 @@ function buildElement(
   } else {
     const element = document.createElement("div");
     element.classList.add("avatar");
-    element.style.backgroundImage = `url(${url})`;
+    element.style.backgroundImage = `url(${url}?size=${options?.size ?? 32})`;
     return element;
   }
 }
+
 export function getAvatarElement(
   data: {
     discordId?: string;
@@ -27,29 +28,30 @@ export function getAvatarElement(
     size?: number;
   }
 ): HTMLElement {
-  if (data.discordId === undefined || data.discordAvatar === undefined) {
+  if (
+    data.discordId === undefined ||
+    data.discordId === "" ||
+    data.discordAvatar === undefined ||
+    data.discordAvatar === ""
+  ) {
     return buildElement(null);
   }
 
+  const discordId: string = data.discordId;
+  const discordAvatar: string = data.discordAvatar;
   const cachedUrl = cachedAvatarUrlByAvatarId.get(data.discordAvatar);
 
   if (cachedUrl !== undefined) {
     console.log("### cache hit", { data, cached: cachedUrl });
     return buildElement(cachedUrl);
   } else {
-    //`<div class="avatarPlaceholder"><i class="fas fa-circle-notch fa-spin"></i></div>`;
     const element = buildElement(null, { loading: true });
 
-    void getDiscordAvatarUrl({
-      ...data,
-      discordAvatarSize: options?.size,
-    }).then((url) => {
+    void getDiscordAvatarUrl({ discordId, discordAvatar }).then((url) => {
       cachedAvatarUrlByAvatarId.set(data.discordAvatar as string, url);
       console.log("### callback", { data, url });
       if (url !== null) {
-        element.classList.replace("avatarPlaceholder", "avatar");
-        element.style.backgroundImage = `url(${url})`;
-        element.innerHTML = "";
+        element.replaceWith(buildElement(url, { size: options?.size ?? 32 }));
       }
     });
 
@@ -61,25 +63,13 @@ export function getAvatarElement(
 async function getDiscordAvatarUrl({
   discordId,
   discordAvatar,
-  discordAvatarSize,
 }: {
-  discordId?: string;
-  discordAvatar?: string;
-  discordAvatarSize?: number;
+  discordId: string;
+  discordAvatar: string;
 }): Promise<string | null> {
-  if (
-    discordId === undefined ||
-    discordId === "" ||
-    discordAvatar === undefined ||
-    discordAvatar === ""
-  ) {
-    return null;
-  }
   // An invalid request to this URL will return a 404.
   try {
-    const avatarUrl = `https://cdn.discordapp.com/avatars/${discordId}/${discordAvatar}.png?size=${
-      discordAvatarSize ?? 32
-    }`;
+    const avatarUrl = `https://cdn.discordapp.com/avatars/${discordId}/${discordAvatar}.png`;
 
     const response = await fetch(avatarUrl, {
       method: "HEAD",
