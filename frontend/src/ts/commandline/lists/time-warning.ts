9ac1c457b72f0a6b69413985cc5081a644d8aa69
Christian Fehmer <cfe@sexy-developer.com>
2025-07-21 15:36:35 +0200

    refactor

--- frontend/src/ts/commandline/lists/time-warning.ts
+++ frontend/src/ts/commandline/lists/time-warning.ts
@@ -1,3 +1,7 @@
+import {
+  PlayTimeWarning,
+  PlayTimeWarningSchema,
+} from "@monkeytype/schemas/configs";
 import * as UpdateConfig from "../../config";
 import * as SoundController from "../../controllers/sound-controller";
 import { Command, CommandsSubgroup } from "../types";
@@ -5,52 +9,20 @@ import { Command, CommandsSubgroup } from "../types";
 const subgroup: CommandsSubgroup = {
   title: "Time warning...",
   configKey: "playTimeWarning",
-  list: [
-    {
-      id: "setPlayTimeWarningOff",
-      display: "off",
-      configValue: "off",
+  list: (Object.keys(PlayTimeWarningSchema.Values) as PlayTimeWarning[]).map(
+    (time) => ({
+      id: `setPlayTimeWarning${time}`,
+      display:
+        time === "off" ? "off" : `${time} second${time !== "1" ? "s" : ""}`,
+      configValue: time,
       exec: (): void => {
-        UpdateConfig.setPlayTimeWarning("off");
+        UpdateConfig.setPlayTimeWarning(time);
+        if (time !== "off") {
+          void SoundController.playTimeWarning();
+        }
       },
-    },
-    {
-      id: "setPlayTimeWarning1",
-      display: "1 second",
-      configValue: "1",
-      exec: (): void => {
-        UpdateConfig.setPlayTimeWarning("1");
-        void SoundController.playTimeWarning();
-      },
-    },
-    {
-      id: "setPlayTimeWarning3",
-      display: "3 seconds",
-      configValue: "3",
-      exec: (): void => {
-        UpdateConfig.setPlayTimeWarning("3");
-        void SoundController.playTimeWarning();
-      },
-    },
-    {
-      id: "setPlayTimeWarning5",
-      display: "5 seconds",
-      configValue: "5",
-      exec: (): void => {
-        UpdateConfig.setPlayTimeWarning("5");
-        void SoundController.playTimeWarning();
-      },
-    },
-    {
-      id: "setPlayTimeWarning10",
-      display: "10 seconds",
-      configValue: "10",
-      exec: (): void => {
-        UpdateConfig.setPlayTimeWarning("10");
-        void SoundController.playTimeWarning();
-      },
-    },
-  ],
+    })
+  ),
 };
 
 const commands: Command[] = [
