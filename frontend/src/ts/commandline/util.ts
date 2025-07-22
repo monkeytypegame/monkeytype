68a4dfe30fe6faec204330df43e358be63f44c35
Miodec <jack@monkeytype.com>
2025-07-22 11:53:18 +0200

    work for enums and booleans

--- frontend/src/ts/commandline/util.ts
+++ frontend/src/ts/commandline/util.ts
@@ -2,36 +2,39 @@ import { configMetadata, genericSet } from "../config";
 import { capitalizeFirstLetter } from "../utils/strings";
 import { Command, CommandsSubgroup } from "./types";
 import * as ConfigSchemas from "@monkeytype/schemas/configs";
-import { z, ZodEnum } from "zod";
+import { z } from "zod";
 
-// Helper type to filter keys where the schema is a ZodEnum
-type EnumConfigKeys = {
-  // oxlint-disable-next-line no-explicit-any
-  [K in keyof typeof ConfigSchemas.ConfigSchema.shape]: (typeof ConfigSchemas.ConfigSchema.shape)[K] extends ZodEnum<any>
-    ? K
-    : never;
-}[keyof typeof ConfigSchemas.ConfigSchema.shape];
-
-export function buildCommandForConfigMetadata(key: EnumConfigKeys): Command {
+export function buildCommandForConfigMetadata(
+  key: keyof typeof configMetadata
+): Command {
   const meta = configMetadata[key];
 
   const display = capitalizeFirstLetter(meta?.displayString ?? key) + "...";
 
   const schema = ConfigSchemas.ConfigSchema.shape[key];
 
-  const values = Object.keys(schema.Values) as unknown as z.infer<
-    typeof schema
-  >[];
+  let values;
+
+  if (schema instanceof z.ZodEnum) {
+    values = Object.keys(schema.Values);
+  } else if (schema instanceof z.ZodBoolean) {
+    values = [true, false];
+  } else {
+    throw new Error(
+      `Unsupported schema type for key "${key}": ${schema._def.typeName}`
+    );
+  }
 
   // const displayValues = (meta.commandline?.displayValues ?? {}) as Record<
   //   string | number | symbol,
   //   string
   // >;
 
-  const list = values.map((value) => {
-    //@ts-expect-error this type is super hard to figure out
-    const display = meta?.commandline?.commandDisplay?.(value) ?? value;
-    //@ts-expect-error this type is super hard to figure out
+  const list: Command[] = values.map((value) => {
+    const display =
+      //@ts-expect-error this type is hard to figure out
+      meta?.commandline?.commandDisplay?.(value) ?? value.toString();
+    //@ts-expect-error this type is hard to figure out
     const alias = meta?.commandline?.commandAlias?.(value) ?? undefined;
 
     const command = {
@@ -51,10 +54,8 @@ export function buildCommandForConfigMetadata(key: EnumConfigKeys): Command {
 
   //put configValue off or false at the start of the list
   list.sort((a, b) => {
-    // if (a.configValue === "off" || a.configValue === false) return -1;
-    // if (b.configValue === "off" || b.configValue === false) return 1;
-    if (a.configValue === "off") return -1;
-    if (b.configValue === "off") return 1;
+    if (a.configValue === "off" || a.configValue === false) return -1;
+    if (b.configValue === "off" || b.configValue === false) return 1;
     return 0;
   });
 
