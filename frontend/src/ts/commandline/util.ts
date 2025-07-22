a2cd0d6b2089c8db9d74b243ef19fa2e1c210c83
Miodec <jack@monkeytype.com>
2025-07-23 00:08:24 +0200

    refactor, prepare for different types

--- frontend/src/ts/commandline/util.ts
+++ frontend/src/ts/commandline/util.ts
@@ -13,13 +13,19 @@ import { z } from "zod";
 export function buildCommandForConfigKey<K extends keyof ConfigSchemas.Config>(
   key: K
 ): Command {
+  return buildCommandWithSubgroup(key);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
+function buildCommandWithSubgroup<K extends keyof ConfigSchemas.Config>(
+  key: K
+): Command {
   const configMeta = configMetadata[key];
   const commandMeta = commandlineConfigMetadata[key];
 
   const display =
     commandMeta?.rootDisplay ??
     `${capitalizeFirstLetter(configMeta?.displayString ?? key)}...`;
-
   const schema = ConfigSchemas.ConfigSchema.shape[key];
 
   let values: ConfigSchemas.Config[K][];
@@ -33,44 +39,6 @@ export function buildCommandForConfigKey<K extends keyof ConfigSchemas.Config>(
       `Unsupported schema type for key "${key}": ${schema._def.typeName}`
     );
   }
-
-  // const list: Command[] = values.map((value) => {
-  //   //@ts-expect-error cant figure out this type
-  //   let display = commandMeta?.commandDisplay?.(value);
-  //   //@ts-expect-error cant figure out this type
-  //   const alias = commandMeta?.commandAlias?.(value) ?? undefined;
-
-  //   if (display === undefined) {
-  //     if (value === true) {
-  //       display = "on";
-  //     } else if (value === false) {
-  //       display = "off";
-  //     } else {
-  //       display = value.toString();
-  //     }
-  //   }
-
-  //   const command = {
-  //     id: `set${capitalizeFirstLetter(key)}${capitalizeFirstLetter(
-  //       value.toString()
-  //     )}`,
-  //     display,
-  //     alias,
-  //     configValue: value,
-  //     exec: (): void => {
-  //       genericSet(key, value);
-  //       //@ts-expect-error cant figure out this type
-  //       commandMeta?.afterExec?.(value);
-  //     },
-  //     hover: (): void => {
-  //       //@ts-expect-error cant figure out this type
-  //       commandMeta?.hover?.(value);
-  //     },
-  //   };
-
-  //   return command;
-  // });
-
   const list = values.map((value) =>
     buildSetCommand<K>(key, value, commandMeta)
   );
