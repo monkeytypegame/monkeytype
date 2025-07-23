2528e575f0b56bd0856fe70bbd8cca02393a3bed
Christian Fehmer <cfe@sexy-developer.com>
2025-07-23 11:13:52 +0200

    added tests

---
+++ frontend/__tests__/commandline/util.spec.ts
@@ -0,0 +1,86 @@
+//import type { ConfigMetadata } from "../../src/ts/config-metadata";
+
+import * as Util from "../../src/ts/commandline/util";
+
+import type { CommandlineConfigMetadata } from "../../src/ts/commandline/commandline-metadata";
+import type { ConfigKey } from "@monkeytype/schemas/configs";
+import type { ConfigMetadata } from "../../src/ts/config-metadata";
+import { z, ZodSchema } from "zod";
+
+const buildCommandForConfigKey = Util.__testing._buildCommandForConfigKey;
+
+describe("CommandlineUtils", () => {
+  vi.mock("../../src/ts/config-metadata", () => ({ configMetadata: [] }));
+  vi.mock("../../src/ts/commandline/commandline-metadata", () => ({
+    commandlineConfigMetadata: [],
+  }));
+
+  afterAll(() => {
+    vi.resetModules();
+    vi.restoreAllMocks();
+  });
+  describe("buildCommandForConfigKey", () => {
+    it("detects values for boolean schema", () => {
+      //GIVEN
+      const schema = z.boolean();
+
+      //WHEN
+      const cmd = buildCommand({ schema });
+
+      //THEN
+      expect(cmd.subgroup?.list.map((it) => it.id)).toEqual([
+        "setFalse",
+        "setTrue",
+      ]);
+    });
+
+    it("detects values for enum schema", () => {
+      //GIVEN
+      const schema = z.enum(["one", "two", "three"]);
+
+      //WHEN
+      const cmd = buildCommand({ schema });
+
+      //THEN
+      expect(cmd.subgroup?.list.map((it) => it.id)).toEqual([
+        "setOne",
+        "setTwo",
+        "setThree",
+      ]);
+    });
+    it("detects values for union schema of enum + literral", () => {
+      //GIVEN
+      const schema = z.literal("default").or(z.enum(["one", "two", "three"]));
+
+      //WHEN
+      const cmd = buildCommand({ schema });
+
+      //THEN
+      expect(cmd.subgroup?.list.map((it) => it.id)).toEqual([
+        "setDefault",
+        "setOne",
+        "setTwo",
+        "setThree",
+      ]);
+    });
+  });
+});
+
+function buildCommand<K extends ConfigKey>({
+  configMeta,
+  cmdMeta,
+  schema,
+  key,
+}: {
+  configMeta?: Partial<ConfigMetadata<K>>;
+  cmdMeta?: Partial<CommandlineConfigMetadata<K>>;
+  schema?: ZodSchema;
+  key?: K;
+}) {
+  return buildCommandForConfigKey(
+    key ?? ("" as any),
+    configMeta ?? ({} as any),
+    cmdMeta ?? ({} as any),
+    schema ?? z.string()
+  );
+}
