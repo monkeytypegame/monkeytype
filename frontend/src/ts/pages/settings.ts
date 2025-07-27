e5b0978d4ef595757a804b2e1ce9948d30c09869
Seif Soliman <byseif21@gmail.com>
2025-07-27 20:43:43 +0300

    fix(settings): ensure themes UI is always up-to-date when renavigating (@byseif21) (#6581)
    ### Description
    When (changing / adding or removing themes from favorite ) via command
    line or from the current theme button "in page e.g test page" and then
    navigating to Settings > Themes, the UI does not immediately reflect the
    current theme changes . The user has to manually refresh the page to see
    these changes.
    
    **Now**
     UI automatically refreshes
    to reflect the current theme selection and favorites without requiring
    a manual page refresh.
    
    - Added refreshThemeUI function in theme-picker.ts that updates all
      theme-related UI components
    - Called this refresh function in the settings page's beforeShow
    lifecycle hook to ensure the theme UI is always up-to-date when the page
    is displayed.
    
    
    **FOR TESTING BEFORE & AFTER:**
    keep changing themes then navigate to the settings and check and keep
    doing those things again to verify that the active theme and favorites
    update.
    
    ---------
    
    Co-authored-by: Miodec <jack@monkeytype.com>

--- frontend/src/ts/pages/settings.ts
+++ frontend/src/ts/pages/settings.ts
@@ -686,8 +686,7 @@ async function fillSettingsPage(): Promise<void> {
   setEventDisabled(true);
 
   await initGroups();
-  await ThemePicker.refreshCustomButtons();
-  await ThemePicker.refreshPresetButtons();
+  await ThemePicker.fillCustomButtons();
 
   setEventDisabled(false);
   settingsInitialized = true;
@@ -819,7 +818,6 @@ export async function update(
   await Misc.sleep(0);
   ThemePicker.updateActiveTab();
   ThemePicker.setCustomInputs(true);
-  // ThemePicker.updateActiveButton();
 
   const setInputValue = (
     key: ConfigKey,
@@ -1271,6 +1269,8 @@ export const page = new PageWithUrlParams({
     await UpdateConfig.loadPromise;
     await fillSettingsPage();
     await update();
+    // theme UI updates manually to avoid duplication
+    await ThemePicker.updateThemeUI();
 
     handleHighlightSection(options.urlParams?.highlight);
   },
