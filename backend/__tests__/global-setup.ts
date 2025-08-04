d9009e51ccfa2d4f9ffaa112116cf4c62b4bf22f
Christian Fehmer <fehmer@users.noreply.github.com>
2025-08-04 15:10:41 +0200

    test: use mongodb testcontainer (@fehmer) (#6808)

--- backend/__tests__/global-setup.ts
+++ backend/__tests__/global-setup.ts
@@ -1,23 +1,32 @@
-import * as MongoDbMock from "vitest-mongodb";
+import { GenericContainer, StartedTestContainer, Wait } from "testcontainers";
 import { isIntegrationTest } from "./__integration__";
+
+let startedMongoContainer: StartedTestContainer | undefined;
+
 export async function setup(): Promise<void> {
   process.env.TZ = "UTC";
+
   if (isIntegrationTest) {
-    console.log("integration download mongomock");
-    await MongoDbMock.setup(MongoDbMockConfig);
+    //use testcontainer to start mongodb
+    //const network = await new Network(new RandomUuid()).start();
+    const mongoContainer = new GenericContainer("mongo:5.0.13")
+      //.withName("monkeytype-mongo-test")
+      .withExposedPorts(27017)
+      // .withNetwork(network)
+      //.withNetworkMode(network.getName())
+      .withWaitStrategy(Wait.forListeningPorts());
+
+    startedMongoContainer = await mongoContainer.start();
+
+    const mongoUrl = `mongodb://${startedMongoContainer?.getHost()}:${startedMongoContainer?.getMappedPort(
+      27017
+    )}`;
+    process.env["TEST_DB_URL"] = mongoUrl;
   }
 }
 
 export async function teardown(): Promise<void> {
   if (isIntegrationTest) {
-    await MongoDbMock.teardown();
+    await startedMongoContainer?.stop();
   }
 }
-
-export const MongoDbMockConfig = {
-  serverOptions: {
-    binary: {
-      version: "6.0.12",
-    },
-  },
-};
