2b43a5f82e8d3c66c40953807e5a6c4b8da3ce21
Seif Soliman <byseif21@gmail.com>
2025-08-04 16:12:38 +0300

    fix(font): 0xProto not working in screenshot (@byseif21) (#6817)
    ### Description
    starting with number seem to cuzes issue, did some hacking idk
    
    ---------
    
    Co-authored-by: Christian Fehmer <cfe@sexy-developer.com>

--- frontend/src/ts/commandline/commandline-metadata.ts
+++ frontend/src/ts/commandline/commandline-metadata.ts
@@ -586,7 +586,11 @@ export const commandlineConfigMetadata: CommandlineConfigMetadataObject = {
   },
   fontFamily: {
     subgroup: {
-      options: typedKeys(Fonts).sort(),
+      options: typedKeys(Fonts).sort((a, b) =>
+        (Fonts[a]?.display ?? a.replace(/_/g, " ")).localeCompare(
+          Fonts[b]?.display ?? b.replace(/_/g, " ")
+        )
+      ),
       display: (name) =>
         Fonts[name as KnownFontName]?.display ?? name.replaceAll(/_/g, " "),
       customData: (name) => {
