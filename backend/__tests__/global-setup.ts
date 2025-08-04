9c41fd5d04c304e7c6ba37668da58819160f3765
Christian Fehmer <fehmer@users.noreply.github.com>
2025-08-04 15:28:55 +0200

    test: add unit tests for daily leaderboards (@fehmer) (#6802)
    - **refactor existing test to use it.for**
    - **use testcontainers**

--- backend/__tests__/global-setup.ts
+++ backend/__tests__/global-setup.ts
@@ -1,7 +1,9 @@
 import { GenericContainer, StartedTestContainer, Wait } from "testcontainers";
 import { isIntegrationTest } from "./__integration__";
+import { getConnection } from "../src/init/redis";
 
 let startedMongoContainer: StartedTestContainer | undefined;
+let startedRedisContainer: StartedTestContainer | undefined;
 
 export async function setup(): Promise<void> {
   process.env.TZ = "UTC";
@@ -22,11 +24,26 @@ export async function setup(): Promise<void> {
       27017
     )}`;
     process.env["TEST_DB_URL"] = mongoUrl;
+
+    //use testcontainer to start redis
+    const redisContainer = new GenericContainer("redis:6.2.6")
+      .withExposedPorts(6379)
+      .withWaitStrategy(Wait.forLogMessage("Ready to accept connections"));
+
+    startedRedisContainer = await redisContainer.start();
+
+    const redisUrl = `redis://${startedRedisContainer.getHost()}:${startedRedisContainer.getMappedPort(
+      6379
+    )}`;
+    process.env["REDIS_URI"] = redisUrl;
   }
 }
 
 export async function teardown(): Promise<void> {
   if (isIntegrationTest) {
     await startedMongoContainer?.stop();
+
+    await getConnection()?.quit();
+    await startedRedisContainer?.stop();
   }
 }
