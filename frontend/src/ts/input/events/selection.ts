a84c35f81c7bb14a12aab5843115c786c69c938c
Miodec <jack@monkeytype.com>
2025-09-05 14:28:33 +0200

    refactor

--- frontend/src/ts/input/events/selection.ts
+++ frontend/src/ts/input/events/selection.ts
@@ -1,15 +1,13 @@
 import { moveCaretToTheEnd } from "../core/input-element";
 
 export function handleSelectionChange(event: Event): void {
-  if (event.target === null || !(event.target instanceof HTMLInputElement)) {
+  const el = event.target;
+  if (el === null || !(el instanceof HTMLInputElement)) {
     return;
   }
 
-  const hasSelectedText =
-    event.target.selectionStart !== event.target.selectionEnd;
-  const isCursorAtEnd =
-    event.target.selectionStart === event.target.value.length;
-
+  const hasSelectedText = el.selectionStart !== el.selectionEnd;
+  const isCursorAtEnd = el.selectionStart === el.value.length;
   if (hasSelectedText || !isCursorAtEnd) {
     // force caret at end of input
     moveCaretToTheEnd();
