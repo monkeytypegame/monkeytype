8667d61a23f7f0ce3009e5ccc9c18bbb883ac6c7
Miodec <jack@monkeytype.com>
2025-09-17 17:12:49 +0200

    dont bother, always update words input position
    do it in a animation frame to not block input
    
    throw away previous frames if a new one is queued up

--- frontend/src/ts/test/test-ui.ts
+++ frontend/src/ts/test/test-ui.ts
@@ -203,9 +203,8 @@ export function updateActiveElement(
 
   activeWordTop = newActiveWord.offsetTop;
 
-  if (!initial && shouldUpdateWordsInputPosition()) {
-    void updateWordsInputPosition();
-  }
+  void updateWordsInputPosition();
+
   if (!initial && Config.tapeMode !== "off") {
     void scrollTape();
   }
@@ -454,7 +453,7 @@ function updateWordWrapperClasses(): void {
 
   updateWordsWidth();
   updateWordsWrapperHeight(true);
-  updateWordsMargin(updateWordsInputPosition, [true]);
+  updateWordsMargin(updateWordsInputPosition, []);
 }
 
 export function showWords(): void {
@@ -484,65 +483,52 @@ export function appendEmptyWordElement(
     `<div class='word' data-wordindex='${index}'><letter class='invisible'>_</letter></div>`
   );
 }
-
-const posUpdateLangList = ["japanese", "chinese", "korean"];
-function shouldUpdateWordsInputPosition(): boolean {
-  const language = posUpdateLangList.some((l) => Config.language.startsWith(l));
-  return language || (Config.mode !== "time" && Config.showAllLines);
-}
-
-export async function updateWordsInputPosition(initial = false): Promise<void> {
-  if (ActivePage.get() !== "test") return;
-
-  const currentLanguage = await JSONData.getCurrentLanguage(Config.language);
-  const isLanguageRTL = currentLanguage.rightToLeft;
-
-  const el = document.querySelector<HTMLElement>("#wordsInput");
-
-  if (!el) return;
-
-  const activeWord = getActiveWordElement();
-
-  if (!activeWord) {
-    el.style.top = "0px";
-    el.style.left = "0px";
-    return;
+let updateWordsInputPositionAnimationFrameId: null | number = null;
+export async function updateWordsInputPosition(): Promise<void> {
+  if (updateWordsInputPositionAnimationFrameId !== null) {
+    cancelAnimationFrame(updateWordsInputPositionAnimationFrameId);
   }
+  updateWordsInputPositionAnimationFrameId = requestAnimationFrame(async () => {
+    updateWordsInputPositionAnimationFrameId = null;
+    if (ActivePage.get() !== "test") return;
+    const currentLanguage = await JSONData.getCurrentLanguage(Config.language);
+    const isLanguageRTL = currentLanguage.rightToLeft;
 
-  const computed = window.getComputedStyle(activeWord);
-  const activeWordMargin =
-    parseInt(computed.marginTop) + parseInt(computed.marginBottom);
+    const el = document.querySelector<HTMLElement>("#wordsInput");
 
-  const letterHeight = convertRemToPixels(Config.fontSize);
-  const targetTop =
-    activeWord.offsetTop + letterHeight / 2 - el.offsetHeight / 2 + 1; //+1 for half of border
+    if (!el) return;
 
-  if (Config.tapeMode !== "off") {
-    el.style.maxWidth = `${100 - Config.tapeMargin}%`;
-  } else {
-    el.style.maxWidth = "";
-  }
-  if (activeWord.offsetWidth < letterHeight) {
-    el.style.width = letterHeight + "px";
-  } else {
-    el.style.width = activeWord.offsetWidth + "px";
-  }
+    const activeWord = getActiveWordElement();
 
-  if (
-    initial &&
-    !shouldUpdateWordsInputPosition() &&
-    Config.tapeMode === "off"
-  ) {
-    el.style.top = targetTop + letterHeight + activeWordMargin + 4 + "px";
-  } else {
+    if (!activeWord) {
+      el.style.top = "0px";
+      el.style.left = "0px";
+      return;
+    }
+
+    const letterHeight = convertRemToPixels(Config.fontSize);
+    const targetTop =
+      activeWord.offsetTop + letterHeight / 2 - el.offsetHeight / 2 + 1; //+1 for half of border
+
+    if (Config.tapeMode !== "off") {
+      el.style.maxWidth = `${100 - Config.tapeMargin}%`;
+    } else {
+      el.style.maxWidth = "";
+    }
+    if (activeWord.offsetWidth < letterHeight) {
+      el.style.width = letterHeight + "px";
+    } else {
+      el.style.width = activeWord.offsetWidth + "px";
+    }
+
     el.style.top = targetTop + "px";
-  }
 
-  if (activeWord.offsetWidth < letterHeight && isLanguageRTL) {
-    el.style.left = activeWord.offsetLeft - letterHeight + "px";
-  } else {
-    el.style.left = Math.max(0, activeWord.offsetLeft) + "px";
-  }
+    if (activeWord.offsetWidth < letterHeight && isLanguageRTL) {
+      el.style.left = activeWord.offsetLeft - letterHeight + "px";
+    } else {
+      el.style.left = Math.max(0, activeWord.offsetLeft) + "px";
+    }
+  });
 }
 
 let centeringActiveLine: Promise<void> = Promise.resolve();
@@ -1717,13 +1703,11 @@ addEventListener("resize", () => {
 });
 
 $("#wordsInput").on("focus", (e) => {
-  console.log("words input getting focus");
   const wordsFocused = e.target === document.activeElement;
   if (!wordsFocused) return;
   if (!resultVisible && Config.showOutOfFocusWarning) {
     OutOfFocus.hide();
   }
-  console.log("words input calling show");
   Caret.show(true);
 });
 
