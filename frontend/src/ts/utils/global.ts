5e3b01e0e2f91cc5f33bf05c4b89593afb018ca8
Miodec <jack@monkeytype.com>
2025-10-15 12:00:53 +0200

    move to misc

--- frontend/src/ts/utils/global.ts
+++
@@ -1,6 +0,0 @@
-export function addToGlobal(items: Record<string, unknown>): void {
-  for (const [name, item] of Object.entries(items)) {
-    //@ts-expect-error dev
-    window[name] = item;
-  }
-}
