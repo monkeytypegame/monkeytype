fb87a4f64db9bdd8bc3da3e223f1f369e703036d
Christian Fehmer <cfe@sexy-developer.com>
2025-11-03 10:39:33 +0100

    impr: language integrity check (@fehmer)

---
+++ frontend/scripts/language-integrity.ts
@@ -0,0 +1,56 @@
+import { Plugin } from "vite";
+import { readdirSync, readFileSync } from "fs";
+import { TextEncoder } from "util";
+import { createHash } from "crypto";
+
+const virtualModuleId = "virtual:language-hashes";
+const resolvedVirtualModuleId = "\0" + virtualModuleId;
+
+export function languageHashes(): Plugin {
+  return {
+    name: "virtual-build-info",
+    resolveId(id) {
+      if (id === virtualModuleId) return resolvedVirtualModuleId;
+      return;
+    },
+    load(id) {
+      if (id === resolvedVirtualModuleId) {
+        const hashes: Record<string, string> = getHashes();
+        return `
+          export const languageHashes = ${JSON.stringify(hashes)};
+        `;
+      }
+      return;
+    },
+  };
+}
+
+function getHashes(): Record<string, string> {
+  const start = performance.now();
+
+  const hashes = Object.fromEntries(
+    readdirSync("./static/languages").map((file) => {
+      return [file.slice(0, -5), calcHash(file)];
+    })
+  );
+
+  const end = performance.now();
+
+  console.log(`Creating language hashes took ${Math.round(end - start)} ms`);
+
+  return hashes;
+}
+
+function calcHash(file: string): string {
+  const currentLanguage = JSON.stringify(
+    JSON.parse(readFileSync("./static/languages/" + file).toString()),
+    null,
+    0
+  );
+  const encoder = new TextEncoder();
+  const data = encoder.encode(currentLanguage);
+  return createHash("sha256").update(data).digest("hex");
+}
+if (import.meta.url.endsWith(process.argv[1] as string)) {
+  console.log(JSON.stringify(getHashes(), null, 4));
+}
