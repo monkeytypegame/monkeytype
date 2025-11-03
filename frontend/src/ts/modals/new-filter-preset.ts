34d78a4b37ab1e793e3f065451a39ee3b894513f
AmanDevelops <amanpal2072004@gmail.com>
2025-11-04 03:23:03 +0530

    feat: update createFilterPreset to return status code and simplify error handling

--- frontend/src/ts/modals/new-filter-preset.ts
+++ frontend/src/ts/modals/new-filter-preset.ts
@@ -1,80 +1,35 @@
-import { createFilterPreset } from "../elements/account/result-filters";
-import AnimatedModal, { ShowOptions } from "../utils/animated-modal";
-import * as Notifications from "../elements/notifications";
-import { InputIndicator } from "../elements/input-indicator";
 import { ResultFiltersSchema } from "@monkeytype/schemas/users";
+import { createFilterPreset } from "../elements/account/result-filters";
+import { SimpleModal } from "../utils/simple-modal";
 
-export function show(showOptions?: ShowOptions): void {
-  void modal.show({
-    ...showOptions,
-    focusFirstInput: true,
-    beforeAnimation: async (modalEl) => {
-      (modalEl.querySelector("input") as HTMLInputElement).value = "";
+export function show(): void {
+  newFilterPresetModal.show(undefined, {});
+}
 
-      const indicator = $(modalEl).data("indicator") as
-        | InputIndicator
-        | undefined;
-      if (indicator) {
-        indicator.hide();
-      }
+const newFilterPresetModal = new SimpleModal({
+  id: "newFilterPresetModal",
+  title: "New Filter Preset",
+  inputs: [
+    {
+      placeholder: "Preset Name",
+      type: "text",
+      initVal: "",
+      validation: {
+        schema: ResultFiltersSchema.shape.name,
+      },
     },
-  });
-}
+  ],
+  buttonText: "add",
+  onlineOnly: true,
+  execFn: async (_thisPopup, name) => {
+    const status = await createFilterPreset(name);
 
-function hide(clearChain = false): void {
-  void modal.hide({
-    clearModalChain: clearChain,
-  });
-}
-
-function apply(): void {
-  const name = $("#newFilterPresetModal input").val() as string;
-  if (name === "") {
-    Notifications.add("Name cannot be empty", 0);
-    return;
-  }
-  void createFilterPreset(name);
-  hide(true);
-}
-
-function addValidation(element: JQuery, schema: Zod.Schema): InputIndicator {
-  const indicator = new InputIndicator(element, {
-    valid: { icon: "fa-check", level: 1 },
-    invalid: { icon: "fa-times", level: -1 },
-  });
-
-  element.on("input", (event) => {
-    const value = (event.target as HTMLInputElement).value;
-    if (value === undefined || value === "") {
-      indicator.hide();
-      return;
+    if (status === 1) {
+      return { status: 1, message: "Filter preset created" };
+    } else {
+      let status: -1 | 0 | 1 = -1;
+      let message: string = "Error creating filter preset";
+      return { status, message, alwaysHide: true };
     }
-    const validationResult = schema.safeParse(value);
-    if (!validationResult.success) {
-      indicator.show(
-        "invalid",
-        validationResult.error.errors.map((err) => err.message).join(", ")
-      );
-      return;
-    }
-    indicator.show("valid");
-  });
-  return indicator;
-}
-
-async function setup(modalEl: HTMLElement): Promise<void> {
-  modalEl.addEventListener("submit", (e) => {
-    e.preventDefault();
-    apply();
-  });
-
-  const inputElement = $(modalEl).find("input");
-
-  const indicator = addValidation(inputElement, ResultFiltersSchema.shape.name);
-  $(modalEl).data("indicator", indicator);
-}
-
-const modal = new AnimatedModal({
-  dialogId: "newFilterPresetModal",
-  setup,
+  },
 });
