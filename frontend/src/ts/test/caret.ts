9565149bde38ed358911745eec53a26ce5e38c5d
Miodec <jack@monkeytype.com>
2025-11-05 08:35:31 +0100

    fix caret positioning with composition

--- frontend/src/ts/test/caret.ts
+++ frontend/src/ts/test/caret.ts
@@ -3,6 +3,7 @@ import * as TestInput from "./test-input";
 import * as TestState from "../test/test-state";
 import { subscribe } from "../observables/config-event";
 import { Caret } from "../utils/caret";
+import * as CompositionState from "../states/composition";
 
 export function stopAnimation(): void {
   caret.stopBlinking();
@@ -31,7 +32,8 @@ export function resetPosition(): void {
 export function updatePosition(noAnim = false): void {
   caret.goTo({
     wordIndex: TestState.activeWordIndex,
-    letterIndex: TestInput.input.current.length,
+    letterIndex:
+      TestInput.input.current.length + CompositionState.getData().length,
     isLanguageRightToLeft: TestState.isLanguageRightToLeft,
     isDirectionReversed: TestState.isDirectionReversed,
     animate: Config.smoothCaret !== "off" && !noAnim,
