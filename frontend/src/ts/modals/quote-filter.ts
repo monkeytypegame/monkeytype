52fd99dbc5df5e35e9ce187768449c55ab6efdd3
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2025-11-15 12:40:45 +0200

    refactor

--- frontend/src/ts/modals/quote-filter.ts
+++ frontend/src/ts/modals/quote-filter.ts
@@ -3,6 +3,13 @@ import AnimatedModal from "../utils/animated-modal";
 export let minFilterLength: number = 0;
 export let maxFilterLength: number = 0;
 
+function handleFilterLength(modalEl: HTMLElement): void {
+  let minEl = modalEl.querySelector(".minFilterLength") as HTMLInputElement;
+  let maxEl = modalEl.querySelector(".maxFilterLength") as HTMLInputElement;
+  minFilterLength = +minEl?.value;
+  maxFilterLength = +maxEl?.value;
+}
+
 export function show(): void {
   void modal.show();
 }
@@ -16,19 +23,20 @@ function hide(clearModalChain: boolean): void {
 async function setup(modalEl: HTMLElement): Promise<void> {
   let submitButton = modalEl.querySelector("button");
   submitButton?.addEventListener("click", () => {
-    let minEl = modalEl.querySelector(".minFilterLength") as HTMLInputElement;
-    let maxEl = modalEl.querySelector(".maxFilterLength") as HTMLInputElement;
-    minFilterLength = +minEl?.value;
-    maxFilterLength = +maxEl?.value;
+    handleFilterLength(modalEl);
     hide(true);
-    let refreshButton = document.querySelector(
-      ".refreshQuotes"
-    ) as HTMLButtonElement;
-    refreshButton.click();
   });
+}
+
+async function cleanup(): Promise<void> {
+  let refreshButton = document.querySelector(
+    ".refreshQuotes"
+  ) as HTMLButtonElement;
+  refreshButton.click();
 }
 
 const modal = new AnimatedModal({
   dialogId: "quoteFilterModal",
   setup,
+  cleanup,
 });
