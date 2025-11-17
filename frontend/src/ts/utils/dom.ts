e2b24b2578d962dc5c9c91dff7734cd8c14c137b
Miodec <jack@monkeytype.com>
2025-11-17 13:51:26 +0100

    default htmlelement

--- frontend/src/ts/utils/dom.ts
+++ frontend/src/ts/utils/dom.ts
@@ -7,13 +7,16 @@ export function onDocumentReady(callback: () => void): void {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
-export function qs<T extends HTMLElement>(
+export function qs<T extends Element = HTMLElement>(
   selector: string,
   nonNull?: false
 ): T | null;
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
-export function qs<T extends HTMLElement>(selector: string, nonNull: true): T;
-export function qs<T extends HTMLElement>(
+export function qs<T extends Element = HTMLElement>(
+  selector: string,
+  nonNull: true
+): T;
+export function qs<T extends Element = HTMLElement>(
   selector: string,
   nonNull = false
 ): T | null {
@@ -24,7 +27,7 @@ export function qs<T extends HTMLElement>(
   return element;
 }
 
-export function qsa<T extends HTMLElement>(
+export function qsa<T extends Element = HTMLElement>(
   selector: string,
   parent: Document | HTMLElement = document
 ): T[] {
