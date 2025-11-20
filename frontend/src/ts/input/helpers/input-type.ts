0ab16b2200299b5c7e8876cb627bdda481279893
Miodec <jack@monkeytype.com>
2025-11-20 20:46:13 +0100

    remove unused code

--- frontend/src/ts/input/helpers/input-type.ts
+++ frontend/src/ts/input/helpers/input-type.ts
@@ -20,38 +20,3 @@ export function isSupportedInputType(
 ): inputType is SupportedInputType {
   return SUPPORTED_INPUT_TYPES.has(inputType as SupportedInputType);
 }
-
-const IGNORED_INPUT_TYPES = [
-  "insertReplacementText", // not allowing autocomplete replacements
-  "insertParagraph",
-  "insertOrderedList",
-  "insertUnorderedList",
-  "insertHorizontalRule",
-  "insertFromYank",
-  "insertFromDrop",
-  "insertFromPaste",
-  "insertFromPasteAsQuotation",
-  "insertTranspose",
-  "insertLink",
-  "deleteSoftLineBackward",
-  "deleteSoftLineForward",
-  "deleteEntireSoftLine",
-  "deleteHardLineBackward",
-  "deleteHardLineForward",
-  "deleteByDrag",
-  "deleteByCut",
-  "deleteContent",
-  "deleteContentForward",
-  "history*",
-  "format*",
-] as const;
-
-export function isIgnoredInputType(inputType: string): boolean {
-  return IGNORED_INPUT_TYPES.some((type) => {
-    if (type.endsWith("*")) {
-      return inputType.startsWith(type.slice(0, -1));
-    } else {
-      return inputType === type;
-    }
-  });
-}
