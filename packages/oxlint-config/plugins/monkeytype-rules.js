08d2e46d9ea44babef459a5f2df5284ca484a1bc
Miodec <jack@monkeytype.com>
2025-11-23 23:50:05 +0100

    move

--- packages/oxlint-config/plugins/monkeytype-rules.js
+++ packages/oxlint-config/plugins/monkeytype-rules.js
@@ -1,26 +1,24 @@
 import { defineRule } from "oxlint";
 
-const rule = defineRule({
-  createOnce(context) {
-    return {
-      MemberExpression(node) {
-        if (node.property?.name === "__testing") {
-          context.report({
-            node,
-            message: "__testing should only be accessed in test files.",
-          });
-        }
-      },
-    };
-  },
-});
-
 const plugin = {
   meta: {
     name: "monkeytype-rules",
   },
   rules: {
-    "no-testing-access": rule,
+    "no-testing-access": defineRule({
+      createOnce(context) {
+        return {
+          MemberExpression(node) {
+            if (node.property?.name === "__testing") {
+              context.report({
+                node,
+                message: "__testing should only be accessed in test files.",
+              });
+            }
+          },
+        };
+      },
+    }),
   },
 };
 
