1a2cf6733623dc9d5984c574670f9a7839cec761
Miodec <jack@monkeytype.com>
2025-11-24 00:32:40 +0100

    revert

--- packages/tsup-config/tsup.config.js
+++ packages/tsup-config/tsup.config.js
@@ -4,7 +4,7 @@ export default defineConfig((_options) => ({
   entry: ["src/index.ts"],
   splitting: false,
   sourcemap: false,
-  clear: _options?.watch !== true,
+  clear: !_options?.watch,
   format: ["cjs", "esm"],
   dts: false,
 }));
