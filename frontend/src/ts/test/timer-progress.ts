0c700b92bb417f7fcc415c055dfeb19e69b582d7
Gian Peña <flaconumero2@gmail.com>
2025-11-26 17:51:44 -0500

    add flash for mini

--- frontend/src/ts/test/timer-progress.ts
+++ frontend/src/ts/test/timer-progress.ts
@@ -36,7 +36,15 @@ export function show(): void {
         textEl.classList.remove("hidden");
       },
     });
-  } else if (Config.timerStyle === "flash") {
+  } else if (Config.timerStyle === "flash mini") {
+    animate(miniEl, {
+      opacity: [0, 1],
+      duration: applyReducedMotion(125),
+      onBegin: () => {
+        miniEl.classList.remove("hidden");
+      },
+    });
+  } else if (Config.timerStyle === "flash text") {
     animate(textEl, {
       opacity: [0, 1],
       duration: applyReducedMotion(125),
@@ -132,7 +140,18 @@ export function update(): void {
       if (textEl !== null) {
         textEl.innerHTML = "<div>" + displayTime + "</div>";
       }
-    } else if (Config.timerStyle === "flash") {
+    } else if (Config.timerStyle === "flash mini") {
+      let displayTime = DateTime.secondsToString(maxtime - time);
+      if (maxtime === 0) {
+        displayTime = DateTime.secondsToString(time);
+      }
+      if (miniEl !== null) {
+        miniEl.innerHTML =
+          "<div>" +
+          `${(maxtime - time) % 15 !== 0 ? "" : displayTime}` +
+          "</div>";
+      }
+    } else if (Config.timerStyle === "flash text") {
       let displayTime = DateTime.secondsToString(maxtime - time);
       if (maxtime === 0) {
         displayTime = DateTime.secondsToString(time);
@@ -182,7 +201,13 @@ export function update(): void {
       } else {
         textEl.innerHTML = `<div>${getCurrentCount()}/${outof}</div>`;
       }
-    } else if (Config.timerStyle === "flash") {
+    } else if (Config.timerStyle === "flash mini") {
+      if (outof === 0) {
+        miniEl.innerHTML = `${TestInput.input.getHistory().length}`;
+      } else {
+        miniEl.innerHTML = `${getCurrentCount()}/${outof}`;
+      }
+    } else if (Config.timerStyle === "flash text") {
       if (outof === 0) {
         textEl.innerHTML = `<div>${TestInput.input.getHistory().length}</div>`;
       } else {
@@ -198,7 +223,9 @@ export function update(): void {
   } else if (Config.mode === "zen") {
     if (Config.timerStyle === "text") {
       textEl.innerHTML = `<div>${TestInput.input.getHistory().length}</div>`;
-    } else if (Config.timerStyle === "flash") {
+    } else if (Config.timerStyle === "flash mini") {
+      miniEl.innerHTML = `${TestInput.input.getHistory().length}`;
+    } else if (Config.timerStyle === "flash text") {
       textEl.innerHTML = `<div>${TestInput.input.getHistory().length}</div>`;
     } else {
       miniEl.innerHTML = `${TestInput.input.getHistory().length}`;
