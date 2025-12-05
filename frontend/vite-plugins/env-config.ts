8cce5bfc7e079e8a9a4b72558a2e95921737c53d
Christian Fehmer <fehmer@users.noreply.github.com>
2025-12-05 19:45:12 +0100

    build: combine vite config into a single file (@fehmer) (#7190)
    - **build: replace dotenv with vite env variables (@fehmer)**
    - **build: combine vite config into a single file (@fehmer)**
    
    ---------
    
    Co-authored-by: Miodec <jack@monkeytype.com>

--- frontend/vite-plugins/env-config.ts
+++ frontend/vite-plugins/env-config.ts
@@ -1,40 +1,14 @@
 import { Plugin } from "vite";
 import { EnvConfig } from "virtual:env-config";
-import { config as dotenvConfig } from "dotenv";
-
-const envFile =
-  process.env["NODE_ENV"] === "production" ? ".env.production" : ".env";
-dotenvConfig({ path: envFile });
 
 const virtualModuleId = "virtual:env-config";
 const resolvedVirtualModuleId = "\0" + virtualModuleId;
 
-const developmentConfig: EnvConfig = {
-  isDevelopment: true,
-  backendUrl: fallbackEnv("BACKEND_URL", "http://localhost:5005"),
-  clientVersion: "DEVELOPMENT_CLIENT",
-  recaptchaSiteKey: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI",
-  quickLoginEmail: process.env["QUICK_LOGIN_EMAIL"],
-  quickLoginPassword: process.env["QUICK_LOGIN_PASSWORD"],
-};
-const productionConfig: Omit<EnvConfig, "clientVersion"> = {
-  isDevelopment: false,
-  backendUrl: fallbackEnv("BACKEND_URL", "https://api.monkeytype.com"),
-  recaptchaSiteKey: process.env["RECAPTCHA_SITE_KEY"] ?? "",
-  quickLoginEmail: undefined,
-  quickLoginPassword: undefined,
-};
-
-export function envConfig(
-  options:
-    | {
-        isDevelopment: true;
-      }
-    | {
-        isDevelopment: false;
-        clientVersion: string;
-      },
-): Plugin {
+export function envConfig(options: {
+  isDevelopment: boolean;
+  clientVersion: string;
+  env: Record<string, string>;
+}): Plugin {
   return {
     name: "virtual-env-config",
     resolveId(id) {
@@ -43,13 +17,31 @@ export function envConfig(
     },
     load(id) {
       if (id === resolvedVirtualModuleId) {
-        const envConfig = options.isDevelopment
-          ? developmentConfig
-          : {
-              ...productionConfig,
-              clientVersion: options.clientVersion,
-            };
+        const devConfig: EnvConfig = {
+          isDevelopment: true,
+          backendUrl: fallback(
+            options.env["BACKEND_URL"],
+            "http://localhost:5005",
+          ),
+          clientVersion: options.clientVersion,
+          recaptchaSiteKey: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI",
+          quickLoginEmail: options.env["QUICK_LOGIN_EMAIL"],
+          quickLoginPassword: options.env["QUICK_LOGIN_PASSWORD"],
+        };
 
+        const prodConfig: EnvConfig = {
+          isDevelopment: false,
+          backendUrl: fallback(
+            options.env["BACKEND_URL"],
+            "https://api.monkeytype.com",
+          ),
+          recaptchaSiteKey: options.env["RECAPTCHA_SITE_KEY"] ?? "",
+          quickLoginEmail: undefined,
+          quickLoginPassword: undefined,
+          clientVersion: options.clientVersion,
+        };
+
+        const envConfig = options.isDevelopment ? devConfig : prodConfig;
         return `
           export const envConfig = ${JSON.stringify(envConfig)};
         `;
@@ -59,8 +51,7 @@ export function envConfig(
   };
 }
 
-function fallbackEnv(envVariable: string, fallback: string): string {
-  const value = process.env[envVariable];
+function fallback(value: string | undefined | null, fallback: string): string {
   if (value === null || value === undefined || value === "") return fallback;
   return value;
 }
