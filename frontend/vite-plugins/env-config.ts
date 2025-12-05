ce76d6b9bb1fa9e3e8a8c7a3c7715483ae98829e
Christian Fehmer <cfe@sexy-developer.com>
2025-12-05 17:49:18 +0100

    cleanup env-config

--- frontend/vite-plugins/env-config.ts
+++ frontend/vite-plugins/env-config.ts
@@ -17,30 +17,31 @@ export function envConfig(options: {
     },
     load(id) {
       if (id === resolvedVirtualModuleId) {
-        const envConfig: EnvConfig = options.isDevelopment
-          ? {
-              isDevelopment: true,
-              backendUrl: fallback(
-                options.env["BACKEND_URL"],
-                "http://localhost:5005",
-              ),
-              clientVersion: "DEVELOPMENT_CLIENT",
-              recaptchaSiteKey: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI",
-              quickLoginEmail: options.env["QUICK_LOGIN_EMAIL"],
-              quickLoginPassword: options.env["QUICK_LOGIN_PASSWORD"],
-            }
-          : {
-              isDevelopment: false,
-              backendUrl: fallback(
-                options.env["BACKEND_URL"],
-                "https://api.monkeytype.com",
-              ),
-              recaptchaSiteKey: options.env["RECAPTCHA_SITE_KEY"] ?? "",
-              quickLoginEmail: undefined,
-              quickLoginPassword: undefined,
-              clientVersion: options.clientVersion,
-            };
+        const devConfig: EnvConfig = {
+          isDevelopment: true,
+          backendUrl: fallback(
+            options.env["BACKEND_URL"],
+            "http://localhost:5005",
+          ),
+          clientVersion: options.clientVersion,
+          recaptchaSiteKey: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI",
+          quickLoginEmail: options.env["QUICK_LOGIN_EMAIL"],
+          quickLoginPassword: options.env["QUICK_LOGIN_PASSWORD"],
+        };
 
+        const prodConfig: EnvConfig = {
+          isDevelopment: false,
+          backendUrl: fallback(
+            options.env["BACKEND_URL"],
+            "https://api.monkeytype.com",
+          ),
+          recaptchaSiteKey: options.env["RECAPTCHA_SITE_KEY"] ?? "",
+          quickLoginEmail: undefined,
+          quickLoginPassword: undefined,
+          clientVersion: options.clientVersion,
+        };
+
+        const envConfig = options.isDevelopment ? devConfig : prodConfig;
         return `
           export const envConfig = ${JSON.stringify(envConfig)};
         `;
