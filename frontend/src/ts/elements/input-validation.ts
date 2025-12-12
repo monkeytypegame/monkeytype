4b73503b39fdf11a65d3288707682012f9a47f2c
Miodec <jack@monkeytype.com>
2025-12-12 15:56:05 +0100

    pointless

--- frontend/src/ts/elements/input-validation.ts
+++ frontend/src/ts/elements/input-validation.ts
@@ -294,9 +294,7 @@ export function handleConfigInput<T extends ConfigKey>({
     if (Config[configName] === value) {
       return;
     }
-    const didConfigSave = setConfig(configName, value, {
-      nosave: false,
-    });
+    const didConfigSave = setConfig(configName, value);
 
     if (didConfigSave) {
       Notifications.add("Saved", 1, {
