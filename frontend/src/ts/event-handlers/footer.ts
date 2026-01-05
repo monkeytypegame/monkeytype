9adc78b06bd3b34007edcabe0a8f57672c1f2eff
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2026-01-05 19:47:23 +0200

    Prettier

--- frontend/src/ts/event-handlers/footer.ts
+++ frontend/src/ts/event-handlers/footer.ts
@@ -11,70 +11,64 @@ import { COMPATIBILITY_CHECK } from "@monkeytype/contracts";
 import { lastSeenServerCompatibility } from "../ape/adapters/ts-rest-adapter";
 import { qs } from "../utils/dom";
 
-qs("footer #commandLineMobileButton")
-  ?.on("click", async () => {
+qs("footer #commandLineMobileButton")?.on("click", async () => {
+  Commandline.show({
+    singleListOverride: false,
+  });
+});
+
+qs("footer #newVersionIndicator")?.on("click", (e) => {
+  e.stopPropagation();
+  qs("#newVersionIndicator")?.hide();
+});
+
+qs("footer .currentVersion")?.on("click", (e) => {
+  const event = e as MouseEvent;
+  if (event.shiftKey) {
+    alert(
+      JSON.stringify(
+        {
+          clientVersion: envConfig.clientVersion,
+          clientCompatibility: COMPATIBILITY_CHECK,
+          lastSeenServerCompatibility,
+        },
+        null,
+        2,
+      ),
+    );
+  } else {
+    VersionHistoryModal.show();
+  }
+});
+
+qs("footer .right .current-theme")?.on("click", async (event) => {
+  const e = event as MouseEvent;
+  if (e.shiftKey) {
+    if (Config.customTheme) {
+      setConfig("customTheme", false);
+      return;
+    }
+    if (
+      isAuthenticated() &&
+      (DB.getSnapshot()?.customThemes?.length ?? 0) < 1
+    ) {
+      Notifications.add("No custom themes!", 0);
+      setConfig("customTheme", false);
+      return;
+    }
+    setConfig("customTheme", true);
+  } else {
+    const subgroup = Config.customTheme ? "customTheme" : "themes";
     Commandline.show({
-      singleListOverride: false,
+      subgroupOverride: subgroup,
     });
-  });
+  }
+});
 
-qs("footer #newVersionIndicator")
-  ?.on("click", (e) => {
-    e.stopPropagation();
-    document.querySelector("#newVersionIndicator")?.classList.add("hidden");
-  });
+qs("footer #supportMeButton")?.on("click", () => {
+  SupportPopup.show();
+});
 
-qs("footer .currentVersion")
-  ?.on("click", (e) => {
-    const event = e as MouseEvent;
-    if (event.shiftKey) {
-      alert(
-        JSON.stringify(
-          {
-            clientVersion: envConfig.clientVersion,
-            clientCompatibility: COMPATIBILITY_CHECK,
-            lastSeenServerCompatibility,
-          },
-          null,
-          2,
-        ),
-      );
-    } else {
-      VersionHistoryModal.show();
-    }
-  });
-
-qs("footer .right .current-theme")
-  ?.on("click", async (event) => {
-    const e = event as MouseEvent;
-    if (e.shiftKey) {
-      if (Config.customTheme) {
-        setConfig("customTheme", false);
-        return;
-      }
-      if (
-        isAuthenticated() &&
-        (DB.getSnapshot()?.customThemes?.length ?? 0) < 1
-      ) {
-        Notifications.add("No custom themes!", 0);
-        setConfig("customTheme", false);
-        return;
-      }
-      setConfig("customTheme", true);
-    } else {
-      const subgroup = Config.customTheme ? "customTheme" : "themes";
-      Commandline.show({
-        subgroupOverride: subgroup,
-      });
-    }
-  });
-
-qs("footer #supportMeButton")
-  ?.on("click", () => {
-    SupportPopup.show();
-  });
-
-qs("footer #contactPopupButton")
-  ?.on("click", () => {
-    ContactModal.show();
-  });
+qs("footer #contactPopupButton")?.on("click", () => {
+  ContactModal.show();
+});
