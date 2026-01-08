205a5154dfb97343d59a27d5fc32de897f3bcedf
Miodec <jack@monkeytype.com>
2026-01-08 16:55:39 +0100

    dont export state setters

--- frontend/src/ts/signals/config.ts
+++ frontend/src/ts/signals/config.ts
@@ -2,15 +2,15 @@ import { createSignal } from "solid-js";
 import * as ConfigEvent from "../observables/config-event";
 import config from "../config";
 
-export const [getLiveSpeedStyle, setLiveSpeedStyle] = createSignal(
+const [getLiveSpeedStyle, setLiveSpeedStyle] = createSignal(
   config.liveSpeedStyle,
 );
-export const [getLiveAccStyle, setLiveAccStyle] = createSignal(
+const [getLiveAccStyle, setLiveAccStyle] = createSignal(config.liveSpeedStyle);
+const [getLiveBurstStyle, setLiveBurstStyle] = createSignal(
   config.liveSpeedStyle,
 );
-export const [getLiveBurstStyle, setLiveBurstStyle] = createSignal(
-  config.liveSpeedStyle,
-);
+
+export { getLiveSpeedStyle, getLiveAccStyle, getLiveBurstStyle };
 
 ConfigEvent.subscribe(({ key, newValue }) => {
   if (key === "liveSpeedStyle") {
