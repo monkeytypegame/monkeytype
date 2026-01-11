3163f1e80c515fcc84043b111e37f439e2d46a82
Jack <jack@monkeytype.com>
2026-01-11 22:15:54 +0100

    refactor: covert version button and version history modal to a component (@miodec, @fehmer) (#7343)
    Also adds a modal component.
    Also reworks mounting logic.
    
    ---------
    
    Co-authored-by: Christian Fehmer <cfe@sexy-developer.com>

--- frontend/src/ts/components/mount.tsx
+++ frontend/src/ts/components/mount.tsx
@@ -1,7 +1,22 @@
 import { render } from "solid-js/web";
-import { qsr } from "../utils/dom";
+import { qsa } from "../utils/dom";
 import { ScrollToTop } from "./ScrollToTop";
+import { VersionButton } from "./VersionButton";
+import { VersionHistoryModal } from "./VersionHistoryModal";
+import { JSXElement } from "solid-js";
+
+const components = [VersionButton, ScrollToTop, VersionHistoryModal];
+
+function mountToMountpoint(name: string, component: () => JSXElement): void {
+  for (const mountPoint of qsa(name)) {
+    render(() => component(), mountPoint.native);
+    mountPoint.native.replaceWith(...mountPoint.native.children);
+  }
+}
 
 export function mountComponents(): void {
-  render(() => <ScrollToTop />, qsr("body").native);
+  for (const component of components) {
+    const name = component.name.replace("[solid-refresh]", "");
+    mountToMountpoint(name, component);
+  }
 }
--- frontend/src/ts/event-handlers/footer.ts
+++ frontend/src/ts/event-handlers/footer.ts
@@ -5,40 +5,12 @@ import * as Notifications from "../elements/notifications";
 import * as Commandline from "../commandline/commandline";
 import * as SupportPopup from "../modals/support";
 import * as ContactModal from "../modals/contact";
-import * as VersionHistoryModal from "../modals/version-history";
-import { envConfig } from "virtual:env-config";
-import { COMPATIBILITY_CHECK } from "@monkeytype/contracts";
-import { lastSeenServerCompatibility } from "../ape/adapters/ts-rest-adapter";
 import { qs } from "../utils/dom";
 
 qs("footer #commandLineMobileButton")?.on("click", async () => {
   Commandline.show({
     singleListOverride: false,
   });
-});
-
-qs("footer #newVersionIndicator")?.on("click", (e) => {
-  e.stopPropagation();
-  qs("#newVersionIndicator")?.hide();
-});
-
-qs("footer .currentVersion")?.on("click", (e) => {
-  const event = e as MouseEvent;
-  if (event.shiftKey) {
-    alert(
-      JSON.stringify(
-        {
-          clientVersion: envConfig.clientVersion,
-          clientCompatibility: COMPATIBILITY_CHECK,
-          lastSeenServerCompatibility,
-        },
-        null,
-        2,
-      ),
-    );
-  } else {
-    VersionHistoryModal.show();
-  }
 });
 
 qs("footer .right .current-theme")?.on("click", async (event) => {
