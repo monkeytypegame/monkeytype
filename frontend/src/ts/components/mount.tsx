7eb4f79f03abb3a339741e8395e29cb05a453dd9
Christian Fehmer <cfe@sexy-developer.com>
2026-01-12 20:44:46 +0100

    fix component mount

--- frontend/src/ts/components/mount.tsx
+++ frontend/src/ts/components/mount.tsx
@@ -1,17 +1,18 @@
 import { render } from "solid-js/web";
 import { qsa } from "../utils/dom";
+
 import { JSXElement } from "solid-js";
 import { Footer } from "./Footer";
 import { Modals } from "./Modals";
 
-const components: Record<string, JSXElement> = {
-  Modals: <Modals />,
-  Footer: <Footer />,
+const components: Record<string, () => JSXElement> = {
+  Footer: () => <Footer />,
+  Modals: () => <Modals />,
 };
 
-function mountToMountpoint(name: string, component: JSXElement): void {
+function mountToMountpoint(name: string, component: () => JSXElement): void {
   for (const mountPoint of qsa(name)) {
-    render(() => component, mountPoint.native);
+    render(() => component(), mountPoint.native);
     mountPoint.native.replaceWith(...mountPoint.native.children);
   }
 }
