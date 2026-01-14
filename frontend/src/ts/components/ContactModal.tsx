c6683551495fe0bb64c9cb54e7fa4b1f0d9a6a95
Christian Fehmer <cfe@sexy-developer.com>
2026-01-14 01:52:23 +0100

    convert contactmodal to use anchor, fix css problems

--- frontend/src/ts/components/ContactModal.tsx
+++ frontend/src/ts/components/ContactModal.tsx
@@ -1,7 +1,7 @@
 import { JSXElement } from "solid-js";
 import { AnimatedModal } from "./AnimatedModal";
 import "./ContactModal.scss";
-import { hideCurrentModalAndClearChain } from "../stores/modals";
+import { Anchor } from "./Anchor";
 
 export function ContactModal(): JSXElement {
   return (
@@ -17,88 +17,49 @@ export function ContactModal(): JSXElement {
         email, update name or clear personal bests - you can do that in the
         settings page.
       </div>
-      <button type="button" onClick={() => hideCurrentModalAndClearChain()}>
-        hide and clear chain
-      </button>
       <div class="buttons">
-        <a
-          class="button"
-          target="_blank"
+        <Anchor
+          type="button"
           href="mailto:contact@monkeytype.com?subject=[Question] "
-          rel="noopener"
-        >
-          <div class="icon">
-            <i class="fas fa-question-circle"></i>
-          </div>
-          <div class="textGroup">
-            <div class="text">Question</div>
-          </div>
-        </a>
-        <a
-          class="button"
-          target="_blank"
+          fixedWidthIcon
+          icon="fas fa-question-circle"
+          text="Question"
+        />
+        <Anchor
+          type="button"
           href="mailto:contact@monkeytype.com?subject=[Feedback] "
-          rel="noopener"
-        >
-          <div class="icon">
-            <i class="fas fa-comment-dots"></i>
-          </div>
-          <div class="textGroup">
-            <div class="text">Feedback</div>
-          </div>
-        </a>
-        <a
-          class="button"
-          target="_blank"
+          fixedWidthIcon
+          icon="fas fa-comment-dots"
+          text="Feedback"
+        />
+        <Anchor
+          type="button"
           href="mailto:support@monkeytype.com?subject=[Bug] "
-          rel="noopener"
-        >
-          <div class="icon">
-            <i class="fas fa-bug"></i>
-          </div>
-          <div class="textGroup">
-            <div class="text">Bug Report</div>
-          </div>
-        </a>
-        <a
-          class="button"
-          target="_blank"
+          fixedWidthIcon
+          icon="fas fa-bug"
+          text="Bug Report"
+        />
+        <Anchor
+          type="button"
           href="mailto:support@monkeytype.com?subject=[Account] "
-          rel="noopener"
-        >
-          <div class="icon">
-            <i class="fas fa-user-circle"></i>
-          </div>
-          <div class="textGroup">
-            <div class="text">Account Help</div>
-          </div>
-        </a>
-        <a
-          class="button"
-          target="_blank"
+          fixedWidthIcon
+          icon="fas fa-user-circle"
+          text="Account Help"
+        />
+        <Anchor
+          type="button"
           href="mailto:jack@monkeytype.com?subject=[Business] "
-          rel="noopener"
-        >
-          <div class="icon">
-            <i class="fas fa-briefcase"></i>
-          </div>
-          <div class="textGroup">
-            <div class="text">Business Inquiry</div>
-          </div>
-        </a>
-        <a
-          class="button"
-          target="_blank"
+          fixedWidthIcon
+          icon="fas fa-briefcase"
+          text="Business Inquiry"
+        />
+        <Anchor
+          type="button"
           href="mailto:contact@monkeytype.com?subject=[Other] "
-          rel="noopener"
-        >
-          <div class="icon">
-            <i class="fas fa-ellipsis-h"></i>
-          </div>
-          <div class="textGroup">
-            <div class="text">Other</div>
-          </div>
-        </a>
+          fixedWidthIcon
+          icon="fas fa-ellipsis-h"
+          text="Other"
+        />
       </div>
     </AnimatedModal>
   );
