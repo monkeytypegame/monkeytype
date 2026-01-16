6f702b2c30d472d5caf1e9edf592a47b9d927718
Miodec <jack@monkeytype.com>
2026-01-16 22:03:37 +0100

    extra space

--- frontend/src/ts/components/utils/TailwindMediaQueryDebugger.tsx
+++ frontend/src/ts/components/utils/TailwindMediaQueryDebugger.tsx
@@ -2,7 +2,7 @@ import { JSXElement } from "solid-js";
 
 export function TailwindMediaQueryDebugger(): JSXElement {
   return (
-    <div class="fixed left-2 top-12 z-999999999999999 bg-sub-alt text-text px-2 py-1  font-bold font-mono rounded shadow-lg">
+    <div class="fixed left-2 top-12 z-999999999999999 bg-sub-alt text-text px-2 py-1 font-bold font-mono rounded shadow-lg">
       <div class="hidden 2xl:block">2xl</div>
       <div class="hidden xl:block 2xl:hidden">xl</div>
       <div class="hidden lg:block xl:hidden">lg</div>
