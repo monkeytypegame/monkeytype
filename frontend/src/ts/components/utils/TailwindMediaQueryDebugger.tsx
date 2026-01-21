22d028249690329f6184c05492112ba47bedc6fb
Christian Fehmer <fehmer@users.noreply.github.com>
2026-01-21 11:26:39 +0100

    refactor: add breakpoints signal based on tailwind (@fehmer) (#7403)
    Replace  `useTailwindBreakpoints` hook with a breakpoint signal.
    
    - Example `bp().lg` to detect a large screen
    - create media queries based on the css variables for breakpoints
    - use change event listeners on the media queries to update the signal,
    debounced
    - rename `2xl` to `xxl` so it can be used without array access
    `bp()["2xl"]`

--- frontend/src/ts/components/utils/TailwindMediaQueryDebugger.tsx
+++ frontend/src/ts/components/utils/TailwindMediaQueryDebugger.tsx
@@ -1,11 +1,12 @@
-import { JSXElement, Show } from "solid-js";
+import { JSXElement, Match, Show, Switch } from "solid-js";
 
+import { bp } from "../../signals/breakpoints";
 import { isDevEnvironment } from "../../utils/misc";
 
 export function TailwindMediaQueryDebugger(): JSXElement {
   return (
     <Show when={isDevEnvironment()}>
-      <div class="bg-sub-alt text-text fixed top-12 left-2 z-999999999999999 rounded px-2 py-1 font-mono font-bold shadow-lg">
+      <div class="bg-sub-alt text-text fixed top-12 left-2 z-999999999999999 flex rounded px-2 py-1 font-mono font-bold shadow-lg">
         <div class="hidden 2xl:block">2xl</div>
         <div class="hidden xl:block 2xl:hidden">xl</div>
         <div class="hidden lg:block xl:hidden">lg</div>
@@ -13,6 +14,17 @@ export function TailwindMediaQueryDebugger(): JSXElement {
         <div class="hidden sm:block md:hidden">sm</div>
         <div class="xs:block hidden sm:hidden">xs</div>
         <div class="xs:hidden">xxs</div>
+        <div>
+          /
+          <Switch>
+            <Match when={bp().xxl}>2xl</Match>
+            <Match when={bp().xl}>xl</Match>
+            <Match when={bp().lg}>lg</Match>
+            <Match when={bp().md}>md</Match>
+            <Match when={bp().sm}>sm</Match>
+            <Match when={bp().xs}>xs</Match>
+          </Switch>
+        </div>
       </div>
     </Show>
   );
---
+++ frontend/src/ts/signals/breakpoints.ts
@@ -0,0 +1,53 @@
+import { Accessor, createSignal, onCleanup } from "solid-js";
+import { debounce } from "throttle-debounce";
+
+type BreakpointKeys = "xxl" | "xl" | "lg" | "md" | "sm" | "xs" | "xxs";
+type Breakpoints = Record<BreakpointKeys, boolean>;
+
+const styles = getComputedStyle(document.documentElement);
+const tw: Record<BreakpointKeys, number> = {
+  xxs: parseInt(styles.getPropertyValue("--breakpoint-xxs")),
+  xs: parseInt(styles.getPropertyValue("--breakpoint-xs")),
+  sm: parseInt(styles.getPropertyValue("--breakpoint-sm")),
+  md: parseInt(styles.getPropertyValue("--breakpoint-md")),
+  lg: parseInt(styles.getPropertyValue("--breakpoint-lg")),
+  xl: parseInt(styles.getPropertyValue("--breakpoint-xl")),
+  xxl: parseInt(styles.getPropertyValue("--breakpoint-2xl")),
+};
+
+export const bp = createBreakpoints(tw);
+
+function createBreakpoints(
+  breakpoints: Record<BreakpointKeys, number>,
+): Accessor<Breakpoints> {
+  const queries = Object.fromEntries(
+    Object.entries(breakpoints).map(([key, px]) => [
+      key,
+      window.matchMedia(`(min-width: ${px}px)`),
+    ]),
+  );
+
+  const [matches, setMatches] = createSignal(
+    Object.fromEntries(Object.entries(queries).map(([k, q]) => [k, q.matches])),
+  );
+
+  const update = debounce(125, () =>
+    setMatches(
+      Object.fromEntries(
+        Object.entries(queries).map(([k, q]) => [k, q.matches]),
+      ),
+    ),
+  );
+
+  for (const q of Object.values(queries)) {
+    q.addEventListener("change", update);
+  }
+
+  onCleanup(() => {
+    for (const q of Object.values(queries)) {
+      q.removeEventListener("change", update);
+    }
+  });
+
+  return matches as Accessor<Record<BreakpointKeys, boolean>>;
+}
