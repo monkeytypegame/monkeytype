0e72ddda2923af6e6aa14cb665a360105dcefcad
Christian Fehmer <cfe@sexy-developer.com>
2026-01-23 09:37:35 +0100

    perf: use local cache for configurations and psas (@fehmer)

--- backend/src/api/controllers/psa.ts
+++ backend/src/api/controllers/psa.ts
@@ -3,8 +3,21 @@ import * as PsaDAL from "../../dal/psa";
 import { MonkeyResponse } from "../../utils/monkey-response";
 import { replaceObjectIds } from "../../utils/misc";
 import { MonkeyRequest } from "../types";
+import { PSA } from "@monkeytype/schemas/psas";
+
+const UPDATE_INTERVAL = 5 * 60 * 1000; // 5 minutes
+let cached: PSA[] = [];
+let lastFetchTime = 0;
 
 export async function getPsas(_req: MonkeyRequest): Promise<GetPsaResponse> {
-  const data = await PsaDAL.get();
-  return new MonkeyResponse("PSAs retrieved", replaceObjectIds(data));
+  return new MonkeyResponse("PSAs retrieved", await getCachedPsas());
+}
+
+async function getCachedPsas(): Promise<PSA[]> {
+  if (lastFetchTime < Date.now() - UPDATE_INTERVAL) {
+    lastFetchTime = Date.now();
+    cached = replaceObjectIds(await PsaDAL.get());
+  }
+
+  return cached;
 }
