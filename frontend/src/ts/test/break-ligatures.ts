b539944a0394a31636b29e36bc98b52b60568bc9
byseif21 <byseif21@gmail.com>
2026-01-28 15:22:47 +0200

    fix moving words

--- frontend/src/ts/test/break-ligatures.ts
+++ frontend/src/ts/test/break-ligatures.ts
@@ -7,11 +7,14 @@ export function set(
 ): void {
   if (state === "normal") {
     wordEl.removeClass("broken-ligatures");
+    wordEl.setStyle({ width: "" });
     return;
   }
 
   const wordsEl = qsr("#words");
   if (Config.typedEffect === "dots" && wordsEl.hasClass("withLigatures")) {
+    const width = wordEl.native.getBoundingClientRect().width;
+    wordEl.setStyle({ width: `${width}px` });
     wordEl.addClass("broken-ligatures");
   }
 }
