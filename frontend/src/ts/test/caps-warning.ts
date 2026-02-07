370bb82d2096512c8c57a961c61fde9ea637dceb
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2026-02-07 09:52:41 +0200

    Revert "Factory"
    This reverts commit f29ffd4dc50987b9087fd819d8852bbe8a0ee64b.

--- frontend/src/ts/test/caps-warning.ts
+++ frontend/src/ts/test/caps-warning.ts
@@ -37,65 +37,38 @@ function isCapsLockOn(event: KeyboardEvent): boolean {
   return event.getModifierState("CapsLock");
 }
 
-function updateCapsKeyupFactory(os: string): (event: KeyboardEvent) => void {
+function updateCapsKeyup(event: KeyboardEvent): void {
   if (os === "Mac") {
     // macOs sends only keydown when enabling CapsLock and only keyup when disabling.
-    return (event: KeyboardEvent) => {
-      if (event.key === "CapsLock") {
-        capsState = isCapsLockOn(event);
-      }
-    };
-  }
-  if (os === "Windows") {
+    if (event.key === "CapsLock") {
+      capsState = isCapsLockOn(event);
+    }
+  } else if (os === "Windows") {
     // Windows always sends the correct state on keyup (for CapsLock and for regular keys)
-    return (event: KeyboardEvent) => (capsState = isCapsLockOn(event));
+    capsState = isCapsLockOn(event);
+  } else if (event.key !== "CapsLock") {
+    capsState = isCapsLockOn(event);
   }
-
-  if (os === "Linux") {
-    return (event: KeyboardEvent) => {
-      if (event.key !== "CapsLock") {
-        capsState = isCapsLockOn(event);
-      }
-    };
-  }
-
-  return (event: KeyboardEvent) => {
-    return;
-  };
+  updateCapsWarningVisibility();
 }
 
-function updateCapsKeydownFactory(os: string): (event: KeyboardEvent) => void {
+function updateCapsKeydown(event: KeyboardEvent): void {
   if (os === "Mac") {
     // macOs sends only keydown when enabling CapsLock and only keyup when disabling.
-    return (event: KeyboardEvent) => {
-      capsState = isCapsLockOn(event);
-      updateCapsWarningVisibility();
-    };
-  }
-
-  if (os === "Linux") {
+    capsState = isCapsLockOn(event);
+    updateCapsWarningVisibility();
+  } else if (os === "Linux") {
     /* Linux sends the correct state before the toggle only on keydown,
      * so we invert the modifier state
      */
-
-    return (event: KeyboardEvent) => {
-      if (event.key === "CapsLock") {
-        capsState = !isCapsLockOn(event);
-      }
-    };
+    if (event.key === "CapsLock") {
+      capsState = !isCapsLockOn(event);
+    }
   }
-
-  return (event: KeyboardEvent) => {
-    return;
-  };
 }
-
-const updateCapsKeyup = updateCapsKeyupFactory(os);
-const updateCapsKeydown = updateCapsKeydownFactory(os);
 
 document.addEventListener("keyup", (event) => {
   updateCapsKeyup(event);
-  updateCapsWarningVisibility();
 });
 
 document.addEventListener("keydown", (event) => {
