c284938781174b344105bc0df56722aba8d9f223
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2026-02-14 09:14:04 +0200

    Frontend

--- frontend/src/ts/components/ui/table/DataTable.tsx
+++ frontend/src/ts/components/ui/table/DataTable.tsx
@@ -64,6 +64,7 @@ export function DataTable<TData, TValue = unknown>(
   props: DataTableProps<TData, TValue>,
 ): JSXElement {
   const [sorting, setSorting] = useLocalStorage<SortingState>({
+    //oxlint-disable-next-line solid/reactivity
     key: `${props.id}Sort`,
     schema: SortingStateSchema,
     fallback: [],
