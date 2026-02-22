328d0ff69fa4dcb5ca813a34a7059c8f028cae76
Kazeno <86889948+kazeno-uta@users.noreply.github.com>
2026-02-22 03:35:18 +0100

    feat(layout): add miligram layout (@kazeno-uta)

--- packages/schemas/src/layouts.ts
+++ packages/schemas/src/layouts.ts
@@ -232,6 +232,7 @@ export const LayoutNameSchema = z.enum(
     "persian_standard_colemak",
     "ergo_split46",
     "tamil99",
+    "miligram"
   ],
   {
     errorMap: customEnumErrorHandler("Must be a supported layout"),
