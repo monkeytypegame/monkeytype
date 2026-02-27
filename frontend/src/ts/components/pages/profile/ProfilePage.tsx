63214f48ba15998c2279134c025391c4d3b5bcb7
Christian Fehmer <cfe@sexy-developer.com>
2026-02-27 18:28:05 +0100

    fix

--- frontend/src/ts/components/pages/profile/ProfilePage.tsx
+++ frontend/src/ts/components/pages/profile/ProfilePage.tsx
@@ -1,55 +1,31 @@
 import { useQuery } from "@tanstack/solid-query";
-import { createSignal, JSXElement, Show } from "solid-js";
+import { JSXElement, Show } from "solid-js";
 
-import Page, { PageName } from "../../../pages/page";
+import { PageName } from "../../../pages/page";
 import { getUserProfile } from "../../../queries/profile";
-import { getActivePage } from "../../../signals/core";
-import { onDOMReady, qsr } from "../../../utils/dom";
-import * as Skeleton from "../../../utils/skeleton";
+import { getActivePage, getSelectedProfileName } from "../../../signals/core";
 import AsyncContent from "../../common/AsyncContent";
 import { Fa } from "../../common/Fa";
 import { UserProfile } from "./UserProfile";
-
-const [currentName, setCurrentName] = createSignal<string | undefined>(
-  undefined,
-);
 
 const pageName: PageName = "profile";
 export function ProfilePage(): JSXElement {
   const isOpen = () => getActivePage() === pageName;
 
   const profileQuery = useQuery(() => ({
-    ...getUserProfile(currentName() as string),
-    enabled: isOpen() && currentName() !== undefined,
+    ...getUserProfile(getSelectedProfileName() as string),
+    enabled: isOpen() && getSelectedProfileName() !== undefined,
   }));
 
   return (
-    <div class="flex h-full items-center justify-center text-lg text-error">
+    <div class="flex h-full items-center justify-center text-lg">
       <AsyncContent query={profileQuery} ignoreError={true}>
         {(profile) => <UserProfile profile={profile} />}
       </AsyncContent>
       <Show when={profileQuery.isError}>
         <Fa icon="fa-times" />
-        &nbsp;User {currentName()} not found
+        &nbsp;User {getSelectedProfileName()} not found
       </Show>
     </div>
   );
 }
-
-export const page = new Page({
-  id: "profile",
-  element: qsr(".page.pageProfile"),
-  path: "/profile",
-  afterHide: async (): Promise<void> => {
-    Skeleton.remove("pageProfile");
-  },
-  beforeShow: async (options): Promise<void> => {
-    Skeleton.append("pageProfile", "main");
-    const userName = options.params?.["uidOrName"];
-    setCurrentName(userName);
-  },
-});
-
-onDOMReady(() => {
-  Skeleton.add("pageProfile");
-});
