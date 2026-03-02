85940d3dc2886d4173a5f5c92a23cced3c137092
Miodec <jack@monkeytype.com>
2026-03-02 23:23:53 +0100

    move menu down

--- frontend/src/ts/components/layout/header/AccountMenu.tsx
+++ frontend/src/ts/components/layout/header/AccountMenu.tsx
@@ -17,69 +17,69 @@ export function AccountMenu(props: Props): JSXElement {
     "w-full justify-start rounded-none px-3 py-2 whitespace-nowrap gap-2";
 
   return (
-    <AnimeShow
-      when={props.show}
-      class="absolute right-0 z-1000 w-auto overflow-hidden rounded bg-sub-alt text-xs ring-6 ring-bg"
-    >
-      <Button
-        text="User stats"
-        class={buttonClass}
-        fa={{
-          icon: "fa-chart-line",
-          fixedWidth: true,
-        }}
-        href="/account"
-        router-link
-      />
-      <Show when={getServerConfiguration()?.connections.enabled}>
+    <AnimeShow when={props.show} class="absolute right-0 z-1000 w-auto text-xs">
+      <div class="h-2"></div>
+      <div class="overflow-hidden rounded bg-sub-alt ring-6 ring-bg">
         <Button
-          text="Friends"
-          class={buttonClass + " relative"}
+          text="User stats"
+          class={buttonClass}
           fa={{
-            icon: "fa-user-friends",
+            icon: "fa-chart-line",
             fixedWidth: true,
           }}
-          href="/friends"
+          href="/account"
           router-link
-        >
-          <NotificationBubble
-            show={props.showFriendsNotificationBubble ?? false}
-            variant="center"
-            class="right-2 left-auto"
-          />
-        </Button>
-      </Show>
-      <Button
-        text="Public profile"
-        class={buttonClass}
-        fa={{
-          icon: "fa-globe-americas",
-          fixedWidth: true,
-        }}
-        href={"/profile/" + (getSnapshot() as MiniSnapshot).name}
-        router-link
-      />
-      <Button
-        text="Account settings"
-        class={buttonClass}
-        fa={{
-          icon: "fa-cog",
-          fixedWidth: true,
-        }}
-        href="/account-settings"
-        router-link
-      />
-      <Button
-        text="Signout"
-        class={buttonClass}
-        fa={{
-          icon: "fa-sign-out-alt",
-          fixedWidth: true,
-        }}
-        onClick={() => {
-          signOut();
-        }}
-      />
+        />
+        <Show when={getServerConfiguration()?.connections.enabled}>
+          <Button
+            text="Friends"
+            class={buttonClass + " relative"}
+            fa={{
+              icon: "fa-user-friends",
+              fixedWidth: true,
+            }}
+            href="/friends"
+            router-link
+          >
+            <NotificationBubble
+              show={props.showFriendsNotificationBubble ?? false}
+              variant="center"
+              class="right-2 left-auto"
+            />
+          </Button>
+        </Show>
+        <Button
+          text="Public profile"
+          class={buttonClass}
+          fa={{
+            icon: "fa-globe-americas",
+            fixedWidth: true,
+          }}
+          href={"/profile/" + (getSnapshot() as MiniSnapshot).name}
+          router-link
+        />
+        <Button
+          text="Account settings"
+          class={buttonClass}
+          fa={{
+            icon: "fa-cog",
+            fixedWidth: true,
+          }}
+          href="/account-settings"
+          router-link
+        />
+        <Button
+          text="Signout"
+          class={buttonClass}
+          fa={{
+            icon: "fa-sign-out-alt",
+            fixedWidth: true,
+          }}
+          onClick={() => {
+            signOut();
+          }}
+        />
+      </div>
     </AnimeShow>
   );
 }
