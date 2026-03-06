6eb7095a5adc73cb383789057f560c2ab167a348
Miodec <jack@monkeytype.com>
2026-03-06 17:25:32 +0100

    vertical variant

--- frontend/src/ts/components/common/Separator.tsx
+++ frontend/src/ts/components/common/Separator.tsx
@@ -2,8 +2,17 @@ import { JSXElement } from "solid-js";
 
 import { cn } from "../../utils/cn";
 
-export function Separator(props: { class?: string }): JSXElement {
+export function Separator(props: {
+  class?: string;
+  vertical?: true;
+}): JSXElement {
   return (
-    <div class={cn(`h-1 w-full rounded bg-sub-alt ${props.class ?? ""}`)}></div>
+    <div
+      class={cn(
+        props.vertical ? "h-full w-1" : "h-1 w-full",
+        `rounded bg-sub-alt`,
+        props.class,
+      )}
+    ></div>
   );
 }
