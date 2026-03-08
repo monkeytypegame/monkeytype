90933d6406d7a74020d627ac8addbef0ae472d69
Christian Fehmer <cfe@sexy-developer.com>
2026-03-08 17:34:26 +0100

    fix:(profile): fix style issues (@fehmer)

--- frontend/src/ts/components/pages/profile/UserDetails.tsx
+++ frontend/src/ts/components/pages/profile/UserDetails.tsx
@@ -390,7 +390,7 @@ function BioAndKeyboard(props: {
       ></div>
       <div
         class={cn(
-          "flex h-full flex-col content-center justify-around gap-2 overflow-hidden text-sm",
+          "flex h-full flex-col content-center justify-around gap-2 overflow-hidden text-sm whitespace-pre-line",
           props.variant === "hasBioOrKeyboard" && "md:order-4",
           props.variant === "full" && "md:col-span-2 lg:order-4 lg:col-span-1",
         )}
