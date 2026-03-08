015ab03b81cc55ade158992d3e87c31540e31fc6
Miodec <jack@monkeytype.com>
2026-03-08 13:20:31 +0100

    button story

--- frontend/storybook/stories/Button.stories.tsx
+++ frontend/storybook/stories/Button.stories.tsx
@@ -53,36 +53,104 @@ export const Default = meta.story({
 
 export const AllVariants = meta.story({
   render: () => (
-    <div class="grid grid-cols-6 gap-4 text-text">
-      <div class="self-center">Button Type</div>
+    <div class="grid grid-cols-5 gap-4 text-text">
+      <div class="self-center">Button</div>
       <Button text="Default" onClick={fn()} />
       <Button text="Active" active={true} onClick={fn()} />
       <Button text="Disabled" disabled={true} onClick={fn()} />
       <Button
-        text="Icon"
+        text="Active + Disabled"
+        active={true}
+        disabled={true}
+        onClick={fn()}
+      />
+
+      <div class="self-center">Button + Icon</div>
+      <Button
+        text="Default"
         fa={{ icon: "fa-cog", variant: "solid" }}
         onClick={fn()}
       />
-      <Button fa={{ icon: "fa-cog", variant: "solid" }} onClick={fn()} />
-      <div class="self-center">Text Type</div>
+      <Button
+        text="Active"
+        fa={{ icon: "fa-cog", variant: "solid" }}
+        active={true}
+        onClick={fn()}
+      />
+      <Button
+        text="Disabled"
+        fa={{ icon: "fa-cog", variant: "solid" }}
+        disabled={true}
+        onClick={fn()}
+      />
+      <Button
+        text="Active + Disabled"
+        fa={{ icon: "fa-cog", variant: "solid" }}
+        active={true}
+        disabled={true}
+        onClick={fn()}
+      />
 
+      <div class="self-center">Icon Only</div>
+      <Button fa={{ icon: "fa-cog", variant: "solid" }} onClick={fn()} />
+      <Button
+        fa={{ icon: "fa-cog", variant: "solid" }}
+        active={true}
+        onClick={fn()}
+      />
+      <Button
+        fa={{ icon: "fa-cog", variant: "solid" }}
+        disabled={true}
+        onClick={fn()}
+      />
+      <Button
+        fa={{ icon: "fa-cog", variant: "solid" }}
+        active={true}
+        disabled={true}
+        onClick={fn()}
+      />
+
+      <div class="self-center">Text</div>
       <Button variant="text" text="Default" onClick={fn()} />
       <Button variant="text" text="Active" active={true} onClick={fn()} />
       <Button variant="text" text="Disabled" disabled={true} onClick={fn()} />
       <Button
         variant="text"
-        text="Icon"
+        text="Active + Disabled"
+        active={true}
+        disabled={true}
+        onClick={fn()}
+      />
+
+      <div class="self-center">Text + Icon</div>
+      <Button
+        variant="text"
+        text="Default"
         fa={{ icon: "fa-cog", variant: "solid" }}
         onClick={fn()}
       />
       <Button
         variant="text"
+        text="Active"
         fa={{ icon: "fa-cog", variant: "solid" }}
+        active={true}
+        onClick={fn()}
+      />
+      <Button
+        variant="text"
+        text="Disabled"
+        fa={{ icon: "fa-cog", variant: "solid" }}
+        disabled={true}
+        onClick={fn()}
+      />
+      <Button
+        variant="text"
+        text="Active + Disabled"
+        fa={{ icon: "fa-cog", variant: "solid" }}
+        active={true}
+        disabled={true}
         onClick={fn()}
       />
     </div>
   ),
-  args: {
-    text: "Button",
-  },
 });
