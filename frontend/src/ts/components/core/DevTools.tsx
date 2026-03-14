2d0f062857beff91dba2ca56fae252fb44d4b1f6
Miodec <jack@monkeytype.com>
2026-03-14 08:54:54 +0100

    dont bundle solid devtools in

--- frontend/src/ts/components/core/DevTools.tsx
+++ frontend/src/ts/components/core/DevTools.tsx
@@ -1,6 +1,21 @@
-import { SolidQueryDevtools } from "@tanstack/solid-query-devtools";
-import { JSXElement } from "solid-js";
+import { type Component, type JSXElement, lazy, Show } from "solid-js";
+
+let SolidQueryDevtools: Component | undefined;
+
+if (import.meta.env.DEV) {
+  SolidQueryDevtools = lazy(async () => {
+    const m = await import("@tanstack/solid-query-devtools");
+    return { default: m.SolidQueryDevtools };
+  });
+}
 
 export function DevTools(): JSXElement {
-  return <SolidQueryDevtools />;
+  return (
+    <Show when={SolidQueryDevtools}>
+      {(Devtools) => {
+        const C = Devtools();
+        return <C />;
+      }}
+    </Show>
+  );
 }
