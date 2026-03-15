fed649bd74499e9ea61205607a5595c96e57daa1
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2026-03-15 14:43:58 +0200

    Remove inline styles

--- frontend/src/ts/test/result.ts
+++ frontend/src/ts/test/result.ts
@@ -921,7 +921,7 @@ function updateQuoteFavorite(randomQuote: Quote | null): void {
   const icon = qs(".pageTest #result #favoriteQuoteButton .icon");
 
   if (Config.mode !== "quote" || !isAuthenticated()) {
-    icon?.getParent()?.setStyle({ display: "none" });
+    icon?.getParent()?.hide();
     return;
   }
 
