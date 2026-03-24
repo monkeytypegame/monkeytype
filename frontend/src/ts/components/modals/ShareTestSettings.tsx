20fbaa1a308ff6c53dde758c3902818ce2d62151
Miodec <jack@monkeytype.com>
2026-03-24 19:39:13 +0100

    use tsform

--- frontend/src/ts/components/modals/ShareTestSettings.tsx
+++ frontend/src/ts/components/modals/ShareTestSettings.tsx
@@ -2,8 +2,9 @@ import type { Difficulty, FunboxName } from "@monkeytype/schemas/configs";
 import type { CustomTextSettings } from "@monkeytype/schemas/results";
 import type { Mode, Mode2 } from "@monkeytype/schemas/shared";
 
+import { createForm } from "@tanstack/solid-form";
 import { compressToURI } from "lz-ts";
-import { createSignal, JSXElement, Show } from "solid-js";
+import { JSXElement, Show } from "solid-js";
 
 import { getConfig } from "../../config/store";
 import * as CustomText from "../../test/custom-text";
@@ -13,6 +14,7 @@ import { getMode2 } from "../../utils/misc";
 import { capitalizeFirstLetter } from "../../utils/strings";
 import { AnimatedModal } from "../common/AnimatedModal";
 import { Fa } from "../common/Fa";
+import { Checkbox } from "../ui/form/Checkbox";
 
 type SharedTestSettings = [
   Mode | null,
@@ -26,34 +28,44 @@ type SharedTestSettings = [
 ];
 
 export function ShareTestSettings(): JSXElement {
-  const [mode, setMode] = createSignal(false);
-  const [mode2, setMode2] = createSignal(false);
-  const [customText, setCustomText] = createSignal(false);
-  const [punctuation, setPunctuation] = createSignal(false);
-  const [numbers, setNumbers] = createSignal(false);
-  const [language, setLanguage] = createSignal(false);
-  const [difficulty, setDifficulty] = createSignal(false);
-  const [funbox, setFunbox] = createSignal(false);
+  const form = createForm(() => ({
+    defaultValues: {
+      mode: false,
+      mode2: false,
+      customText: false,
+      punctuation: false,
+      numbers: false,
+      language: false,
+      difficulty: false,
+      funbox: false,
+    },
+  }));
+
+  const formValues = form.useStore((s) => s.values);
 
   const url = () => {
     const baseUrl = location.origin + "?testSettings=";
     const settings: SharedTestSettings = new Array(8).fill(
       null,
     ) as SharedTestSettings;
+    const values = formValues();
 
     const settingsMap = [
-      { enabled: mode, getValue: () => getConfig.mode },
-      { enabled: mode2, getValue: () => getMode2(getConfig, currentQuote) },
-      { enabled: customText, getValue: () => CustomText.getData() },
-      { enabled: punctuation, getValue: () => getConfig.punctuation },
-      { enabled: numbers, getValue: () => getConfig.numbers },
-      { enabled: language, getValue: () => getConfig.language },
-      { enabled: difficulty, getValue: () => getConfig.difficulty },
-      { enabled: funbox, getValue: () => getConfig.funbox },
+      { enabled: values.mode, getValue: () => getConfig.mode },
+      {
+        enabled: values.mode2,
+        getValue: () => getMode2(getConfig, currentQuote),
+      },
+      { enabled: values.customText, getValue: () => CustomText.getData() },
+      { enabled: values.punctuation, getValue: () => getConfig.punctuation },
+      { enabled: values.numbers, getValue: () => getConfig.numbers },
+      { enabled: values.language, getValue: () => getConfig.language },
+      { enabled: values.difficulty, getValue: () => getConfig.difficulty },
+      { enabled: values.funbox, getValue: () => getConfig.funbox },
     ];
 
     for (const [index, { enabled, getValue }] of settingsMap.entries()) {
-      if (enabled()) {
+      if (enabled) {
         settings[index] = getValue();
       }
     }
@@ -81,113 +93,148 @@ export function ShareTestSettings(): JSXElement {
 
   return (
     <AnimatedModal id="ShareTestSettings" title="Share test settings">
-      <label class="grid grid-cols-[max-content_auto] items-center gap-2">
-        <input
-          type="checkbox"
-          checked={mode()}
-          onChange={(e) => setMode(e.currentTarget.checked)}
-        />
-        <div>Mode</div>
-        <div class="col-start-2 text-em-xs text-sub">
-          {capitalizeFirstLetter(getConfig.mode)}
-        </div>
-      </label>
+      <form.Field name="mode">
+        {(field) => (
+          <Checkbox
+            field={() => field()}
+            class="items-start"
+            label={
+              <div>
+                <div>Mode</div>
+                <div class="text-em-xs text-sub">
+                  {capitalizeFirstLetter(getConfig.mode)}
+                </div>
+              </div>
+            }
+          />
+        )}
+      </form.Field>
 
       <Show when={getConfig.mode === "custom"}>
-        <label
-          class={cn(
-            "ml-8 grid grid-cols-[max-content_auto] items-center gap-2",
-            mode() ? "" : "pointer-events-none opacity-33",
+        <form.Field name="customText">
+          {(field) => (
+            <Checkbox
+              field={() => field()}
+              class={cn(
+                "ml-8 items-start",
+                formValues().mode ? "" : "pointer-events-none opacity-33",
+              )}
+              label={
+                <div>
+                  <div>Custom text</div>
+                  <div class="text-em-xs text-sub">
+                    {CustomText.getData().text.length}{" "}
+                    {CustomText.getData().pipeDelimiter ? "sections" : "words"},{" "}
+                    {CustomText.getData().mode} mode,{" "}
+                    {CustomText.getData().limit.value}{" "}
+                    {CustomText.getData().limit.mode} limit
+                  </div>
+                </div>
+              }
+            />
           )}
-        >
-          <input
-            type="checkbox"
-            checked={customText()}
-            onChange={(e) => setCustomText(e.currentTarget.checked)}
-          />
-          <div>Custom text</div>
-          <div class="col-start-2 text-em-xs text-sub">
-            {CustomText.getData().text.length}{" "}
-            {CustomText.getData().pipeDelimiter ? "sections" : "words"},{" "}
-            {CustomText.getData().mode} mode, {CustomText.getData().limit.value}{" "}
-            {CustomText.getData().limit.mode} limit
-          </div>
-        </label>
+        </form.Field>
       </Show>
       <Show when={getConfig.mode !== "custom" && getConfig.mode !== "zen"}>
-        <label
-          class={cn(
-            "ml-8 grid grid-cols-[max-content_auto] items-center gap-2",
-            mode() ? "" : "pointer-events-none opacity-33",
+        <form.Field name="mode2">
+          {(field) => (
+            <Checkbox
+              field={() => field()}
+              class={cn(
+                "ml-8 items-start",
+                formValues().mode ? "" : "pointer-events-none opacity-33",
+              )}
+              label={
+                <div>
+                  <div>Mode2</div>
+                  <div class="text-em-xs text-sub">{mode2subtext()}</div>
+                </div>
+              }
+            />
           )}
-        >
-          <input
-            type="checkbox"
-            checked={mode2()}
-            onChange={(e) => setMode2(e.currentTarget.checked)}
-          />
-          <div>Mode2</div>
-          <div class="col-start-2 text-em-xs text-sub">{mode2subtext()}</div>
-        </label>
+        </form.Field>
       </Show>
 
       <Show when={getConfig.mode !== "zen"}>
-        <label class="grid grid-cols-[max-content_auto] items-center gap-2">
-          <input
-            type="checkbox"
-            checked={punctuation()}
-            onChange={(e) => setPunctuation(e.currentTarget.checked)}
-          />
-          <div>Punctuation</div>
-          <div class="col-start-2 text-em-xs text-sub">
-            {getConfig.punctuation ? "enabled" : "disabled"}
-          </div>
-        </label>
-        <label class="grid grid-cols-[max-content_auto] items-center gap-2">
-          <input
-            type="checkbox"
-            checked={numbers()}
-            onChange={(e) => setNumbers(e.currentTarget.checked)}
-          />
-          <div>Numbers</div>
-          <div class="col-start-2 text-em-xs text-sub">
-            {getConfig.numbers ? "enabled" : "disabled"}
-          </div>
-        </label>
-        <label class="grid grid-cols-[max-content_auto] items-center gap-2">
-          <input
-            type="checkbox"
-            checked={language()}
-            onChange={(e) => setLanguage(e.currentTarget.checked)}
-          />
-          <div>Language</div>
-          <div class="col-start-2 text-em-xs text-sub">
-            {getConfig.language}
-          </div>
-        </label>
-        <label class="grid grid-cols-[max-content_auto] items-center gap-2">
-          <input
-            type="checkbox"
-            checked={difficulty()}
-            onChange={(e) => setDifficulty(e.currentTarget.checked)}
-          />
-          <div>Difficulty</div>
-          <div class="col-start-2 text-em-xs text-sub">
-            {getConfig.difficulty}
-          </div>
-        </label>
+        <form.Field name="punctuation">
+          {(field) => (
+            <Checkbox
+              field={() => field()}
+              class="items-start"
+              label={
+                <div>
+                  <div>Punctuation</div>
+                  <div class="text-em-xs text-sub">
+                    {getConfig.punctuation ? "enabled" : "disabled"}
+                  </div>
+                </div>
+              }
+            />
+          )}
+        </form.Field>
+        <form.Field name="numbers">
+          {(field) => (
+            <Checkbox
+              field={() => field()}
+              class="items-start"
+              label={
+                <div>
+                  <div>Numbers</div>
+                  <div class="text-em-xs text-sub">
+                    {getConfig.numbers ? "enabled" : "disabled"}
+                  </div>
+                </div>
+              }
+            />
+          )}
+        </form.Field>
+        <form.Field name="language">
+          {(field) => (
+            <Checkbox
+              field={() => field()}
+              class="items-start"
+              label={
+                <div>
+                  <div>Language</div>
+                  <div class="text-em-xs text-sub">{getConfig.language}</div>
+                </div>
+              }
+            />
+          )}
+        </form.Field>
+        <form.Field name="difficulty">
+          {(field) => (
+            <Checkbox
+              field={() => field()}
+              class="items-start"
+              label={
+                <div>
+                  <div>Difficulty</div>
+                  <div class="text-em-xs text-sub">{getConfig.difficulty}</div>
+                </div>
+              }
+            />
+          )}
+        </form.Field>
       </Show>
-      <label class="grid grid-cols-[max-content_auto] items-center gap-2">
-        <input
-          type="checkbox"
-          checked={funbox()}
-          onChange={(e) => setFunbox(e.currentTarget.checked)}
-        />
-        <div>Funbox</div>
-        <div class="col-start-2 text-em-xs text-sub">
-          {getConfig.funbox.length > 0 ? getConfig.funbox.join(", ") : "none"}
-        </div>
-      </label>
+      <form.Field name="funbox">
+        {(field) => (
+          <Checkbox
+            field={() => field()}
+            class="items-start"
+            label={
+              <div>
+                <div>Funbox</div>
+                <div class="text-em-xs text-sub">
+                  {getConfig.funbox.length > 0
+                    ? getConfig.funbox.join(", ")
+                    : `none`}
+                </div>
+              </div>
+            }
+          />
+        )}
+      </form.Field>
       <textarea
         placeholder="url"
         value={url()}
