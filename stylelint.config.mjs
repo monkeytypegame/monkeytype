9324c19ccaaae16342dd49137293f7cbcfdc782f
Nad Alaba <37968805+nadalaba@users.noreply.github.com>
2026-03-25 18:33:16 +0300

    re-add selector-pseudo-element-colon-notation: double

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -31,7 +31,6 @@ export default {
     "hue-degree-notation": null, // default: angle
     "media-feature-range-notation": null, // default: context
     "selector-not-notation": null, // default: complex
-    "selector-pseudo-element-colon-notation": null, // add // default: double
 
     // default pattern for these rules is a kebab case pattern
     "custom-property-pattern": null,
