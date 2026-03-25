314e8bc312dba7ded403d03a83cab067d8d306b6
Miodec <jack@monkeytype.com>
2026-03-25 11:45:09 +0100

    fix

--- frontend/src/ts/input/hotkeys/commandline.ts
+++ frontend/src/ts/input/hotkeys/commandline.ts
@@ -1,8 +1,10 @@
 import { hotkeys } from "../../states/hotkeys";
 import { showModal } from "../../states/modals";
+import { isAnyPopupVisible } from "../../utils/misc";
 import { createHotkey } from "./utils";
 
 function openCommandline(): void {
+  if (isAnyPopupVisible()) return;
   showModal("Commandline");
 }
 
