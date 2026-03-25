8dbbe10e72d8e11bdb2856b0ebfb9141870db1ae
Miodec <jack@monkeytype.com>
2026-03-25 10:22:46 +0100

    based on hotkey

--- frontend/src/ts/components/hotkeys/QuickRestartHotkey.tsx
+++ frontend/src/ts/components/hotkeys/QuickRestartHotkey.tsx
@@ -1,16 +1,16 @@
-import { JSXElement, Show } from "solid-js";
+import { Hotkey } from "@tanstack/solid-hotkeys";
+import { JSXElement } from "solid-js";
 
-import { getConfig } from "../../config/store";
 import { hotkeys } from "../../states/hotkeys";
+import { Conditional } from "../common/Conditional";
 import { Kbd } from "./Kbd";
 
 export function QuickRestartHotkey(): JSXElement {
   return (
-    <Show
-      when={getConfig.quickRestart !== "off"}
-      fallback=<kbd>tab {">"} enter</kbd>
-    >
-      <Kbd hotkey={hotkeys.quickRestart} />
-    </Show>
+    <Conditional
+      if={hotkeys.quickRestart === ("" as Hotkey)}
+      then={<kbd>tab {">"} enter</kbd>}
+      else={<Kbd hotkey={hotkeys.quickRestart} />}
+    />
   );
 }
