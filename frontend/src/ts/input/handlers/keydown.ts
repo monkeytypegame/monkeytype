6a9f2e2f6c1e0f068684c2ad68052e8c372ffa5a
Christian Fehmer <cfe@sexy-developer.com>
2026-03-25 09:48:42 +0100

    prevent shift+tab to insert tab

--- frontend/src/ts/input/handlers/keydown.ts
+++ frontend/src/ts/input/handlers/keydown.ts
@@ -168,7 +168,7 @@ export async function onKeydown(event: KeyboardEvent): Promise<void> {
     }
   }
 
-  if (event.key === "Tab") {
+  if (event.key === "Tab" && !event.shiftKey) {
     await handleTab(event, now);
     return;
   }
