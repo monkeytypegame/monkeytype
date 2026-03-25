98bb7033ef638453ad6aebfe01d6a7d50d6adc75
byseif21 <byseif21@gmail.com>
2026-03-25 13:03:12 +0200

    update tests

--- packages/schemas/__tests__/util.spec.ts
+++ packages/schemas/__tests__/util.spec.ts
@@ -22,6 +22,11 @@ describe("Schema Validation Tests", () => {
       expect(schema.safeParse("inv--alid").success).toBe(false);
       expect(schema.safeParse("inv-_alid").success).toBe(false);
     });
+
+    it("rejects dots", () => {
+      expect(schema.safeParse("invalid.dot").success).toBe(false);
+      expect(schema.safeParse(".invalid").success).toBe(false);
+    });
   });
 
   describe("slug", () => {
@@ -30,8 +35,22 @@ describe("Schema Validation Tests", () => {
     it("accepts valid slugs", () => {
       expect(schema.safeParse("valid-slug.123_test").success).toBe(true);
       expect(schema.safeParse("valid.dots").success).toBe(true);
-      expect(schema.safeParse("_leading_is_fine_in_slug").success).toBe(true);
+      expect(schema.safeParse("_leading_underscore_is_fine").success).toBe(
+        true,
+      );
+      expect(schema.safeParse("-leading_hyphen_is_fine").success).toBe(true);
       expect(schema.safeParse("trailing_is_fine_in_slug_").success).toBe(true);
+    });
+
+    it("rejects leading dots", () => {
+      expect(schema.safeParse(".invalid").success).toBe(false);
+    });
+
+    it("rejects invalid characters", () => {
+      expect(schema.safeParse("invalid,comma").success).toBe(false);
+      expect(schema.safeParse(",invalid").success).toBe(false);
+      expect(schema.safeParse("invalid space").success).toBe(false);
+      expect(schema.safeParse("invalid#hash").success).toBe(false);
     });
   });
 });
