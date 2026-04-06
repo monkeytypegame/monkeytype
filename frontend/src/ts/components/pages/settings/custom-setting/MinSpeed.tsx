03d09475db0de46f376be5da20c8363def6aa5bd
Miodec <jack@monkeytype.com>
2026-04-06 19:02:32 +0200

    min speed

--- frontend/src/ts/components/pages/settings/custom-setting/MinSpeed.tsx
+++ frontend/src/ts/components/pages/settings/custom-setting/MinSpeed.tsx
@@ -1,17 +1,21 @@
 import { MinWpmCustomSpeedSchema } from "@monkeytype/schemas/configs";
 import { createForm } from "@tanstack/solid-form";
-import { JSXElement } from "solid-js";
+import { createSignal, JSXElement } from "solid-js";
 
 import { configMetadata } from "../../../../config/metadata";
 import { setConfig } from "../../../../config/setters";
 import { getConfig } from "../../../../config/store";
-import { showSuccessNotification } from "../../../../states/notifications";
+import { AnimeShow } from "../../../common/anime";
+// import { showSuccessNotification } from "../../../../states/notifications";
 import { Button } from "../../../common/Button";
+import { Fa } from "../../../common/Fa";
 import { InputField } from "../../../ui/form/InputField";
 import { fromSchema } from "../../../ui/form/utils";
 import { Setting } from "../Setting";
 
 export function MinSpeed(): JSXElement {
+  const [showSavedIndicator, setShowSavedIndicator] = createSignal(false);
+
   const form = createForm(() => ({
     defaultValues: {
       minWpmCustomSpeed: getConfig.minWpmCustomSpeed,
@@ -24,7 +28,13 @@ export function MinSpeed(): JSXElement {
       } else {
         setConfig("minWpm", "custom");
       }
-      showSuccessNotification("Min speed saved");
+      // showSuccessNotification("Min speed saved", {
+      //   durationMs: 1000,
+      // });
+      setShowSavedIndicator(true);
+      setTimeout(() => {
+        setShowSavedIndicator(false);
+      }, 2000);
       setConfig("minWpmCustomSpeed", val);
     },
   }));
@@ -58,12 +68,19 @@ export function MinSpeed(): JSXElement {
                 },
               }}
               children={(field) => (
-                <InputField
-                  field={field}
-                  placeholder="min speed"
-                  showIndicator
-                  type="number"
-                />
+                <div class="relative">
+                  <InputField
+                    field={field}
+                    placeholder="min speed"
+                    showIndicator
+                    type="number"
+                  />
+                  <AnimeShow when={showSavedIndicator()}>
+                    <div class="absolute top-0 right-0 rounded bg-sub-alt p-[0.5em] text-main">
+                      <Fa icon="fa-save" fixedWidth />
+                    </div>
+                  </AnimeShow>
+                </div>
               )}
             />
           </form>
