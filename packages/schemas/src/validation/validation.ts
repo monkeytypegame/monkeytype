e3ca4489c675fe9f6cc7b36f8a68836f0c26b927
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2026-04-07 14:47:06 +0300

    impr(profanity): change error message when disallowed word is detected in username (@Leonabcd123) (#7766)
    !nuf

--- packages/schemas/src/validation/validation.ts
+++ packages/schemas/src/validation/validation.ts
@@ -2,7 +2,7 @@ import { replaceHomoglyphs } from "./homoglyphs";
 import { ZodEffects, ZodString } from "zod";
 
 // Sorry for the bad words
-const profanities = [
+const disallowedWords = [
   "miodec",
   "bitly",
   "niqqa",
@@ -403,7 +403,7 @@ function sanitizeString(str: string | undefined): string | undefined {
     .replace(/\s{3,}/g, "  ");
 }
 
-export function containsProfanity(
+function containsDisallowedWords(
   text: string,
   mode: "word" | "substring",
 ): boolean {
@@ -414,27 +414,29 @@ export function containsProfanity(
       return replaceHomoglyphs(sanitizeString(str) ?? "");
     });
 
-  const hasProfanity = profanities.some((profanity) => {
+  const hasDisallowedWords = disallowedWords.some((disallowedWord) => {
     return normalizedText.some((word) => {
       return mode === "word"
-        ? word.startsWith(profanity)
-        : word.includes(profanity);
+        ? word.startsWith(disallowedWord)
+        : word.includes(disallowedWord);
     });
   });
 
-  return hasProfanity;
+  return hasDisallowedWords;
 }
 
-export function doesNotContainProfanity(
+export function doesNotContainDisallowedWords(
   mode: "word" | "substring",
   schema: ZodString,
 ): ZodEffects<ZodString> {
   return schema.refine(
     (val) => {
-      return !containsProfanity(val, mode);
+      return !containsDisallowedWords(val, mode);
     },
     (val) => ({
-      message: `Profanity detected. Please remove it. If you believe this is a mistake, please contact us. (${val})`,
+      message: `Disallowed word detected. Please remove it. If you believe this is a mistake, please contact us (${val}).`,
     }),
   );
 }
+
+export const __testing = { containsDisallowedWords };
