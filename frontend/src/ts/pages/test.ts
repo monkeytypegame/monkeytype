7d3fbd78421d8f30d03a702fc4f3a7721c3e5e37
d1rshan <darshan.paccha@gmail.com>
2026-04-26 17:32:29 +0530

    fix(funbox): avoid reinitializing funboxes on test page exit (@d1rshan)

--- frontend/src/ts/pages/test.ts
+++ frontend/src/ts/pages/test.ts
@@ -18,6 +18,7 @@ export const page = new Page({
   afterHide: async (): Promise<void> => {
     TestLogic.restart({
       noAnim: true,
+      skipInit: true,
     });
     void Funbox.clear();
     void ModesNotice.update();
