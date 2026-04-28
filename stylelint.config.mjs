f1b83dde0a5ccc99857fed57d9954c0111111a81
Nad Alaba <37968805+nadalaba@users.noreply.github.com>
2026-04-28 22:23:47 +0300

    chore: add stylelint linter for css/scss (@nadalaba) (#7718)
    supersedes #7716
    
    ---------
    
    Co-authored-by: Jack <jack@monkeytype.com>

---
+++ stylelint.config.mjs
@@ -0,0 +1,76 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "font-family-no-missing-generic-family-keyword": [
+      true,
+      {
+        ignoreFontFamilies: [
+          "Font Awesome",
+          "Font Awesome Brands",
+          "Gallaudet",
+        ],
+      },
+    ], //default: true
+
+    "at-rule-no-unknown": [
+      true,
+      { ignoreAtRules: ["theme", "utility", "tailwind", "apply", "source"] },
+    ], // source for lightningCss and the rest for tailwind //default: true
+    "selector-type-no-unknown": [
+      true,
+      { ignore: ["custom-elements"], ignoreTypes: ["letter", "hint"] },
+    ], // default: [true, {ignore: ['custom-elements'],},]
+
+    "length-zero-no-unit": null, // default: [true, {ignore: ['custom-properties'], ignorePreludeOfAtRules: ['function', 'mixin'],},]
+
+    "value-keyword-case": null, // default: lower
+
+    "at-rule-empty-line-before": null, // default: ['always', {except: ['blockless-after-same-name-blockless', 'first-nested'], ignore: ['after-comment'],},]
+    "comment-empty-line-before": null, // default: ['always', {except: ['first-nested'], ignore: ['stylelint-commands'],},]
+    "custom-property-empty-line-before": null, // default: 'custom-property-empty-line-before': ['always',{except: ['after-custom-property', 'first-nested'],ignore: ['after-comment', 'inside-single-line-block'],},]
+    "declaration-empty-line-before": null, // default: ['always', {except: ['after-declaration', 'first-nested'], ignore: ['after-comment', 'inside-single-line-block'],},]
+    "rule-empty-line-before": null, // default: ['always-multi-line', {except: ['first-nested'], ignore: ['after-comment'],},]
+
+    "alpha-value-notation": null, // default: ['percentage',{exceptProperties: ['opacity','fill-opacity','flood-opacity','stop-opacity','stroke-opacity',],},]
+    "color-function-alias-notation": null, // default: without-alpha
+    "color-function-notation": null, // default: modern
+    "color-hex-length": null, // default: short
+    "hue-degree-notation": null, // default: angle
+    "import-notation": null, // default: url
+    "media-feature-range-notation": null, // default: context
+    "selector-not-notation": null, // default: complex
+
+    // default pattern for these rules is a kebab case pattern
+    "custom-property-pattern": null,
+    "keyframes-name-pattern": null,
+    "selector-class-pattern": null,
+    "selector-id-pattern": null,
+
+    "declaration-block-no-redundant-longhand-properties": null,
+    "shorthand-property-no-redundant-values": null,
+
+    "comment-whitespace-inside": null, // default: always
+  },
+  overrides: [
+    {
+      files: ["**/*.scss"],
+      extends: ["stylelint-config-standard-scss"],
+      rules: {
+        "no-invalid-position-at-import-rule": null, // consider adding // default: [true, {ignoreAtRules: ['use', 'forward'],},]
+
+        "at-rule-no-unknown": null, // use "scss/at-rule-no-unknown" to override this
+
+        "scss/at-extend-no-missing-placeholder": null,
+        "scss/load-no-partial-leading-underscore": null,
+        "scss/load-partial-extension": null, // default: never,
+        "scss/no-global-function-names": null,
+
+        "scss/dollar-variable-pattern": null,
+        "scss/double-slash-comment-empty-line-before": null,
+        "scss/double-slash-comment-whitespace-inside": null,
+      },
+    },
+  ],
+  ignoreFiles: ["**/dist/**"],
+};
