89b55490fc622a4ede9889feccf8613e464866e9
Christian Fehmer <cfe@sexy-developer.com>
2026-04-28 15:09:36 +0200

    fix userState not set correctly for signin providers

--- frontend/src/ts/firebase.ts
+++ frontend/src/ts/firebase.ts
@@ -69,8 +69,7 @@ export async function init(callback: ReadyCallback): Promise<void> {
 
     onAuthStateChanged(Auth, async (user) => {
       if (!ignoreAuthCallback) {
-        setUserId(user?.uid ?? null);
-        setUserVerified(user?.emailVerified ?? false);
+        setUserState(user);
         await callback(true, user);
       }
     });
@@ -79,8 +78,7 @@ export async function init(callback: ReadyCallback): Promise<void> {
     Auth = undefined;
     console.error("Firebase failed to initialize", e);
     await callback(false, null);
-    setUserId(null);
-    setUserVerified(false);
+    setUserState(null);
     if (isDevEnvironment()) {
       addBanner({
         level: "notice",
@@ -136,6 +134,21 @@ export async function signInWithEmailAndPassword(
   return result;
 }
 
+function setUserState(
+  options: {
+    uid: string;
+    emailVerified: boolean;
+  } | null,
+): void {
+  if (options === null) {
+    setUserId(null);
+    setUserVerified(false);
+  } else {
+    setUserId(options.uid);
+    setUserVerified(options.emailVerified);
+  }
+}
+
 export async function signInWithPopup(
   provider: AuthProvider,
   rememberMe: boolean,
@@ -153,11 +166,11 @@ export async function signInWithPopup(
     throw translateFirebaseError(error, "Failed to sign in with popup");
   }
   const additionalUserInfo = getAdditionalUserInfo(signedInUser);
+  setUserState(signedInUser.user);
   if (additionalUserInfo?.isNewUser) {
     googleSignUpEvent.dispatch({ signedInUser, isNewUser: true });
   } else {
     ignoreAuthCallback = false;
-    setUserId(signedInUser.user.uid);
     await readyCallback?.(true, signedInUser.user);
   }
 }
