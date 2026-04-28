2231a0d8a7cee155661e4b4268ba1dd9a79bd8c0
Miodec <jack@monkeytype.com>
2026-04-28 19:40:49 +0200

    verify notice responsiveness

--- frontend/src/ts/components/pages/account/VerifyNotice.tsx
+++ frontend/src/ts/components/pages/account/VerifyNotice.tsx
@@ -2,10 +2,11 @@ import { createSignal, JSXElement, Show } from "solid-js";
 
 import { sendVerificationEmail } from "../../../auth";
 import { isUserVerified } from "../../../states/core";
+import { cn } from "../../../utils/cn";
 import { Button } from "../../common/Button";
 import { Fa } from "../../common/Fa";
 
-export function VerifyNotice(): JSXElement {
+export function VerifyNotice(props: { class: string }): JSXElement {
   const [isProcessing, setProcessing] = createSignal(false);
 
   const resendVerificationEmail = () => {
@@ -14,12 +15,18 @@ export function VerifyNotice(): JSXElement {
   };
   return (
     <Show when={!isUserVerified()}>
-      <div class="mb-8 flex flex-row items-center gap-6 rounded p-4 ring-4 ring-sub-alt">
-        <Fa icon="fa-exclamation-triangle" class="text-4xl text-sub" />
-        <div>Your email address is still not verified</div>
+      <div
+        class={cn(
+          `grid items-center gap-4 rounded p-4 ring-4 ring-sub-alt md:grid-cols-[1fr_auto] ${props.class}`,
+        )}
+      >
+        <div class="grid grid-cols-[auto_1fr] items-center gap-4">
+          <Fa icon="fa-exclamation-triangle" class="text-4xl text-sub" />
+          <div>Your email address is still not verified</div>
+        </div>
         <Button
+          class="px-4"
           text="resend verification email"
-          class="ml-auto flex-none"
           disabled={isProcessing()}
           onClick={resendVerificationEmail}
         />
