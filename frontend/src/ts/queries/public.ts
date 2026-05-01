0d553f45bdc24085ce98fc682810c66956a05041
Christian Fehmer <cfe@sexy-developer.com>
2026-05-01 18:59:21 +0200

    revert staleTime

--- frontend/src/ts/queries/public.ts
+++ frontend/src/ts/queries/public.ts
@@ -20,7 +20,7 @@ const queryKeys = {
 };
 
 //cache results for one hour
-const staleTime = 100;
+const staleTime = 1000 * 60 * 60;
 
 // oxlint-disable-next-line typescript/explicit-function-return-type
 export const getContributorsQueryOptions = () =>
