4e5c8e0174d5376c0bd39a950f15c38a39610f61
Christian Fehmer <fehmer@users.noreply.github.com>
2026-05-05 09:36:42 +0200

    fix(themes): don't leak custom theme css after preview (@fehmer) (#7876)
    Fixes #7875
    
    Slow network connections caused the theme css to be applied on the
    #customTheme stylesheet link element after clearing the theme preview
    with `href=""`.
    
    Empty href is invalid, so best to remove the whole element if we don't
    need it:
    
    >The address of the link(s) is given by the href attribute. If the
    [href](https://html.spec.whatwg.org/multipage/semantics.html#attr-link-href)
    attribute is present, then its value must be a [valid non-empty URL
    potentially surrounded by
    spaces](https://html.spec.whatwg.org/multipage/urls-and-fetching.html#valid-non-empty-url-potentially-surrounded-by-spaces).
    
    https://html.spec.whatwg.org/multipage/semantics.html#attr-link-href
    
    While chrome is applying the last css file firefox removes it correctly
    but keeps the loaderBar active because of the missing onLoad callback

--- frontend/__tests__/components/core/Theme.spec.tsx
+++ frontend/__tests__/components/core/Theme.spec.tsx
@@ -85,13 +85,13 @@ describe("Theme component", () => {
   it("removes CSS when theme has no CSS", async () => {
     themeSignalMock.mockImplementation(() => ({ name: "light" }) as any);
     const { css } = renderComponent();
-    expect(css.getAttribute("href")).toBe("");
+    expect(css).not.toBeInTheDocument();
   });
 
   it("removes CSS when theme is custom", async () => {
     themeSignalMock.mockImplementation(() => ({ name: "custom" }) as any);
     const { css } = renderComponent();
-    expect(css.getAttribute("href")).toBe("");
+    expect(css).not.toBeInTheDocument();
   });
 
   it("handles CSS load error", () => {
