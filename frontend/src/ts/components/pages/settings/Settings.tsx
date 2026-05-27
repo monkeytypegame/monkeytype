cbe6030424bb584b9717811e593f216436add6e3
Darshan Paccha <darshan.paccha@gmail.com>
2026-05-27 14:09:11 +0530

    fix(account-settings): restore responsiveness in notice card and page (@d1rshan) (#8016)

--- frontend/src/ts/components/pages/settings/Settings.tsx
+++ frontend/src/ts/components/pages/settings/Settings.tsx
@@ -265,7 +265,7 @@ function AccountSettingsNotice(): JSXElement {
   });
   return (
     <Show when={!dismissed()}>
-      <div class="grid grid-cols-[auto_1fr_auto] items-center gap-8 rounded px-8 py-4 ring-4 ring-sub-alt">
+      <div class="grid grid-cols-[auto_1fr] items-center gap-4 rounded px-4 py-4 ring-4 ring-sub-alt md:grid-cols-[auto_1fr_auto] md:gap-8">
         <Fa icon="fa-user-cog" class="text-4xl text-sub" />
         <div>
           Account settings have moved. You can now access them by hovering over
@@ -275,7 +275,7 @@ function AccountSettingsNotice(): JSXElement {
         <Button
           text="go to account settings"
           href="/account-settings"
-          class="p-4"
+          class="col-span-2 p-4 md:col-span-1"
           router-link
           onClick={() => {
             setDismissed(true);
