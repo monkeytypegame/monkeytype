eeffceae26eef135623aeadfacc2ac9839b36e0c
Miodec <jack@monkeytype.com>
2026-06-12 22:52:03 +0200

    style

--- frontend/src/ts/components/pages/404Page.tsx
+++ frontend/src/ts/components/pages/404Page.tsx
@@ -6,18 +6,23 @@ export function NotFoundPage() {
   return (
     <Page id="404">
       <div class="flex h-full items-center justify-center">
-        <div class="flex flex-col gap-4 md:flex-row">
-          <img src="/images/monkeymeme.jpg" class="rounded-xl" />
+        <div class="flex flex-col gap-8 md:flex-row">
+          <div class="place-self-center">
+            <img src="/images/monkeymeme.jpg" class="rounded-xl" />
+          </div>
           <div class="flex max-w-md flex-col items-center gap-4">
-            <H2 text="404" class="text-7xl text-main" />
+            <H2 text="404" class="pb-0 text-7xl text-main" />
             <p class="text-center">
-              Ooops! Looks like this page or resource doesn&apos;t exist.
+              Ooops! Looks like this page
+              <br />
+              or resource doesn&apos;t exist.
             </p>
             <Button
               fa={{ icon: "fa-home" }}
               text="Go Home"
               router-link
               href="/"
+              class="px-8 py-4"
             />
           </div>
         </div>
