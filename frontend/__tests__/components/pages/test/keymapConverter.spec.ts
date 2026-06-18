4651c5f18f63b227338757223bc44882c4b71c9d
Christian Fehmer <cfe@sexy-developer.com>
2026-06-18 10:32:23 +0200

    format test

--- frontend/__tests__/components/pages/test/keymapConverter.spec.ts
+++ frontend/__tests__/components/pages/test/keymapConverter.spec.ts
@@ -13,162 +13,65 @@ describe("keymap converter", () => {
         showAllKeys: false,
       });
 
-      expect(result.row1).toEqual([
-        {
-          legends: ["1", "!", "1", "!"],
-        },
-        {
-          legends: ["2", "@", "2", "@"],
-        },
-        {
-          legends: ["3", "#", "3", "#"],
-        },
-        {
-          legends: ["4", "$", "4", "$"],
-        },
-        {
-          legends: ["5", "%", "5", "%"],
-        },
-        {
-          legends: ["6", "^", "6", "^"],
-        },
-        {
-          legends: ["7", "&", "7", "&"],
-        },
-        {
-          legends: ["8", "*", "8", "*"],
-        },
-        {
-          legends: ["9", "(", "9", "("],
-        },
-        {
-          legends: ["0", ")", "0", ")"],
-        },
-        {
-          legends: ["-", "_", "-", "_"],
-        },
-        {
-          legends: ["=", "+", "=", "+"],
-        },
+      expect(result.row1, "row1").toEqual([
+        { legends: ["1", "!", "1", "!"] },
+        { legends: ["2", "@", "2", "@"] },
+        { legends: ["3", "#", "3", "#"] },
+        { legends: ["4", "$", "4", "$"] },
+        { legends: ["5", "%", "5", "%"] },
+        { legends: ["6", "^", "6", "^"] },
+        { legends: ["7", "&", "7", "&"] },
+        { legends: ["8", "*", "8", "*"] },
+        { legends: ["9", "(", "9", "("] },
+        { legends: ["0", ")", "0", ")"] },
+        { legends: ["-", "_", "-", "_"] },
+        { legends: ["=", "+", "=", "+"] },
       ]);
 
-      expect(result.row2).toEqual([
-        {
-          legends: ["q", "Q", "q", "Q"],
-          x: 2,
-        },
-        {
-          legends: ["w", "W", "w", "W"],
-        },
-        {
-          legends: ["e", "E", "e", "E"],
-        },
-        {
-          legends: ["r", "R", "r", "R"],
-        },
-        {
-          legends: ["t", "T", "t", "T"],
-        },
-        {
-          legends: ["y", "Y", "y", "Y"],
-        },
-        {
-          legends: ["u", "U", "u", "U"],
-        },
-        {
-          legends: ["i", "I", "i", "I"],
-        },
-        {
-          legends: ["o", "O", "o", "O"],
-        },
-        {
-          legends: ["p", "P", "p", "P"],
-        },
-        {
-          legends: ["[", "{", "[", "{"],
-        },
-        {
-          legends: ["]", "}", "]", "}"],
-        },
+      expect(result.row2, "row2").toEqual([
+        { legends: ["q", "Q", "q", "Q"], x: 2 },
+        { legends: ["w", "W", "w", "W"] },
+        { legends: ["e", "E", "e", "E"] },
+        { legends: ["r", "R", "r", "R"] },
+        { legends: ["t", "T", "t", "T"] },
+        { legends: ["y", "Y", "y", "Y"] },
+        { legends: ["u", "U", "u", "U"] },
+        { legends: ["i", "I", "i", "I"] },
+        { legends: ["o", "O", "o", "O"] },
+        { legends: ["p", "P", "p", "P"] },
+        { legends: ["[", "{", "[", "{"] },
+        { legends: ["]", "}", "]", "}"] },
       ]);
 
-      expect(result.row3).toEqual([
-        {
-          legends: ["a", "A", "a", "A"],
-          x: 4,
-        },
-        {
-          legends: ["s", "S", "s", "S"],
-        },
-        {
-          legends: ["d", "D", "d", "D"],
-        },
-        {
-          legends: ["f", "F", "f", "F"],
-        },
-        {
-          legends: ["g", "G", "g", "G"],
-        },
-        {
-          legends: ["h", "H", "h", "H"],
-        },
-        {
-          legends: ["j", "J", "j", "J"],
-        },
-        {
-          legends: ["k", "K", "k", "K"],
-        },
-        {
-          legends: ["l", "L", "l", "L"],
-        },
-        {
-          legends: [";", ":", ";", ":"],
-        },
-        {
-          legends: ["'", '"', "'", '"'],
-        },
+      expect(result.row3, "row3").toEqual([
+        { legends: ["a", "A", "a", "A"], x: 4 },
+        { legends: ["s", "S", "s", "S"] },
+        { legends: ["d", "D", "d", "D"] },
+        { legends: ["f", "F", "f", "F"] },
+        { legends: ["g", "G", "g", "G"] },
+        { legends: ["h", "H", "h", "H"] },
+        { legends: ["j", "J", "j", "J"] },
+        { legends: ["k", "K", "k", "K"] },
+        { legends: ["l", "L", "l", "L"] },
+        { legends: [";", ":", ";", ":"] },
+        { legends: ["'", '"', "'", '"'] },
       ]);
 
-      expect(result.row4).toEqual([
-        {
-          legends: ["z", "Z", "z", "Z"],
-          x: 6,
-        },
-        {
-          legends: ["x", "X", "x", "X"],
-        },
-        {
-          legends: ["c", "C", "c", "C"],
-        },
-        {
-          legends: ["v", "V", "v", "V"],
-        },
-        {
-          legends: ["b", "B", "b", "B"],
-        },
-        {
-          legends: ["n", "N", "n", "N"],
-        },
-        {
-          legends: ["m", "M", "m", "M"],
-        },
-        {
-          legends: [",", "<", ",", "<"],
-        },
-        {
-          legends: [".", ">", ".", ">"],
-        },
-        {
-          legends: ["/", "?", "/", "?"],
-        },
+      expect(result.row4, "row4").toEqual([
+        { legends: ["z", "Z", "z", "Z"], x: 6 },
+        { legends: ["x", "X", "x", "X"] },
+        { legends: ["c", "C", "c", "C"] },
+        { legends: ["v", "V", "v", "V"] },
+        { legends: ["b", "B", "b", "B"] },
+        { legends: ["n", "N", "n", "N"] },
+        { legends: ["m", "M", "m", "M"] },
+        { legends: [",", "<", ",", "<"] },
+        { legends: [".", ">", ".", ">"] },
+        { legends: ["/", "?", "/", "?"] },
       ]);
 
-      expect(result.row5).toEqual([
-        {
-          legends: ["qwerty", "qwerty", "qwerty", "qwerty"],
-          width: 6,
-          x: 22,
-        },
+      expect(result.row5, "row5").toEqual([
+        { legends: ["qwerty", "qwerty", "qwerty", "qwerty"], width: 6, x: 22 },
       ]);
     });
 
@@ -179,217 +82,80 @@ describe("keymap converter", () => {
         showAllKeys: true,
       });
 
-      expect(result.row1).toEqual([
-        {
-          legends: ["`", "~", "`", "~"],
-        },
-        {
-          legends: ["1", "!", "1", "!"],
-        },
-        {
-          legends: ["2", "@", "2", "@"],
-        },
-        {
-          legends: ["3", "#", "3", "#"],
-        },
-        {
-          legends: ["4", "$", "4", "$"],
-        },
-        {
-          legends: ["5", "%", "5", "%"],
-        },
-        {
-          legends: ["6", "^", "6", "^"],
-        },
-        {
-          legends: ["7", "&", "7", "&"],
-        },
-        {
-          legends: ["8", "*", "8", "*"],
-        },
-        {
-          legends: ["9", "(", "9", "("],
-        },
-        {
-          legends: ["0", ")", "0", ")"],
-        },
-        {
-          legends: ["-", "_", "-", "_"],
-        },
-        {
-          legends: ["=", "+", "=", "+"],
-        },
-        {
-          legends: ["BS", "BS", "BS", "BS"],
-          width: 2,
-        },
+      expect(result.row1, "row1").toEqual([
+        { legends: ["`", "~", "`", "~"] },
+        { legends: ["1", "!", "1", "!"] },
+        { legends: ["2", "@", "2", "@"] },
+        { legends: ["3", "#", "3", "#"] },
+        { legends: ["4", "$", "4", "$"] },
+        { legends: ["5", "%", "5", "%"] },
+        { legends: ["6", "^", "6", "^"] },
+        { legends: ["7", "&", "7", "&"] },
+        { legends: ["8", "*", "8", "*"] },
+        { legends: ["9", "(", "9", "("] },
+        { legends: ["0", ")", "0", ")"] },
+        { legends: ["-", "_", "-", "_"] },
+        { legends: ["=", "+", "=", "+"] },
+        { legends: ["BS", "BS", "BS", "BS"], width: 2 },
       ]);
 
-      expect(result.row2).toEqual([
-        {
-          legends: ["Tab", "Tab", "Tab", "Tab"],
-          width: 1.5,
-        },
-        {
-          legends: ["q", "Q", "q", "Q"],
-        },
-        {
-          legends: ["w", "W", "w", "W"],
-        },
-        {
-          legends: ["e", "E", "e", "E"],
-        },
-        {
-          legends: ["r", "R", "r", "R"],
-        },
-        {
-          legends: ["t", "T", "t", "T"],
-        },
-        {
-          legends: ["y", "Y", "y", "Y"],
-        },
-        {
-          legends: ["u", "U", "u", "U"],
-        },
-        {
-          legends: ["i", "I", "i", "I"],
-        },
-        {
-          legends: ["o", "O", "o", "O"],
-        },
-        {
-          legends: ["p", "P", "p", "P"],
-        },
-        {
-          legends: ["[", "{", "[", "{"],
-        },
-        {
-          legends: ["]", "}", "]", "}"],
-        },
-        {
-          legends: ["\\", "|", "\\", "|"],
-          width: 1.5,
-        },
+      expect(result.row2, "row2").toEqual([
+        { legends: ["Tab", "Tab", "Tab", "Tab"], width: 1.5 },
+        { legends: ["q", "Q", "q", "Q"] },
+        { legends: ["w", "W", "w", "W"] },
+        { legends: ["e", "E", "e", "E"] },
+        { legends: ["r", "R", "r", "R"] },
+        { legends: ["t", "T", "t", "T"] },
+        { legends: ["y", "Y", "y", "Y"] },
+        { legends: ["u", "U", "u", "U"] },
+        { legends: ["i", "I", "i", "I"] },
+        { legends: ["o", "O", "o", "O"] },
+        { legends: ["p", "P", "p", "P"] },
+        { legends: ["[", "{", "[", "{"] },
+        { legends: ["]", "}", "]", "}"] },
+        { legends: ["\\", "|", "\\", "|"], width: 1.5 },
       ]);
 
-      expect(result.row3).toEqual([
-        {
-          legends: ["Caps", "Caps", "Caps", "Caps"],
-          width: 1.75,
-        },
-        {
-          legends: ["a", "A", "a", "A"],
-        },
-        {
-          legends: ["s", "S", "s", "S"],
-        },
-        {
-          legends: ["d", "D", "d", "D"],
-        },
-        {
-          legends: ["f", "F", "f", "F"],
-        },
-        {
-          legends: ["g", "G", "g", "G"],
-        },
-        {
-          legends: ["h", "H", "h", "H"],
-        },
-        {
-          legends: ["j", "J", "j", "J"],
-        },
-        {
-          legends: ["k", "K", "k", "K"],
-        },
-        {
-          legends: ["l", "L", "l", "L"],
-        },
-        {
-          legends: [";", ":", ";", ":"],
-        },
-        {
-          legends: ["'", '"', "'", '"'],
-        },
-        {
-          legends: ["Enter", "Enter", "Enter", "Enter"],
-          width: 2.25,
-        },
+      expect(result.row3, "row3").toEqual([
+        { legends: ["Caps", "Caps", "Caps", "Caps"], width: 1.75 },
+        { legends: ["a", "A", "a", "A"] },
+        { legends: ["s", "S", "s", "S"] },
+        { legends: ["d", "D", "d", "D"] },
+        { legends: ["f", "F", "f", "F"] },
+        { legends: ["g", "G", "g", "G"] },
+        { legends: ["h", "H", "h", "H"] },
+        { legends: ["j", "J", "j", "J"] },
+        { legends: ["k", "K", "k", "K"] },
+        { legends: ["l", "L", "l", "L"] },
+        { legends: [";", ":", ";", ":"] },
+        { legends: ["'", '"', "'", '"'] },
+        { legends: ["Enter", "Enter", "Enter", "Enter"], width: 2.25 },
       ]);
 
-      expect(result.row4).toEqual([
-        {
-          legends: ["Shift", "Shift", "Shift", "Shift"],
-          width: 2.25,
-        },
-        {
-          legends: ["z", "Z", "z", "Z"],
-        },
-        {
-          legends: ["x", "X", "x", "X"],
-        },
-        {
-          legends: ["c", "C", "c", "C"],
-        },
-        {
-          legends: ["v", "V", "v", "V"],
-        },
-        {
-          legends: ["b", "B", "b", "B"],
-        },
-        {
-          legends: ["n", "N", "n", "N"],
-        },
-        {
-          legends: ["m", "M", "m", "M"],
-        },
-        {
-          legends: [",", "<", ",", "<"],
-        },
-        {
-          legends: [".", ">", ".", ">"],
-        },
-        {
-          legends: ["/", "?", "/", "?"],
-        },
-        {
-          legends: ["Shift", "Shift", "Shift", "Shift"],
-          width: 2.75,
-        },
+      expect(result.row4, "row4").toEqual([
+        { legends: ["Shift", "Shift", "Shift", "Shift"], width: 2.25 },
+        { legends: ["z", "Z", "z", "Z"] },
+        { legends: ["x", "X", "x", "X"] },
+        { legends: ["c", "C", "c", "C"] },
+        { legends: ["v", "V", "v", "V"] },
+        { legends: ["b", "B", "b", "B"] },
+        { legends: ["n", "N", "n", "N"] },
+        { legends: ["m", "M", "m", "M"] },
+        { legends: [",", "<", ",", "<"] },
+        { legends: [".", ">", ".", ">"] },
+        { legends: ["/", "?", "/", "?"] },
+        { legends: ["Shift", "Shift", "Shift", "Shift"], width: 2.75 },
       ]);
 
-      expect(result.row5).toEqual([
-        {
-          legends: ["Alt", "Alt", "Alt", "Alt"],
-          width: 1.25,
-        },
-        {
-          legends: ["Monke", "Monke", "Monke", "Monke"],
-          width: 1.25,
-        },
-        {
-          legends: ["Ctrl", "Ctrl", "Ctrl", "Ctrl"],
-          width: 1.25,
-        },
-        {
-          legends: ["qwerty", "qwerty", "qwerty", "qwerty"],
-          width: 6.25,
-        },
-        {
-          legends: ["Ctrl", "Ctrl", "Ctrl", "Ctrl"],
-          width: 1.25,
-        },
-        {
-          legends: ["Monke", "Monke", "Monke", "Monke"],
-          width: 1.25,
-        },
-        {
-          legends: ["Meta", "Meta", "Meta", "Meta"],
-          width: 1.25,
-        },
-        {
-          legends: ["Ctrl", "Ctrl", "Ctrl", "Ctrl"],
-          width: 1.25,
-        },
+      expect(result.row5, "row5").toEqual([
+        { legends: ["Alt", "Alt", "Alt", "Alt"], width: 1.25 },
+        { legends: ["Monke", "Monke", "Monke", "Monke"], width: 1.25 },
+        { legends: ["Ctrl", "Ctrl", "Ctrl", "Ctrl"], width: 1.25 },
+        { legends: ["qwerty", "qwerty", "qwerty", "qwerty"], width: 6.25 },
+        { legends: ["Ctrl", "Ctrl", "Ctrl", "Ctrl"], width: 1.25 },
+        { legends: ["Monke", "Monke", "Monke", "Monke"], width: 1.25 },
+        { legends: ["Meta", "Meta", "Meta", "Meta"], width: 1.25 },
+        { legends: ["Ctrl", "Ctrl", "Ctrl", "Ctrl"], width: 1.25 },
       ]);
     });
 
@@ -400,171 +166,66 @@ describe("keymap converter", () => {
         showAllKeys: false,
       });
 
-      expect(result.row1).toEqual([
-        {
-          legends: ["1", "!", "1", "!"],
-        },
-        {
-          legends: ["2", "@", "2", "@"],
-        },
-        {
-          legends: ["3", "#", "3", "#"],
-        },
-        {
-          legends: ["4", "$", "4", "$"],
-        },
-        {
-          legends: ["5", "%", "5", "%"],
-        },
-        {
-          legends: ["6", "^", "6", "^"],
-        },
-        {
-          legends: ["7", "&", "7", "&"],
-          x: 8,
-        },
-        {
-          legends: ["8", "*", "8", "*"],
-        },
-        {
-          legends: ["9", "(", "9", "("],
-        },
-        {
-          legends: ["0", ")", "0", ")"],
-        },
-        {
-          legends: ["-", "_", "-", "_"],
-        },
-        {
-          legends: ["=", "+", "=", "+"],
-        },
+      expect(result.row1, "row1").toEqual([
+        { legends: ["1", "!", "1", "!"] },
+        { legends: ["2", "@", "2", "@"] },
+        { legends: ["3", "#", "3", "#"] },
+        { legends: ["4", "$", "4", "$"] },
+        { legends: ["5", "%", "5", "%"] },
+        { legends: ["6", "^", "6", "^"] },
+        { legends: ["7", "&", "7", "&"], x: 8 },
+        { legends: ["8", "*", "8", "*"] },
+        { legends: ["9", "(", "9", "("] },
+        { legends: ["0", ")", "0", ")"] },
+        { legends: ["-", "_", "-", "_"] },
+        { legends: ["=", "+", "=", "+"] },
       ]);
 
-      expect(result.row2).toEqual([
-        {
-          legends: ["q", "Q", "q", "Q"],
-          x: 2,
-        },
-        {
-          legends: ["w", "W", "w", "W"],
-        },
-        {
-          legends: ["e", "E", "e", "E"],
-        },
-        {
-          legends: ["r", "R", "r", "R"],
-        },
-        {
-          legends: ["t", "T", "t", "T"],
-        },
-        {
-          legends: ["y", "Y", "y", "Y"],
-          x: 8,
-        },
-        {
-          legends: ["u", "U", "u", "U"],
-        },
-        {
-          legends: ["i", "I", "i", "I"],
-        },
-        {
-          legends: ["o", "O", "o", "O"],
-        },
-        {
-          legends: ["p", "P", "p", "P"],
-        },
-        {
-          legends: ["[", "{", "[", "{"],
-        },
-        {
-          legends: ["]", "}", "]", "}"],
-        },
+      expect(result.row2, "row2").toEqual([
+        { legends: ["q", "Q", "q", "Q"], x: 2 },
+        { legends: ["w", "W", "w", "W"] },
+        { legends: ["e", "E", "e", "E"] },
+        { legends: ["r", "R", "r", "R"] },
+        { legends: ["t", "T", "t", "T"] },
+        { legends: ["y", "Y", "y", "Y"], x: 8 },
+        { legends: ["u", "U", "u", "U"] },
+        { legends: ["i", "I", "i", "I"] },
+        { legends: ["o", "O", "o", "O"] },
+        { legends: ["p", "P", "p", "P"] },
+        { legends: ["[", "{", "[", "{"] },
+        { legends: ["]", "}", "]", "}"] },
       ]);
 
-      expect(result.row3).toEqual([
-        {
-          legends: ["a", "A", "a", "A"],
-          x: 4,
-        },
-        {
-          legends: ["s", "S", "s", "S"],
-        },
-        {
-          legends: ["d", "D", "d", "D"],
-        },
-        {
-          legends: ["f", "F", "f", "F"],
-        },
-        {
-          legends: ["g", "G", "g", "G"],
-        },
-        {
-          legends: ["h", "H", "h", "H"],
-          x: 8,
-        },
-        {
-          legends: ["j", "J", "j", "J"],
-        },
-        {
-          legends: ["k", "K", "k", "K"],
-        },
-        {
-          legends: ["l", "L", "l", "L"],
-        },
-        {
-          legends: [";", ":", ";", ":"],
-        },
-        {
-          legends: ["'", '"', "'", '"'],
-        },
+      expect(result.row3, "row3").toEqual([
+        { legends: ["a", "A", "a", "A"], x: 4 },
+        { legends: ["s", "S", "s", "S"] },
+        { legends: ["d", "D", "d", "D"] },
+        { legends: ["f", "F", "f", "F"] },
+        { legends: ["g", "G", "g", "G"] },
+        { legends: ["h", "H", "h", "H"], x: 8 },
+        { legends: ["j", "J", "j", "J"] },
+        { legends: ["k", "K", "k", "K"] },
+        { legends: ["l", "L", "l", "L"] },
+        { legends: [";", ":", ";", ":"] },
+        { legends: ["'", '"', "'", '"'] },
       ]);
 
-      expect(result.row4).toEqual([
-        {
-          legends: ["z", "Z", "z", "Z"],
-          x: 6,
-        },
-        {
-          legends: ["x", "X", "x", "X"],
-        },
-        {
-          legends: ["c", "C", "c", "C"],
-        },
-        {
-          legends: ["v", "V", "v", "V"],
-        },
-        {
-          legends: ["b", "B", "b", "B"],
-        },
-        {
-          legends: ["n", "N", "n", "N"],
-          x: 8,
-        },
-        {
-          legends: ["m", "M", "m", "M"],
-        },
-        {
-          legends: [",", "<", ",", "<"],
-        },
-        {
-          legends: [".", ">", ".", ">"],
-        },
-        {
-          legends: ["/", "?", "/", "?"],
-        },
+      expect(result.row4, "row4").toEqual([
+        { legends: ["z", "Z", "z", "Z"], x: 6 },
+        { legends: ["x", "X", "x", "X"] },
+        { legends: ["c", "C", "c", "C"] },
+        { legends: ["v", "V", "v", "V"] },
+        { legends: ["b", "B", "b", "B"] },
+        { legends: ["n", "N", "n", "N"], x: 8 },
+        { legends: ["m", "M", "m", "M"] },
+        { legends: [",", "<", ",", "<"] },
+        { legends: [".", ">", ".", ">"] },
+        { legends: ["/", "?", "/", "?"] },
       ]);
 
-      expect(result.row5).toEqual([
-        {
-          legends: ["qwerty", "qwerty", "qwerty", "qwerty"],
-          width: 3,
-          x: 23,
-        },
-        {
-          legends: ["", "", "", ""],
-          width: 3,
-          x: 8,
-        },
+      expect(result.row5, "row5").toEqual([
+        { legends: ["qwerty", "qwerty", "qwerty", "qwerty"], width: 3, x: 23 },
+        { legends: ["", "", "", ""], width: 3, x: 8 },
       ]);
     });
   });
