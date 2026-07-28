9e137981cd51aeebad17a895e882333ad355b715
Carter Cormier <ccorm007@gmail.com>
2026-07-28 03:49:12 -0700

    feat(layout): add russian+qwerty layout

--- packages/schemas/src/layouts.ts
+++ packages/schemas/src/layouts.ts
@@ -242,6 +242,7 @@ export const LayoutNameSchema = z.enum(
     "nokwts",
     "vylet_v4",
     "armenian_hm_qwerty",
+    "russian+qwerty",
   ],
   {
     errorMap: customEnumErrorHandler("Must be a supported layout"),
