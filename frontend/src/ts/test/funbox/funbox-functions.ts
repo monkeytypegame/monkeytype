1261307da20aea3f866920c16a649be9e320efe0
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2026-08-17 13:33:38 +0300

    Remove

--- frontend/src/ts/test/funbox/funbox-functions.ts
+++ frontend/src/ts/test/funbox/funbox-functions.ts
@@ -352,12 +352,10 @@ const list: Partial<Record<FunboxName, FunboxFunctions>> = {
       const graphemes = [...segmenter.segment(word)].map((s) => s.segment);
 
       const emojiRegex = /\p{Emoji}/u;
-      const emojiComponentRegex = /\p{Emoji_Component}/u;
 
       const units = [];
       for (const g of graphemes) {
-        // Handles emojis that have ZWJs and regional indicators
-        if (emojiRegex.test(g) && emojiComponentRegex.test(g)) {
+        if (emojiRegex.test(g)) {
           units.push(g);
           continue;
         }
